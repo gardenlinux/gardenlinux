ebc458ea193c6b53bc866342d26e3e65dbc30686
Andre Russ <andre.russ@sap.com>
2020-03-13 14:16:24 +0100

    make it bootable with efi and bios based on labels

--- makef.sh
+++ makef.sh
@@ -4,17 +4,18 @@ dd if=/dev/zero of=$1.raw seek=2048 bs=1 count=0 seek=2G
 loopback=$(losetup -f --show $1.raw)
 
 echo 'label: gpt
-type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B, name="EFI", size=128MiB
+type=21686148-6449-6E6F-744E-656564454649, name="BIOS", size=1MiB
+type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B, name="EFI", size=127MiB
 type=0FC63DAF-8483-4772-8E79-3D69D8477DE4, name="ROOT"' | sfdisk $loopback
 partprobe $loopback
 
-mkfs.vfat ${loopback}p1 -n EFI
-mkfs.ext4 ${loopback}p2 -L ROOT
+mkfs.vfat ${loopback}p2 -n EFI
+mkfs.ext4 ${loopback}p3 -L ROOT
 
 mkdir -p $1
-mount ${loopback}p2 $1
+mount ${loopback}p3 $1
 mkdir -p $1/boot/efi
-mount ${loopback}p1 $1/boot/efi
+mount ${loopback}p2 $1/boot/efi
 
 tar xf $2 -C $1
 
@@ -22,5 +23,15 @@ mount -t proc proc $1/proc
 mount -t sysfs sys $1/sys
 mount --bind /dev  $1/dev
 
+chroot $1 grub-install --target=i386-pc /dev/loop0
+chroot $1 grub-install --target=x86_64-efi /dev/loop0 --no-nvram
+chroot $1 update-grub
 
+umount $1/dev
+umount $1/sys
+umount $1/proc
+umount $1/boot/efi
+umount $1
+
+losetup -d loopback
 
