6c5b5867e7b4c8c5b1cfec4b6e4918b2c4536dd9
Andre Russ <andre.russ@sap.com>
2020-03-13 14:58:49 +0100

    bugfixes

--- features/server/configure.sh
+++ features/server/configure.sh
@@ -2,5 +2,6 @@ systemctl enable systemd-networkd
 systemctl enable systemd-timesyncd
 systemctl enable systemd-resolved
 systemctl enable fail2ban
+systemctl enable ferm
 #systemctl enable systemd-machine-id-setup
 #systemctl enable systemd-machine-id-commit
