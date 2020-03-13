217d9bda16337b278b9291b03caeb5ab41f2a0a1
Andre Russ <andre.russ@sap.com>
2020-03-13 15:14:00 +0100

    upgraded server feature

--- features/server/configure.sh
+++ features/server/configure.sh
@@ -5,3 +5,6 @@ systemctl enable fail2ban
 systemctl enable ferm
 #systemctl enable systemd-machine-id-setup
 #systemctl enable systemd-machine-id-commit
+for i in $(ls /boot | grep vmlinuz | sed "s/vmlinuz-//"); do
+	systemctl enable kexec-load@$i
+done
