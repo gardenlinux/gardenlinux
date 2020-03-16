d6be693f37fc0af4c5c18e6ad999922732e50f70
Dirk Marwinski <dirk.marwinski@gmx.de>
2020-03-16 18:26:20 +0100

    Upload Script for GCP, EC2 upload fixes, more
    - remove resizefs and growroot capabilites from aws cloud init

--- features/aws/configure.sh
+++ features/aws/configure.sh
@@ -3,3 +3,8 @@
 sed -i "s/^#NTP=/NTP=instance-data.ec2.internal/g" /etc/systemd/system.conf
 # add "nvme_core.io_timeout=4294967295" to GRUB_CMDLINE_LINUX
 echo "TODO add GRUB paremeter"
+
+# growpart is done in initramfs, growroot by systemd
+mv /etc/cloud/cloud.cfg /etc/cloud/cloud.cfg.bak
+cat /etc/cloud/cloud.cfg.bak | grep -v "^ - growpart$" | grep -v "^ - resizefs$" >/etc/cloud/cloud.cfg  
+rm /etc/cloud/cloud.cfg.bak 
