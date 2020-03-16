da55eec2460b3e5dcff1f3677925fe9338f6f54a
Andre Russ <andre.russ@sap.com>
2020-03-16 18:48:00 +0100

    no need for chrony any more (ntp done by systemd)

--- features/aws/configure.sh
+++ features/aws/configure.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash 
 
+sed -i "s/^#NTP=/NTP=instance-data.ec2.internal/g" /etc/systemd/system.conf
 # add "nvme_core.io_timeout=4294967295" to GRUB_CMDLINE_LINUX
 echo "TODO add GRUB paremeter"
