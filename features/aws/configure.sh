8b2b65f9a0632f50f8f3d96d3524a6989cf76d02
Dirk Marwinski <dirk.marwinski@gmx.de>
2020-03-17 10:35:52 +0100

    Cleanup

--- features/aws/configure.sh
+++ features/aws/configure.sh
@@ -6,5 +6,5 @@ echo "TODO add GRUB paremeter"
 
 # growpart is done in initramfs, growroot by systemd
 mv /etc/cloud/cloud.cfg /etc/cloud/cloud.cfg.bak
-cat /etc/cloud/cloud.cfg.bak | grep -v "^ - growpart$" | grep -v "^ - resizefs$" >/etc/cloud/cloud.cfg  
+cat /etc/cloud/cloud.cfg.bak | grep -v "^ - growpart$" | grep -v "^ - resizefs$" | grep -v "^ - ntp$" >/etc/cloud/cloud.cfg  
 rm /etc/cloud/cloud.cfg.bak 
