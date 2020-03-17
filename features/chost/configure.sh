d95d97d5c5a996c053c9dfabc5ba372f97149e22
Andre Russ <andre.russ@sap.com>
2020-03-17 04:30:36 +0100

    cleaner download of k8s executables

--- features/chost/configure.sh
+++ features/chost/configure.sh
@@ -1,3 +1,8 @@
-wget -P /usr/local/bin https://storage.googleapis.com/kubernetes-release/release/v1.17.4/bin/linux/amd64/{kubeadm,kubelet,kubectl}
+wget --progress=bar:force:noscroll -P \
+	/usr/local/bin https://storage.googleapis.com/kubernetes-release/release/v1.17.4/bin/linux/amd64/{kubeadm,kubelet,kubectl} \
+	https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.17.0/crictl-v1.17.0-linux-amd64.tar.gz 
+	
+tar -C /usr/local/bin -xf /usr/local/bin/crictl-v1.17.0-linux-amd64.tar.gz
+rm -f /usr/local/bin/crictl-v1.17.0-linux-amd64.tar.gz
+
 chmod +x /usr/local/bin/{kubeadm,kubelet,kubectl}
-wget -O- https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.17.0/crictl-v1.17.0-linux-amd64.tar.gz | tar -C /usr/local/bin -xz
