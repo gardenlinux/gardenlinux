84117e3a3f4aaf770fe4c355080dfb49b6351b58
Andre Russ <andre.russ@sap.com>
2020-03-23 11:24:52 +0100

    Removed curl from build dependencies

--- scripts/.snapshot-url.sh
+++ scripts/.snapshot-url.sh
@@ -20,10 +20,14 @@ timestamp="${1:-}"; shift || eusage 'missing timestamp'
 archive="${1:-debian}"
 
 t="$(date --date "$timestamp" '+%Y%m%dT%H%M%SZ')"
-# use caching proxy to avoid throttling, if available
 
-if curl -s --fail http://snapshot-cache.ci.gardener.cloud/archive/debian/ -o/dev/null; then
-  echo "http://snapshot-cache.ci.gardener.cloud/archive/$archive/$t"
+# use caching proxy to avoid throttling, if available
+if wget -t1 -qO/dev/null http://localhost/archive/debian/; then
+	echo "http://localhost/archive/$archive/$t"
+elif wget -t1 -qO/dev/null http://172.17.0.1/archive/debian/; then	
+	echo "http://172.17.0.1/archive/$archive/$t"
+elif wget -t1 -qO/dev/null http://snapshot-cache.ci.gardener.cloud/archive/debian/; then
+	echo "http://snapshot-cache.ci.gardener.cloud/archive/$archive/$t"
 else
-  echo "http://snapshot.debian.org/archive/$archive/$t"
+	echo "http://snapshot.debian.org/archive/$archive/$t"
 fi
