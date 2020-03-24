e939ae356923e00ffadbb864c61feef81c5d26e6
Dirk Marwinski <dirk.marwinski@gmx.de>
2020-03-24 17:00:38 +0100

    Initial OpenStatck/VMware support

--- features/openstack/configure.sh
+++ features/openstack/configure.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash 
 
-# AWS has an own time server
-sed -i "s/^#NTP=/NTP=instance-data.ec2.internal/g" /etc/systemd/system.conf
-
 # growpart is done in initramfs, growroot by systemd
 mv /etc/cloud/cloud.cfg /etc/cloud/cloud.cfg.bak
 cat /etc/cloud/cloud.cfg.bak | grep -v "^ - growpart$" | grep -v "^ - resizefs$" | grep -v "^ - ntp$" >/etc/cloud/cloud.cfg  
-rm /etc/cloud/cloud.cfg.bak
-
-#systemctl enable cloud-init
+rm /etc/cloud/cloud.cfg.bak 
