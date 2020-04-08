e7678b8d51882a88999554cdac0436d5e8f2d6ad
Dirk Marwinski <dirk.marwinski@sap.com>
2020-04-08 07:57:12 +0200

    AWS DNS (default) does not support DNSSEC

--- features/aws/configure.sh
+++ features/aws/configure.sh
@@ -3,6 +3,9 @@
 # AWS has an own time server
 sed -i "s/^#NTP=/NTP=instance-data.ec2.internal/g" /etc/systemd/system.conf
 
+# AWS DNS does not support DNSSEC
+sed -i 's/^#DNSSEC=allow-downgrade/DNSSEC=false/g' /etc/systemd/resolved.conf
+
 # growpart is done in initramfs, growroot by systemd
 mv /etc/cloud/cloud.cfg /etc/cloud/cloud.cfg.bak
 cat /etc/cloud/cloud.cfg.bak | grep -v "^ - growpart$" | grep -v "^ - resizefs$" | grep -v "^ - ntp$" >/etc/cloud/cloud.cfg  
