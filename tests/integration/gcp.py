a33d0178683bef5f9f03ced228989dbdc3e6d157
Matthias Sohn <matthias.sohn@sap.com>
2020-04-09 17:10:13 +0200

    Finish implementation of first GCP integration tests
    - authenticate using configured service account credentials
    - create a compute instance using the configured image
    - wait until it is reachable
    - run integration tests
    - delete the compute instance

--- tests/integration/gcp.py
+++ tests/integration/gcp.py
@@ -1,9 +1,10 @@
-import datetime
 import json
 import logging
+import os
 import time
+from datetime import datetime
 from os import path
-from pprint import pformat
+from urllib.parse import urlparse
 
 import googleapiclient.discovery
 import google.oauth2.service_account
@@ -14,6 +15,8 @@ from .sshclient import RemoteClient
 
 
 logger = logging.getLogger(__name__)
+googleapiclient_logger = logging.getLogger("googleapiclient")
+googleapiclient_logger.setLevel(logging.ERROR)
 
 
 class GCP:
@@ -22,23 +25,20 @@ class GCP:
     @classmethod
     def fixture(cls, config):
         gcp = GCP(config)
-        instance = gcp.create_vm()
-        logger.info(f"{instance=}")
-        # ssh = None
-        # try:
-        #     ssh = RemoteClient(
-        #         host=instance.public_dns_name,
-        #         user=config["user"],
-        #         ssh_key_filepath=config["ssh_key_filepath"],
-        #         passphrase=config["passphrase"],
-        #         remote_path=config["remote_path"],
-        #     )
-        #     yield ssh
-        # finally:
-        #     if ssh is not None:
-        #         ssh.disconnect()
-        #     if gcp is not None:
-        #         gcp.__del__()
+        _, public_ip = gcp.create_vm()
+        ssh = None
+        try:
+            ssh = RemoteClient(
+                host=public_ip,
+                user=config["user"],
+                ssh_key_filepath=config["ssh_key_filepath"],
+                passphrase=config["passphrase"],
+                remote_path=config["remote_path"],
+            )
+            yield ssh
+        finally:
+            if ssh is not None:
+                ssh.disconnect()
 
     def __init__(self, config):
         """
@@ -48,14 +48,28 @@ class GCP:
         """
         self.config = config
         credentials: Credentials = self._auth(config)
-        self._compute : Resource = googleapiclient.discovery.build('compute', 'v1', credentials=credentials, cache_discovery=False)
-        self.project = config['project']
-        self.zone = config['zone']
+        self._compute: Resource = googleapiclient.discovery.build(
+            "compute", "v1", credentials=credentials, cache_discovery=False
+        )
+        self.project = config["project"]
+        self.zone = config["zone"]
         self.image_name = self.config["image_name"]
+        self.machine_type = self.config["machine_type"]
         self.ssh_key_filepath = path.expanduser(self.config["ssh_key_filepath"])
         self.user = self.config["user"]
 
+    def __del__(self):
+        """Cleanup resources held by this object"""
+        if self.instance:
+            self.delete_vm(self.instance)
+            self.instance = None
+
     def _auth(self, config) -> Credentials:
+        """Loads the authentication credentials given in the config
+
+        :param config: the config specifying path to service account json file
+                       or the service account credentials json
+        """
         service_account_json_path = path.expanduser(config["service_account_json_path"])
         if service_account_json_path:
             with open(service_account_json_path, "r") as f:
@@ -63,35 +77,50 @@ class GCP:
         if not service_account_json:
             service_account_json = config["service_account_json"]
         return google.oauth2.service_account.Credentials.from_service_account_info(
-            json.loads(service_account_json))
+            json.loads(service_account_json)
+        )
 
-    def __del__(self):
-        """Cleanup resources held by this object"""
+    def _get_image(self, project, image_name):
+        """Get image with given name
 
-    def get_image(self, project, image_name):
-        response = self._compute.images().get(
-            project=project,
-            image=image_name,
-        ).execute()
-        image = response.get('selfLink')
+        :param image_name: name of the image
+        """
+        response = (
+            self._compute.images().get(project=project, image=image_name,).execute()
+        )
+        image = response.get("selfLink")
         logger.info(f"{image=}")
         return image
 
-    def wait_for_operation(self, operation):
-        print(f"Waiting for operation {operation} to finish...")
+    def _wait_for_operation(self, operation):
+        """Wait for a GCP operation to finish"""
+        logger.info(f"Waiting for {operation} to finish ...")
         while True:
-            result = self._compute.zoneOperations().get(
-                project=self.project,
-                zone=self.zone,
-                operation=operation).execute()
-            logger.info(f"status={result['status']}")
-            if result['status'] == 'DONE':
-                print("### done.")
-                if 'error' in result:
-                    raise Exception(result['error'])
+            result = (
+                self._compute.zoneOperations()
+                .get(project=self.project, zone=self.zone, operation=operation)
+                .execute()
+            )
+            if result["status"] == "DONE":
+                logger.info("... done")
+                if "error" in result:
+                    raise Exception(result["error"])
                 return result
-
             time.sleep(1)
+
+    def _wait_until_reachable(self, hostname):
+        logger.info(f"Waiting for {hostname} to respond to ping ...")
+        while True:
+            response = os.system("ping -c 1 " + hostname)
+            if response == 0:
+                logger.info(f"... {hostname} is reachable, wait 20 sec more")
+                time.sleep(20)
+                return
+            time.sleep(1)
+
+    def _get_resource_name(self, url):
+        """Get resource name from GCP url"""
+        return urlparse(url).path.split("/")[-1]
 
     def create_vm(self):
         """
@@ -101,51 +130,65 @@ class GCP:
 
         :returns: instance to enable cleanup
         """
-        image = self.get_image(self.project, self.image_name)
+        image = self._get_image(self.project, self.image_name)
 
-        machine_type = f"zones/{self.zone}/machineTypes/n1-standard-1"
-        time = datetime.datetime.now().isoformat().replace(":", "-").split(".")[0].lower()
-        name = f"test-{self.image_name}-{time}"
-        with open(f"{self.ssh_key_filepath}.pub", "rb") as f:
-            public_key_bytes = f.read()
+        machine_type = f"zones/{self.zone}/machineTypes/{self.machine_type}"
+        time = datetime.now().strftime("%Y-%m-%dt%H-%M-%S")
+        name = f"gardenlinux-test-{self.image_name}-{time}"
         config = {
-            'name': name,
-            'machineType': machine_type,
-            'disks': [
+            "name": name,
+            "machineType": machine_type,
+            "disks": [
                 {
-                    'boot': True,
-                    'autoDelete': True,
-                    'initializeParams': {
-                        'sourceImage': image,
-                    }
+                    "boot": True,
+                    "autoDelete": True,
+                    "initializeParams": {
+                        "diskSizeGb": 3,
+                        "sourceImage": image,
+                    },
                 }
             ],
-            'networkInterfaces': [{
-                'network': 'global/networks/default',
-                'accessConfigs': [
-                    {'type': 'ONE_TO_ONE_NAT', 'name': 'External NAT'}
-                ]
-            }],
-
-            'metadata': {
-                'items': [{
-                    'key': 'block-project-ssh-keys',
-                    'value': False
-                }, {
-                    "key": "ssh-keys",
-                    "value": f"{self.user}:{public_key_bytes} {self.user}\n"
-                }]
-            }
+            "networkInterfaces": [
+                {
+                    "network": "global/networks/default",
+                    "accessConfigs": [
+                        {"type": "ONE_TO_ONE_NAT", "name": "External NAT"}
+                    ],
+                }
+            ],
         }
-        logger.info(pformat(f"{config=}"))
 
-        operation = self._compute.instances().insert(
-            project=self.project,
-            zone=self.zone,
-            body=config).execute()
-        logger.info(pformat(f"{operation=}"))
-        self.wait_for_operation(operation['name'])
+        operation = (
+            self._compute.instances()
+            .insert(project=self.project, zone=self.zone, body=config)
+            .execute()
+        )
+        self._wait_for_operation(operation["name"])
+        url = operation.get("targetLink")
+        self.name = self._get_resource_name(url)
+        list_result = (
+            self._compute.instances()
+            .list(project=self.project, zone=self.zone, filter=f"name = {self.name}")
+            .execute()
+        )
+        if len(list_result["items"]) != 1:
+            raise KeyError(f"unexpected number of items: {len(list_result['items'])}")
+        self.instance = list_result["items"][0]
+        interface = self.instance["networkInterfaces"][0]
+        access_config = interface["accessConfigs"][0]
+        self.public_ip = access_config["natIP"]
+        logger.info(f"created instance {url=} with {self.public_ip=}")
+        self._wait_until_reachable(self.public_ip)
+        return self.instance, self.public_ip
 
-        instance = operation.get('targetLink')
-        logger.info(pformat(f"{instance=}"))
-        return instance
+    def delete_vm(self, instance):
+        """ Delete the given ComputeEngine instance
+
+        :param instance: the instance to delete
+        """
+        operation = (
+            self._compute.instances()
+            .delete(project=self.project, zone=self.zone, instance=self.name)
+            .execute()
+        )
+        self._wait_for_operation(operation["name"])
