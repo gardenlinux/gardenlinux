042315b9ad76f32303c65441e5dc7508d207c84d
Christian Cwienk (d051236) <christian.cwienk@sap.com>
2020-04-21 14:12:53 +0200

    refactor ci-coding into separate modules

--- ci/glci/model.py
+++ ci/glci/model.py
@@ -1,14 +1,8 @@
-#!/usr/bin/env python3
-
-import dacite
 import dataclasses
 import datetime
 import dateutil.parser
 import enum
-import itertools
-import os
 import typing
-import yaml
 
 
 class Architecture(enum.Enum):
@@ -105,52 +99,3 @@ def gardenlinux_epoch(date:typing.Union[str, datetime.datetime]=None):
     if gardenlinux_epoch < 0:
         raise ValueError() # must not be older than gardenlinux' inception
     return gardenlinux_epoch
-
-
-def enumerate_build_flavours(build_yaml: str='build.yaml'):
-    with open(build_yaml) as f:
-        parsed = yaml.safe_load(f)
-
-    flavour_combinations = [
-        dacite.from_dict(
-            data_class=GardenlinuxFlavourCombination,
-            data=flavour_def,
-            config=dacite.Config(
-                cast=[Architecture, Platform, Extension, Modifier],
-            )
-        ) for flavour_def in parsed['flavours']
-    ]
-    for comb in flavour_combinations:
-        for arch, platf, exts, mods in itertools.product(
-            comb.architectures,
-            comb.platforms,
-            comb.extensions,
-            comb.modifiers,
-        ):
-            yield GardenlinuxFlavour(
-                architecture=arch,
-                platform=platf,
-                extensions=exts,
-                modifiers=mods,
-                fails=comb.fails, # not part of variant
-            )
-
-
-def print_build_flavours(version:str, build_yaml:str='build.yaml'):
-    for flavour in enumerate_build_flavours(build_yaml=build_yaml):
-        for fn in flavour.release_files(version=version):
-            print(fn)
-
-
-def version_path(version: str):
-    return f'version/{version}'
-
-
-def main():
-    version = '18-deadbeef'
-
-    print_build_flavours(version=version)
-
-
-if __name__ == '__main__':
-    main()
