f2e0904299f84673639e95d4b1f9476d06737a81
Matthias Sohn <matthias.sohn@sap.com>
2020-04-29 10:47:40 +0200

    Add option --iaas to select infrastructure where tests should run
    Now the infrastructure to run the tests on has to be passed
    to pytest via option --iaas. Possible values are currently
    "aws" and "gcp".
    
    Issue: #22

--- tests/__main__.py
+++ tests/__main__.py
@@ -4,6 +4,7 @@ import pytest
 pytest.main(
     [
         # "-x",   # stop on first failure
+        "--iaas=gcp",
         "--log-auto-indent=true",
         "--log-cli-level=INFO",
         "--log-level=INFO",
