25c4f1c99657158f8da1ee37284ffd40da29d488
Stefan Catargiu <stefan.catargiu@gmx.de>
2020-05-24 05:32:55 +0000

    remove ftparchive from apt-utils/py27 patch update

--- hack/libdb/apt.sh
+++ hack/libdb/apt.sh
@@ -8,21 +8,19 @@ fi
 
 docker run --rm \
 	--volume $(readlink -f packages):/packages \
-	--volume $(readlink -f py27-remove-libdb.diff):/patch.diff \
+	--volume $(readlink -f apt-remove-libdb.diff):/patch.diff \
 	-e DEBFULLNAME="GardenLinux Maintainers" \
 	-e DEBEMAIL="contact@gardenlinux.io" \
 	-ti gardenlinux:build \
         bash -c "
 		set -euo pipefail
 		sudo apt-get update
-		sudo apt-get build-dep -y --no-install-recommends python2.7
+		sudo apt-get build-dep -y --no-install-recommends apt-utils 
 		sudo apt-get install -y --no-install-recommends devscripts # required for debuild
-		sudo apt-get install -y libgdbm-compat-dev
-		apt-get source python2.7
-		cd python2.7-*
+		apt-get source apt-utils 
+		cd apt-*
 		patch -p1 < /patch.diff
-		dch -i 'remove _bsddb module'
-		sudo apt-get remove -y --purge libdb-dev libdb5.3-dev
+		dch -i 'remove ftparchive/libdb'
 		debuild -b -uc -us
 		sudo mv ../*.deb /packages
 	"
