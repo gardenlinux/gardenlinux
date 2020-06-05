1c62f0bcd4132554dd9543282a50bb1726f81dba
Kistner, Dominic <dominic.kistner@sap.com>
2020-06-05 21:06:21 +0200

    Prepare full automation of the Azure Marketplace process.

--- ci/glci/azure.py
+++ ci/glci/azure.py
@@ -9,6 +9,8 @@ from datetime import (
     timedelta,
 )
 
+from enum import Enum
+
 from msal import ConfidentialClientApplication
 
 from azure.storage.blob import (
@@ -89,8 +91,21 @@ class AzureImageStore:
         return f"https://{self.sa_name}.blob.core.windows.net/{self.container_name}/{image_name}?{container_sas}"
 
 
+class AzmpOperationState(Enum):
+    NOTSTARETD = "notStarted"
+    RUNNING = "running"
+    COMPLETED = "completed"
+    SUCCEEDED = "succeeded"
+    FAILED = "failed"
+
+class AzmpTransportDest(Enum):
+    STAGING = "staging"
+    PROD = "production"
+
 class AzureMarketplaceClient:
     """Azure Marketplace Client is a client to interact with the Azure Marketplace."""
+
+    marketplace_baseurl = "https://cloudpartner.azure.com/api/publishers"
 
     def __init__(self, spn_tenant_id: str, spn_client_id: str, spn_client_secret: str):
         app_client = ConfidentialClientApplication(
@@ -107,10 +122,15 @@ class AzureMarketplaceClient:
             "api-version": "2017-10-31"
         }
 
+    def _api_url(self, *parts):
+        url_parts = "/".join(p for p in parts)
+        return f"{self.marketplace_baseurl}/{url_parts}"
+
     def fetch_offer(self, publisher_id: str, offer_id: str):
         """Fetch an offer from Azure marketplace."""
+
         response = requests.get(
-            f"https://cloudpartner.azure.com/api/publishers/{publisher_id}/offers/{offer_id}",
+            self._api_url(publisher_id, "offers", offer_id),
             headers=self.req_headers,
             params=self.req_params
         )
@@ -125,12 +145,13 @@ class AzureMarketplaceClient:
 
     def update_offer(self, publisher_id: str, offer_id: str, spec: dict):
         """Update an offer with a give spec."""
+
         headers = self.req_headers.copy()
         headers.update({
             "If-Match": "*",
         })
         response = requests.put(
-            f"https://cloudpartner.azure.com/api/publishers/{publisher_id}/offers/{offer_id}",
+            self._api_url(publisher_id, "offers", offer_id),
             headers=headers,
             params=self.req_params,
             json=spec,
@@ -144,13 +165,14 @@ class AzureMarketplaceClient:
 
     def publish_offer(self, publisher_id: str, offer_id: str, notification_mails):
         """Trigger (re-)publishing of an offer."""
+
         data = {
             "metadata": {
                 "notification-emails": ",".join(notification_mails)
             }
         }
         response = requests.post(
-            f"https://cloudpartner.azure.com/api/publishers/{publisher_id}/offers/{offer_id}/publish",
+            self._api_url(publisher_id, "offers", offer_id, "publish"),
             headers=self.req_headers,
             params=self.req_params,
             json=data,
@@ -161,6 +183,51 @@ class AzureMarketplaceClient:
                 f"gardenlinux. statuscode={response.status_code}"
             )
         response.close()
+
+    def fetch_ongoing_operation_id(self, publisher_id: str, offer_id: str, transport_dest: AzmpTransportDest):
+        '''Fetches the id of an ongoing Azure Marketplace transport operation to a certain transport destination.'''
+
+        response = requests.get(
+            self._api_url(publisher_id, "offers", offer_id, "submissions"),
+            headers=self.req_headers,
+            params=self.req_params,
+        )
+        if not response.ok:
+            raise RuntimeError("Could not fetch Azure Marketplace transport operations for gardenlinux offer.")
+        operations = response.json()
+        response.close()
+        for operation in operations:
+            if AzmpTransportDest(operation["slot"]) == transport_dest and AzmpOperationState(operation["submissionState"]) == AzmpOperationState.RUNNING:
+                return operation["id"]
+        raise RuntimeError(f"Did not find an ongoing transport operation to ship gardenliunx offer on the Azure Marketplace.")
+
+    def fetch_operation_state(self, publisher_id: str, offer_id: str, operation_id: str):
+        """Fetches the state of a given Azure Marketplace transport operation."""
+
+        response = requests.get(
+            self._api_url(publisher_id, "offers", offer_id, "operations", operation_id),
+            headers=self.req_headers,
+            params=self.req_params,
+        )
+        if not response.ok:
+            raise RuntimeError(f"Can't fetch state for transport operation {operation_id}. statuscode={response.status_code}")
+        operation = response.json()
+        response.close()
+        return AzmpOperationState(operation['status'])
+
+    def go_live(self, publisher_id: str, offer_id: str):
+        """Trigger a go live operation to transport an Azure Marketplace offer to production."""
+
+        response = requests.post(
+            self._api_url(publisher_id, "offers", offer_id, "golive"),
+            headers=self.req_headers,
+            params=self.req_params,
+        )
+        if not response.ok:
+            raise RuntimeError(
+                    "Go live of updated gardenlinux Azure Marketplace offer failed."
+                    f"statuscode={response.status_code}"
+                )
 
 
 def add_image_version_to_plan(
@@ -290,6 +357,68 @@ def update_offer(
         offer_id=offer_id,
         notification_mails=notification_recipients,
     )
+
+    # TODO Persist the publish_operation_id to start polling the transport operation status to staging.
+    publish_operation_id = marketplace_client.fetch_ongoing_operation_id(
+        publisher_id,
+        offer_id,
+        AzmpTransportDest.STAGING,
+    )
+
+def check_offer_transport_state(
+    service_principal_config,
+    plan_config,
+    transport_dest: AzmpTransportDest,
+    operation_id: str
+):
+    """Checks the state of the gardenlinux Azure Marketplace offer transport
+
+    If the an transport operation has been failed some clean up action can be triggered.
+    In case the transport to staging enviroment has been succeeded then the transport
+    to production (go live) will be automatically triggered. If the transport has
+    been completed then some follow up action can be triggered.
+    """
+
+    marketplace_client = AzureMarketplaceClient(
+        service_principal_config.tenant_id(),
+        service_principal_config.client_id(),
+        service_principal_config.client_secret(),
+    )
+    publisher_id = plan_config.publisher_id()
+    offer_id = plan_config.offer_id()
+    operation_status = marketplace_client.fetch_operation_state(
+        publisher_id,
+        offer_id,
+        operation_id,
+    )
+
+    # Check first if the process has been failed...
+    if operation_status == AzmpOperationState.FAILED:
+        # TODO Trigger clean up of artifacts. Abort polling.
+        return
+
+    # Publish completed. Trigger go live to transport the offer changes to production.
+    if operation_status == AzmpOperationState.SUCCEEDED and transport_dest == AzmpTransportDest.STAGING:
+        print("Publishing of gardenlinux offer to staging has been successfully completed. Trigger go live...")
+        marketplace_client.go_live(
+            publisher_id,
+            offer_id,
+        )
+        # TODO Persist golive_operation_id and poll next time for transport operations to production.
+        golive_operation_id = marketplace_client.fetch_ongoing_operation_id(
+            publisher_id,
+            offer_id,
+            AzmpTransportDest.PROD,
+        )
+        return
+
+    # Go Live completed. Done! Trigger follow up actions.
+    if operation_status == AzmpOperationState.SUCCEEDED and transport_dest == AzmpTransportDest.PROD:
+        print("Tranport to production of gardenlinux offer succeeded.")
+        # TODO Trigger some follow up actions e.g. clean up of build artifacts.
+        return
+
+    print(f"Publishing of gardenlinux Azure Marketplace offer to {transport_dest.value} enviroment is still ongoing...")
 
 
 def copy_image_and_publish_offer(
