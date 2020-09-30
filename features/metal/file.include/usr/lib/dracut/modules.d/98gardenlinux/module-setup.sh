3104c7041fb8a81d038af169329055a94eb1ac8b
Andre Russ <andre.russ@sap.com>
2020-09-30 19:21:28 +0000

    fixing metal once and for good

---
+++ features/metal/file.include/usr/lib/dracut/modules.d/98gardenlinux/module-setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+check() {
+    [[ $mount_needs ]] && return 1
+    return 0
+}
+
+depends() {
+    echo "fs-lib"
+    echo "dracut-systemd"
+}
+
+install() {
+    inst_multiple grep sfdisk growpart udevadm awk mawk sed rm readlink
+   
+    # grow root
+    if [ -f "$moddir/growroot.sh" ]; then
+        inst_hook pre-mount 00 "$moddir/growroot.sh"
+    fi
+
+    # handle usr mounting
+    if [[ -f "$moddir/usr-mount.service" ]] && [[ -f "$moddir/usr-mount.sh" ]]; then
+        inst_simple "$moddir/usr-mount.service" ${systemdsystemunitdir}/usr-mount.service
+        inst_script "$moddir/usr-mount.sh" /bin/usr-mount.sh
+        systemctl -q --root "$initdir" add-wants initrd-root-fs.target usr-mount.service
+    fi
+}
