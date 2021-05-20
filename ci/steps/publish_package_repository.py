12110abb46b8d4a5fdcd5fa50267a40795ede322
Andreas Burger <andreas.burger@sap.com>
2021-05-20 11:15:07 +0000

    ci: refactor pkg-repo-publishing
    - use cicd-cfg to be more consistent with other ci-gear
    - rm most hardcoded variables

--- ci/steps/publish_package_repository.py
+++ ci/steps/publish_package_repository.py
@@ -6,10 +6,7 @@ import tempfile
 import ccc.aws
 import glci.s3
 
-AWS_CONFIG_NAME = 'gardenlinux'
 GARDENLINUX_DISTRIBUTION = 'bullseye'
-GARDENLINUX_PKGS_BUCKET_NAME = 'gardenlinux-pkgs'
-REPREPRO_BASE_DIR = '~/repo/gardenlinux'
 
 logger = logging.getLogger(__name__)
 
@@ -17,15 +14,15 @@ logger = logging.getLogger(__name__)
 # Not sure yet what to do with the inital reprepro update
 
 
-def setup():
-    config_dir = os.path.join(REPREPRO_BASE_DIR, 'conf')
+def setup(reprepro_base_dir: str):
+    config_dir = os.path.join(reprepro_base_dir, 'conf')
     os.makedirs(config_dir, exist_ok=False)
 
     with open(os.path.join(config_dir, 'distributions'), 'w') as f:
         f.write(
             'Origin: Gardenlinux-bullseye\n'
             'Label: Gardenlinux-bullseye\n'
-            'Codename: bullseye\n'
+            f'Codename: {GARDENLINUX_DISTRIBUTION}\n'
             'Architectures: amd64\n'
             'Components: gardenlinux\n'
             'Description: Apt repository for Gardenlinux\n'
@@ -49,52 +46,51 @@ def setup():
     #     )
 
 
-def main():
-
-    setup()
-
-    session = ccc.aws.session(aws_cfg=AWS_CONFIG_NAME)
+def build_package_repository(
+    cicd_cfg_name: str,
+):
+    cicd_cfg = glci.util.cicd_cfg(cfg_name=cicd_cfg_name)
+    aws_cfg_name = cicd_cfg.build.aws_cfg_name
+    session = ccc.aws.session(aws_cfg=aws_cfg_name)
     s3_resource = session.resource('s3')
+    s3_bucket_name = cicd_cfg.build.s3_bucket_name
 
-    def _files_with_extension(
-        dir: str,
-        extension: str,
-    ):
+    def _files_with_extension(dir, extension):
         for dirpath, _, filenames in os.walk(os.path.abspath(os.path.realpath(dir))):
             for filename in filenames:
                 _, ext = os.path.splitext(filename)
                 if ext and ext == extension:
                     yield os.path.join(dirpath, filename)
 
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        # download existing packages from s3 (put there by previous build-steps)
-        glci.s3.download_dir_from_s3(
-            s3_resource=s3_resource,
-            bucket_name=GARDENLINUX_PKGS_BUCKET_NAME,
-            s3_dir='packages/',
-            local_dir=tmp_dir,
-        )
-        # add all debian-packages to reprepo-repository
-        for file_path in _files_with_extension(tmp_dir, '.deb'):
-            logger.info(f"adding '{file_path}' to repository")
-            subprocess.run(
-                [
-                    'reprepro', '-b', REPREPRO_BASE_DIR,
-                    'includedeb', GARDENLINUX_DISTRIBUTION, file_path,
-                ],
-                capture_output=True,
-                check=True,
+    with tempfile.TemporaryDirectory() as reprepro_base_dir:
+
+        setup(reprepro_base_dir=reprepro_base_dir)
+
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            # download existing packages from s3 (put there by previous build-steps)
+            glci.s3.download_dir_from_s3(
+                s3_resource=s3_resource,
+                bucket_name=s3_bucket_name,
+                s3_dir='packages/',
+                local_dir=tmp_dir,
             )
+            # add all debian-packages to reprepo-repository
+            for file_path in _files_with_extension(tmp_dir, '.deb'):
+                logger.info(f"adding '{file_path}' to repository")
+                subprocess.run(
+                    [
+                        'reprepro', '-b', reprepro_base_dir,
+                        'includedeb', GARDENLINUX_DISTRIBUTION, file_path,
+                    ],
+                    capture_output=True,
+                    check=True,
+                )
 
-    logger.info('uploading to s3-bucket ... ')
-    for dir_name in ['pool', 'dists']:
-        glci.s3.upload_dir_to_s3(
-            s3_resource=s3_resource,
-            bucket_name=GARDENLINUX_PKGS_BUCKET_NAME,
-            src_dir_path=os.path.join(REPREPRO_BASE_DIR, dir_name),
-            dest_dir_path=dir_name,
-        )
-
-
-if __name__ == '__main__':
-    main()
+        logger.info('uploading to s3-bucket ... ')
+        for dir_name in ['pool', 'dists']:
+            glci.s3.upload_dir_to_s3(
+                s3_resource=s3_resource,
+                bucket_name=s3_bucket_name,
+                src_dir_path=os.path.join(reprepro_base_dir, dir_name),
+                dest_dir_path=dir_name,
+            )
