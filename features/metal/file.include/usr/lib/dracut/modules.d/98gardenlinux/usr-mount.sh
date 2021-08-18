3da40f4ffa9a9e843781aad8c28891b21d5e5634
Stefan Catargiu <stefan.catargiu@gmx.de>
2021-08-18 22:56:01 +0200

    update gardenlinux dracut module for the metal feature

--- features/metal/file.include/usr/lib/dracut/modules.d/98gardenlinux/usr-mount.sh
+++ features/metal/file.include/usr/lib/dracut/modules.d/98gardenlinux/usr-mount.sh
@@ -1,29 +1,12 @@
 #!/bin/sh
 
-#
-# FIXME
-#
 mountUnit="/sysroot/etc/systemd/system/usr.mount"
 
 FSTYPE=$(awk -F= '/^Type=/ { print $2}' "$mountUnit")
 DEVICE=$(awk -F= '/^What=/ { print $2}' "$mountUnit")
 OPTIONS=$(awk -F= '/^Options=/ { print $2}' "$mountUnit") 
 
-DEVNAME=$(systemd-escape -p "$DEVICE")
-# create the mount unit in ramdisk
-{
-echo "[Unit]"
-echo "Before=initrd-root-fs.target"
-echo "Requires=systemd-fsck@${DEVNAME}.service"
-echo "After=sysroot.mount"
-echo "After=systemd-fsck@${DEVNAME}.service"
-echo "After=blockdev@${DEVNAME}.target"
-echo "[Mount]"
-echo "Where=/sysroot/usr"
-echo "What=$DEVICE"
-echo "Options=$OPTIONS"
-echo "Type=$FSTYPE"
-} > /etc/systemd/system/sysroot-usr.mount
-
-mkdir -p /etc/systemd/system/initrd-fs.target.wants
-ln -s /etc/systemd/system/sysroot-usr.mount /etc/systemd/system/initrd-fs.target.wants/sysroot-usr.mount
+echo "FSTYPE=${FSTYPE}
+DEVICE=${DEVICE}
+OPTIONS=${OPTIONS}
+DEVNAME=${DEVNAME}" > /run/systemd/sysroot-usr.env
