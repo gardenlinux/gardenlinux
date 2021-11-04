8d80a5d1c34fc32d403f7231bce840fc6516696a
Andreas Burger <andreas.burger@sap.com>
2021-11-04 10:20:38 +0000

    ci: rm pipeline cfssl-coding

--- ci/steps.py
+++ ci/steps.py
@@ -152,35 +152,6 @@ def clone_step(
     return step, step_params
 
 
-def cfssl_clone_step(
-    name: str,
-    params: params.AllParams,
-    env_vars: typing.List[typing.Dict] = [],
-    volume_mounts: typing.List[typing.Dict] = [],
-):
-    step_params = [
-        params.cfssl_git_url,
-        params.cfssl_committish,
-        params.cfssl_dir,
-    ]
-    step = tkn.model.TaskStep(
-        name=name,
-        image=DEFAULT_IMAGE,
-        script=task_step_script(
-            script_type=ScriptType.PYTHON3,
-            path=os.path.join(steps_dir, 'clone_simple_step.py'),
-            callable="git_clone('$(params.cfssl_git_url)', '$(params.cfssl_committish)', "
-                     "'$(params.cfssl_dir)'); dummy",
-            params=[],
-            repo_path_param=params.repo_dir,
-        ),
-        volumeMounts=volume_mounts,
-        env=env_vars,
-    )
-
-    return step, step_params
-
-
 def upload_results_step(
     params: params.AllParams,
     env_vars: typing.List[typing.Dict] = [],
@@ -325,32 +296,6 @@ def release_step(
             callable='release_step',
             params=step_params,
             repo_path_param=params.repo_dir,
-        ),
-        volumeMounts=volume_mounts,
-        env=env_vars,
-    )
-    return step, step_params
-
-
-def build_cfssl_step(
-    params: params.AllParams,
-    env_vars: typing.List[typing.Dict] = [],
-    volume_mounts: typing.List[typing.Dict] = [],
-):
-    step_params = [
-        # !DO NOT CHANGE ORDER!
-        params.repo_dir,
-        params.cfssl_dir,
-    ]
-    step = tkn.model.TaskStep(
-        name='build-cfssl-step',
-        image='golang:latest',
-        script=task_step_script(
-            path=os.path.join(steps_dir, 'build_cfssl.sh'),
-            script_type=ScriptType.BOURNE_SHELL,
-            callable='build_cfssl',
-            repo_path_param=params.repo_dir,
-            params=step_params,
         ),
         volumeMounts=volume_mounts,
         env=env_vars,
