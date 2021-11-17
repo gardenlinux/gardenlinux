9bf538f0c9fd9a21b09c6c28fac654ec4b71401f
jensh007 <j.huebel@sap.com>
2021-11-17 14:45:32 +0100

    ci: make github release in release-step

--- ci/steps/release_step.py
+++ ci/steps/release_step.py
@@ -1,3 +1,4 @@
+import github_release
 import glci.model
 import release
 
@@ -5,10 +6,15 @@ parsable_to_int = str
 
 
 def release_step(
-    giturl: str,
-    committish: str,
-    gardenlinux_epoch: parsable_to_int,
     build_targets: str,
+    cicd_cfg_name: str,
+    committish: str,
+    ctx_repository_config_name: str,
+    flavourset: str,
+    gardenlinux_epoch: parsable_to_int,
+    giturl: str,
+    repo_dir: str,
+    version: str,
 ):
     build_target_set = glci.model.BuildTarget.set_from_str(build_targets)
 
@@ -16,10 +22,24 @@ def release_step(
       print(f'{build_target_set=} - will not perform release')
       return
 
+    print(f'Making release for {committish=}')
+
     release.ensure_target_branch_exists(
         release_branch=release.release_branch_name(gardenlinux_epoch=gardenlinux_epoch),
         release_committish=committish,
-        release_version=glci.model.next_release_version_from_workingtree(),
+        release_version=glci.model.next_release_version_from_workingtree(epoch=gardenlinux_epoch),
         git_helper=release._git_helper(giturl=giturl),
         giturl=giturl,
+    )
+
+    # Make the github release
+    github_release.make_release(
+        cicd_cfg_name=cicd_cfg_name,
+        committish=committish,
+        ctx_repository_config_name=ctx_repository_config_name,
+        flavourset_name=flavourset,
+        gardenlinux_epoch=gardenlinux_epoch,
+        giturl=giturl,
+        repo_dir=repo_dir,
+        version=version,
     )
