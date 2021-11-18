eea2374445ae7c2162eb37d84a66fadeb69441a4
jensh007 <j.huebel@sap.com>
2021-11-18 15:18:04 +0100

    ci: make ali test more robust

--- integration_tests/ali/ali_test.py
+++ integration_tests/ali/ali_test.py
@@ -7,13 +7,15 @@ from aliyunsdkecs.request.v20140526 import DescribeInstancesRequest
 def test_ali_instances(ali_client):
     region_id = ali_client.get_region_id()
     assert(region_id == 'eu-central-1')
-    
+
     request = DescribeInstancesRequest.DescribeInstancesRequest()
     request.set_PageSize(10)
 
     response = ali_client.do_action_with_exception(request)
     instances_dict = json.loads(response.decode())
     assert(len(instances_dict) > 0)
-    inst = instances_dict['Instances']['Instance'][0]
-    assert(inst)
-    assert(inst['RegionId'] == region_id)
+    if 'Instances' in instances_dict and 'Instance' in instances_dict['Instances'] \
+        and len(instances_dict['Instances']['Instance']) > 0:
+        inst = instances_dict['Instances']['Instance'][0]
+        assert(inst)
+        assert(inst['RegionId'] == region_id)
