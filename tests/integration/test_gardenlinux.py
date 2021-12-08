66580c3b4d89a34ef8832d51118dc86a13d71c4b
Thomas Buchner <thomas.buchner@sap.com>
2021-12-08 16:10:15 +0100

    test(aws): check for presence of ena driver

--- tests/integration/test_gardenlinux.py
+++ tests/integration/test_gardenlinux.py
@@ -389,3 +389,7 @@ def test_startup_script(client, gcp):
     (exit_code, output, error) = client.execute_command("test -f /tmp/startup-script-ok")
     assert exit_code == 0, f"no {error=} expected. Startup script did not run"
 
+def test_aws_ena_driver(client, aws):
+    (exit_code, output, error) = client.execute_command("ethtool -i $(ip -j link show  | jq -r '.[] | if .ifname != \"lo\" and .ifname != \"docker0\" then .ifname else empty end') | grep \"^driver\" | awk '{print $2}'")
+    assert exit_code == 0, f"no {error=} expected"
+    assert output.rstrip() == "ena", "Expected network interface to run with ena driver"
