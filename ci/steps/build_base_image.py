0307af134c59ddc21d3551140312d1188a34a49c
jensh007 <j.huebel@sap.com>
2021-12-17 17:18:44 +0100

    ci: freeze base images on later hotfix builds

--- ci/steps/build_base_image.py
+++ ci/steps/build_base_image.py
@@ -2,6 +2,8 @@ import glob
 import os
 
 import build_kaniko as builder
+import glci.model
+import version
 
 
 def build_base_image(
@@ -10,16 +12,32 @@ def build_base_image(
     repo_dir: str,
     oci_path: str,
     version_label: str,
+    build_targets: str,
 ):
+    additional_tags = ['latest']
+    build_target_set = glci.model.BuildTarget.set_from_str(build_targets)
+
     # check if build is configured to use an existing (older) base-image
     # if yes skip building base images otherwise build base images for commit
     targeted_build_deb_image = f'{oci_path}/gardenlinux-build-deb:{version_label}'
     targeted_build_image = f'{oci_path}/gardenlinux-build-image:{version_label}'
 
-    if gardenlinux_build_deb_image != targeted_build_deb_image and build_image != targeted_build_image:
+    if (gardenlinux_build_deb_image != targeted_build_deb_image and
+        build_image != targeted_build_image):
         print(f'Build will use (older) base images {gardenlinux_build_deb_image}, {build_image}'
             ' skip step.')
         return
+
+    # if we are in a release and have a .x version with x>0 do not build base image
+    # use the existing one.
+    parsed_version = version.parse_to_semver(version_label)
+    if parsed_version.minor > 0:
+        print(f'Skip building base images for versions with minor > 0: {version_label}')
+
+    if glci.model.BuildTarget.FREEZE_VERSION in build_target_set and parsed_version.minor == 0:
+        tag = f'rel-{parsed_version.major}'
+        print(f'Set additional tag for later minor releases: {tag}')
+        additional_tags.append(tag)
 
     dockerfile_relpath = os.path.join(repo_dir, "docker", "build-image", "Dockerfile")
     print(f'repo_dir is: {repo_dir}')
@@ -41,7 +59,6 @@ def build_base_image(
         else:
             build_base_image = 'debian:testing-slim'
 
-
         context_dir = os.path.join(repo_dir, "docker", docker_dir)
         print(f'---Using base image {build_base_image}')
         builder.build_and_push_kaniko(
@@ -49,6 +66,6 @@ def build_base_image(
             context_dir=context_dir,
             image_push_path=f'{oci_path}/gardenlinux-{docker_dir}',
             image_tag=version_label,
-            additional_tags=['latest'],
+            additional_tags=additional_tags,
             build_args=[f'build_base_image={build_base_image}']
         )
