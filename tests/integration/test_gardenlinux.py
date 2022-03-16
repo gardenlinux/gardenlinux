ae28ba3e933ab24ccd57a05760660dc3331841a4
Christoph Senkel <christoph.senkel@credativ.de>
2022-03-16 14:06:22 +0100

    integrations test still use docker

--- tests/integration/test_gardenlinux.py
+++ tests/integration/test_gardenlinux.py
@@ -269,7 +269,7 @@ def test_docker(client):
     if exit_code != 0:
         (journal_rc, output, error) = client.execute_command("sudo journactl --no-pager -xu docker.service")
     assert exit_code == 0, f"no {error=} expected"
-    (exit_code, output, error) = client.execute_command("sudo podman run --rm  eu.gcr.io/gardenlinux/gardenlinux:184.0 sh -c 'echo from container'")
+    (exit_code, output, error) = client.execute_command("sudo docker run --rm  eu.gcr.io/gardenlinux/gardenlinux:184.0 sh -c 'echo from container'")
     assert exit_code == 0, f"no {error=} expected"
     assert output == "from container\n", f"Expected 'from container' output but got {output}"
 
