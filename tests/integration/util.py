6794100a8f505408019fd307a85003ed10bc34e3
Thomas Buchner <thomas.buchner@sap.com>
2022-03-25 11:04:36 +0100

    test: prepare platform tests for pipeline integration (#724)
    * tests: pipeline enablement for AWS integration tests
    
    * tests: pipeline enablement for Azure integration tests
    
    * tests: pipeline enablement for GCP integration tests
    
    * fix: clean_oprhans should not crash when iterating EC2 instances
    
    * test(az): print out tags for tests
    
    * tests(gcp): code cleanup for gcp integration tests

--- tests/integration/util.py
+++ tests/integration/util.py
@@ -1,10 +1,8 @@
-import subprocess
 import logging
 import json
 import urllib.request
 
 import paramiko
-from googleapiclient.errors import HttpError
 
 logger = logging.getLogger(__name__)
 
@@ -21,40 +19,6 @@ def get_my_ip():
 def get_public_key(private_key_file):
     k = paramiko.RSAKey.from_private_key_file(private_key_file)
     return k.get_name() + " " + k.get_base64()
-
-# gcp related
-def delete_firewall_rule(compute, project, name):
-    try:
-        request = compute.firewalls().delete(project=project, firewall=name)
-        response = request.execute()
-        logger.info(response)
-        op_name = response['name']
-        logger.info(f'waiting for delete filewall rule {op_name=}')
-        wait_for_global_operation(compute, project, op_name)
-    except HttpError as h:
-        if h.resp.status != 404:
-            raise
-
-def ensure_firewall_rules(compute, project, restfw):
-    name = restfw["name"]
-    delete_firewall_rule(compute, project, name)
-
-    request = compute.firewalls().insert(project=project, body=restfw)
-    response = request.execute()
-    logger.info(response)
-    op_name = response['name']
-    logger.info(f'waiting for create filewall rule {op_name=}')
-    wait_for_global_operation(compute, project, op_name)
-
-
-def wait_for_global_operation(compute, project, operation):
-    response = compute.globalOperations().wait(project=project, operation=operation,).execute()
-    if response["status"] != "DONE":
-        logger.error("Operation failed %s" % json.dumps(response, indent=4))
-        error = ""
-        if "error" in response:
-            error = response["error"]
-        raise Exception("Operation %s failed: %s" % (operation, error))
 
 def get_config_value(config, key):
     if key in config and config[key] != "":
