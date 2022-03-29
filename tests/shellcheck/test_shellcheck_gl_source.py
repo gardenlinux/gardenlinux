fa097c75611d434b31a59efa17b3d19c96504bea
Vincent Riesop <v.riesop@sap.com>
2022-03-29 09:36:40 +0000

    test(shellcheck): add skip comment feature
    * add "# skip-shellcheck" in second line of bash scripts that
    should be skipped during shellcheck

--- tests/shellcheck/test_shellcheck_gl_source.py
+++ tests/shellcheck/test_shellcheck_gl_source.py
@@ -7,6 +7,7 @@ from subprocess import PIPE, run
 
 logger = logging.getLogger(__name__)
 
+SKIP_COMMENT = "skip-shellcheck"
 
 def is_bash_script(filepath):
     try:
@@ -18,20 +19,36 @@ def is_bash_script(filepath):
         return True
     return False
 
+def has_skip_comment(filepath):
+    fp = open(filepath)
+    ret = False
+    for i, line in enumerate(fp):
+        if i == 1:
+            logger.info(line)
+            if SKIP_COMMENT in line:
+                ret = True
+                break
+        if i > 1:
+            break;
+
+    fp.close()
+    return ret
+
 def get_files_to_check(directory):
     all_files = Path(directory).glob('**/*')
     ret = list()
     for filename in all_files:
         if os.path.isfile(filename) and is_bash_script(filename):
-            ret.append(filename)
+            if not has_skip_comment(filename):
+                ret.append(filename)
     return ret
 
-
-FILES_TO_CHECK = get_files_to_check('../')
+FILES_TO_CHECK = get_files_to_check('../features/metal')
 
 @pytest.mark.parametrize('filepath', FILES_TO_CHECK)
 def test_shellcheck_on_file(filepath):
     command = ['shellcheck','--severity=warning' , filepath]
     result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-    assert result.returncode==0, f"Shellcheck failed for file {filepath} \n {result.stderr} {result.stdout} "
+    assert result.returncode==0, f"Shellcheck failed for: {filepath} \n {result.stderr} {result.stdout} "
+    #assert result.returncode==0, f"Shellcheck failed for: {filepath}"
 
