112cb5965634ba503f63b67ece0ca0467898402e
Florian Paul Hoberg <florian.hoberg@credativ.de>
2022-03-31 07:57:38 +0000

    Adjust cis test

--- tests/integration/test_cis.py
+++ tests/integration/test_cis.py
@@ -6,7 +6,7 @@ import yaml
 from .sshclient import RemoteClient
 
 LYNIS_VER = "3.0.7"
-
+LYNIS_REPORT = "/var/log/lynis-report.dat"
 logger = logging.getLogger(__name__)
 
 def test_download_lynis(client):
@@ -14,10 +14,32 @@ def test_download_lynis(client):
     assert exit_code == 0, f"no {error=} expected"
 
 def test_unarchive_lynis(client):
-    (exit_code, output, error) = client.execute_command("tar xfvz /tmp/lynis.tar.gz")
+    (exit_code, output, error) = client.execute_command("tar xfvz /tmp/lynis.tar.gz -C /tmp/")
     assert exit_code == 0, f"no {error=} expected"
 
 def test_lynis_cis(client):
     # Unfortunately we need to change to the
     # lynis dir before we may execute lynis
-    (exit_code, output, error) = client.execute_command("cd /root/lynis/ && /root/lynis/lynis audit system")
+    (exit_code, output, error) = client.execute_command("cd /tmp/lynis/ && /usr/bin/sh /tmp/lynis/lynis --no-log audit system > /tmp/lynis.output")
+    assert exit_code == 0, f"no {error=} expected"
+
+def test_lynis_output(client):
+    (exit_code, output, error) = client.execute_command("cat /tmp/lynis.output")
+    validation = False
+    if "Great, no warnings" in output:
+        validation = True
+    assert validation
+
+def test_lynis_log_warn(client):
+    (exit_code, output, error) = client.execute_command(f"cat {LYNIS_REPORT}")
+    validation = False
+    if not "warning[]" in output:
+        validation = True
+    assert validation
+
+def test_lynis_log_crit(client):
+    (exit_code, output, error) = client.execute_command(f"cat {LYNIS_REPORT}")
+    validation = False
+    if not "critical[]" in output:
+        validation = True
+    assert validation
