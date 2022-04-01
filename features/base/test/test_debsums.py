fa1567f417770cd41f8c8f369e9d1e1705c4a45c
Christoph Senkel <christoph.senkel@credativ.de>
2022-04-01 15:29:52 +0200

    add debsums test and check for disabled tests

--- features/base/test/test_debsums.py
+++ features/base/test/test_debsums.py
@@ -1,10 +1,10 @@
 from helper.exception import NotPartOfFeatureError, DisabledBy
-from helper.tests.blacklisted_packages import BlacklistedPackages
+from helper.tests.debsums import Debsums
 import pytest
 
-def test_blacklisted_packages(client, features):
+def test_debsums(client, features):
     """The test function executed by pytest"""
     try:
-        BlacklistedPackages(client, features)
+        Debsums(client, features)
     except (NotPartOfFeatureError, DisabledBy) as e:
         pytest.skip(str(e))
--- tests/helper/tests/debsums.py
+++ tests/helper/tests/debsums.py
@@ -6,8 +6,8 @@ from helper.exception import NotPartOfFeatureError, TestFailed, DisabledBy
 logger = logging.getLogger(__name__)
 
 
-class Capabilities():
-    """Class containing the test for checking the file capabilities"""
+class Debsums():
+    """Class containing the test for checking if debsums are available"""
     failed_before = False
     def __new__(cls, client, features):
         """The actual test.
@@ -26,7 +26,7 @@ class Capabilities():
         (enabledfeatures, myfeature) = features
 
         # check if test is disabled in a feature
-        test_is_disabled = utils.is_disabled(enabledfeatures, 'capabilities')
+        test_is_disabled = utils.is_disabled(enabledfeatures, 'debsums')
         if not len(test_is_disabled) == 0:
             raise DisabledBy("Test is explicitly disabled by features %s" % (', '.join(test_is_disabled)))
 
@@ -36,27 +36,32 @@ class Capabilities():
             # first check if there is already an instance of this class, if it is the first time this instance is initiated
             # add the class variable instance containing the instance itself and then do the actual testing. 
             if not hasattr(cls, 'instance'):
-                cls.instance = super(Capabilities, cls).__new__(cls)
-                (exit_code, output, error) = client.execute_command("find /boot /etc /usr /var -type f -exec getcap {} \\;")
+                cls.instance = super(Debsums, cls).__new__(cls)
+
+                utils.AptUpdate(client)                
+                (exit_code, output, error) = client.execute_command("apt-get install -y --no-install-recommends debsums apt-utils")
                 assert exit_code == 0, f"no {error=} expected"
 
-                capabilities = utils.read_test_config(enabledfeatures, 'capabilities')
+                debsums_exclude = utils.read_test_config(enabledfeatures, 'debsums', '_exclude.list')
 
-                cap_found = []
-                cap_notfound = []
-                for line in output.splitlines():
-                    if line not in capabilities:
-                        cap_notfound.append(line)
-                    if line in capabilities:
-                        cap_found.append(line)
+                (exit_code, output, error) = client.execute_command("debsums -l")
+                assert exit_code == 0, f"no {error=} expected"
 
-                if not len(cap_found) == len(capabilities):
+                if not output == '':
                     cls.failed_before = True
-                    raise TestFailed("Found capabilities do not match expected capabilities. Found: %s Expected: %s" % (', '.join(cap_found), ', '.join(capabilities)))
+                    raise TestFailed("the following packages don't have md5sums: %s" % (output))
 
-                if not len(cap_notfound) == 0:
+                (exit_code, output, error) = client.execute_command("debsums -sc")
+                assert exit_code == 0 or exit_code == 2, f"no {error=} expected"
+                
+                changed = []
+                for line in error.splitlines():
+                    if line.split()[3] not in debsums_exclude:
+                        changed.append(line.split()[3] + " " + line.split()[4] + " " + line.split()[5] + " " + line.split()[6])
+
+                if not len(changed) == 0:
                     cls.failed_before = True
-                    raise TestFailed("Capabilities %s are not in expected capabilities list" % (', '.join(cap_notfound)))
+                    raise TestFailed("the following files have changes: %s " % (', '.join(changed)))
 
         else:
             raise NotPartOfFeatureError("Feature %s this test belongs to is not enabled" % (myfeature))
