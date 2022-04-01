909ba54bb6a94d2b374c739f947a834f70f4fc1e
Christoph Senkel <christoph.senkel@credativ.de>
2022-04-01 10:24:19 +0200

    add comments to features pytest

--- features/base/test/test_capabilities.py
+++ features/base/test/test_capabilities.py
@@ -3,6 +3,7 @@ from helper.tests.capabilities import Capabilities
 import pytest
 
 def test_capabilities(client, features):
+    """The test function executed by pytest"""
     try:
         Capabilities(client, features)
     except NotPartOfFeatureError as e:
--- features/base/test/test_packages_musthave.py
+++ features/base/test/test_packages_musthave.py
@@ -4,6 +4,7 @@ from helper.tests.packages_musthave import PackagesMusthave
 import pytest
 
 def test_packages_musthave(client, features):
+    """The test function executed by pytest"""
     try:
         PackagesMusthave(client, features)
     except NotPartOfFeatureError as e:
