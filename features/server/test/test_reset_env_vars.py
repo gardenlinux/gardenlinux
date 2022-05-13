9e5eff2dc3ada55d0f8b971134b6ee18fecaa858
Christoph Senkel <christoph.senkel@credativ.de>
2022-05-13 09:26:28 +0200

    smplify existing pytest reset_env_vars

--- features/server/test/test_reset_env_vars.py
+++ features/server/test/test_reset_env_vars.py
@@ -1,10 +1,1 @@
-from helper.exception import NotPartOfFeatureError, DisabledBy
-from helper.tests.reset_env_vars import ResetEnvVars
-import pytest
-
-def test_reset_env_vars(client, features):
-    """The test function executed by pytest"""
-    try:
-        ResetEnvVars(client, features)
-    except (NotPartOfFeatureError, DisabledBy) as e:
-        pytest.skip(str(e))
+from helper.tests.reset_env_vars import reset_env_vars as test_reset_env_vars
