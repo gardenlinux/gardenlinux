4e519fc07b2a58fb8f40867b978540369eba0db0
Christoph Senkel <christoph.senkel@credativ.de>
2022-05-16 15:06:44 +0200

    use parametrize

--- features/server/test/test_reset_env_vars.py
+++ features/server/test/test_reset_env_vars.py
@@ -1,5 +1,12 @@
 from helper.tests.key_val_in_file import key_val_in_file
+import pytest
 
-def test_reset_env_vars(client):
-    key_val_in_file(client, "/etc/sudoers", {"Defaults": "env_reset"},
-                    invert=False, ignore_missing=False)
+@pytest.mark.parametrize(
+    "file,args",
+    [
+        ("/etc/sudoers", {"Defaults": "env_reset"})
+    ]
+)
+
+def test_reset_env_vars(client, file, args):
+    key_val_in_file(client, file, args, invert=False, ignore_missing=False)
