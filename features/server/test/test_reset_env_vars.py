55426fbbe72d60dddb719dbbeb3034b4d4a669c7
Christoph Senkel <christoph.senkel@credativ.de>
2022-05-16 14:55:13 +0200

    fix test options

--- features/server/test/test_reset_env_vars.py
+++ features/server/test/test_reset_env_vars.py
@@ -2,4 +2,4 @@ from helper.tests.key_val_in_file import key_val_in_file
 
 def test_reset_env_vars(client):
     key_val_in_file(client, "/etc/sudoers", {"Defaults": "env_reset"},
-                    invert=True, ignore_missing=True)
+                    invert=False, ignore_missing=False)
--- tests/helper/tests/reset_env_vars.py
+++
@@ -1,6 +0,0 @@
-def reset_env_vars(client):
-    """Test for checking if sudoers defaults set env_reset"""
-    (exit_code, _, _) = client.execute_command(
-        f"grep '^Defaults.env_reset' /etc/sudoers", quiet=True)
-
-    assert exit_code == 0, "For sudo the environment variables are not reset"
