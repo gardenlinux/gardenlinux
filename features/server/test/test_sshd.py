55441b2d63934804882f6a2cd97c074af4d291a4
Christoph Senkel <christoph.senkel@credativ.de>
2022-05-16 10:45:56 +0200

    define expected as a string and not as tuple

--- features/server/test/test_sshd.py
+++ features/server/test/test_sshd.py
@@ -5,23 +5,23 @@ import pytest
     "expected",
     [
         # Supported HostKey algorithms.
-        ("HostKey", "/etc/ssh/ssh_host_ed25519_key"),
-        ("HostKey", "/etc/ssh/ssh_host_rsa_key"),
+        "HostKey /etc/ssh/ssh_host_ed25519_key",
+        "HostKey /etc/ssh/ssh_host_rsa_key",
         #"HostKey /etc/ssh/ssh_host_ecdsa_key",
-        ("KexAlgorithms", "sntrup761x25519-sha512@openssh.com,curve25519-sha256,curve25519-sha256@libssh.org,ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group-exchange-sha256,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group14-sha256"),
+        "KexAlgorithms sntrup761x25519-sha512@openssh.com,curve25519-sha256,curve25519-sha256@libssh.org,ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group-exchange-sha256,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group14-sha256",
 
-        ("Ciphers", "chacha20-poly1305@openssh.com,aes128-ctr,aes192-ctr,aes256-ctr,aes128-gcm@openssh.com,aes256-gcm@openssh.com"),
+        "Ciphers chacha20-poly1305@openssh.com,aes128-ctr,aes192-ctr,aes256-ctr,aes128-gcm@openssh.com,aes256-gcm@openssh.com",
         
-        ("MACs", "umac-64-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,hmac-sha1-etm@openssh.com,umac-64@openssh.com,umac-128@openssh.com,hmac-sha2-256,hmac-sha2-512,hmac-sha1"),
+        "MACs umac-64-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,hmac-sha1-etm@openssh.com,umac-64@openssh.com,umac-128@openssh.com,hmac-sha2-256,hmac-sha2-512,hmac-sha1",
         
         # Password based logins are disabled - only public key based logins are allowed.
-        ("AuthenticationMethods", "publickey"),
+        "AuthenticationMethods publickey",
         
         # LogLevel VERBOSE logs user's key fingerprint on login. Needed to have a clear audit track of which key was using to log in.
-        ("LogLevel", "VERBOSE"),
+        "LogLevel VERBOSE",
         
         # Log sftp level file access (read/write/etc.) that would not be easily logged otherwise - be aware that the path has to be adapted to the Distribution installed.
-        ("Subsystem", "sftp /usr/lib/openssh/sftp-server -f AUTHPRIV -l INFO"),
+        "Subsystem sftp /usr/lib/openssh/sftp-server -f AUTHPRIV -l INFO",
         
         # Root login is not allowed for auditing reasons. This is because it's difficult to track which process belongs to which root user:
         #
@@ -29,18 +29,18 @@ import pytest
         # Additionally, only tools such as systemd and auditd record the process session id.
         # On other OSes, the user session id is not necessarily recorded at all kernel-side.
         # Using regular users in combination with /bin/su or /usr/bin/sudo ensure a clear audit track.
-        ("PermitRootLogin", "No"),
+        "PermitRootLogin No",
         
         # Use kernel sandbox mechanisms where possible in unprivileged processes
         # Systrace on OpenBSD, Seccomp on Linux, seatbelt on MacOSX/Darwin, rlimit elsewhere.
-        #("UsePrivilegeSeparation", "sandbox"),
+        #"UsePrivilegeSeparation sandbox",
         
         # Disable Tunneling and Forwarding
         # AllowTcpForwarding no # required for Gardener
-        ("X11Forwarding", "no"),
+        "X11Forwarding no",
         
         #use PAM
-        ("UsePAM", "yes")
+        "UsePAM yes"
     ]
 )
 
--- tests/helper/tests/sshd.py
+++ tests/helper/tests/sshd.py
@@ -2,8 +2,8 @@ import re
 
 def sshd(client, expected):
     """Tests if a sshd option is set to an expected value. The expected
-    option value pair is a tuple, the sshd_config is conterted into a list 
-    of tuples"""
+    option value string and the sshd_config are conterted into lists of 
+    tuples"""
 
     (exit_code, output, error) = client.execute_command("sshd -T",
                                                         quiet=True)
@@ -11,9 +11,9 @@ def sshd(client, expected):
 
     sshd_config = _create_list_of_tuples(output)
 
-    expected = (expected[0].lower(), _normalize_value(expected[1]))
+    expected = _create_list_of_tuples(expected)
 
-    assert expected in sshd_config, f"{expected} not found in sshd_config"
+    assert expected[0] in sshd_config, f"{expected} not found in sshd_config"
 
 
 def _create_list_of_tuples(input):
@@ -21,17 +21,16 @@ def _create_list_of_tuples(input):
     as a tuple. The 1st value of the tuple is the ssh option, the 2nd is
     value"""
     out = []
-    for line in input.splitlines():
+    for line in input.lower().splitlines():
         l = line.split(' ', 1)
         out.append((l[0], _normalize_value(l[1])))
     return out
 
 def _normalize_value(string):
     """Convert a given string.
-    All elements will be converted to lower case and the string will be
-    returned as a set. If the element contains a comma separated string,
-    it will be split into a list first."""
-    normalized = string.lower().split(" ")
+    The string will be returned as a set. If the element contains a comma
+    separated string, it will be split into a list first."""
+    normalized = string.split(" ")
     if len(normalized) == 1 and re.match(r".*,.*",normalized[0]):
         value_as_set = set(normalized[0].split(','))
     else:
