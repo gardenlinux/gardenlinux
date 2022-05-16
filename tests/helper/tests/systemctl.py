36f474b8fa95895d784473631c8d098c93bfb556
Christoph Senkel <christoph.senkel@credativ.de>
2022-05-16 11:35:57 +0200

    replace shell command with python

--- tests/helper/tests/systemctl.py
+++ tests/helper/tests/systemctl.py
@@ -1,23 +1,20 @@
 def systemctl(client, state, services):
     """Test to check if systemd services are enabled or disabled as desired"""
+    (exit_code, output, error) = client.execute_command(
+            "systemctl list-unit-files", quiet=True)
+    assert exit_code == 0, f"no {error=} expected"
+
+    enabled = _return_services(output, ["enabled", "static"])
+    disabled = _return_services(output, ["disabled"])
+
     # get list of enabled systemd services
     if state == "enabled":
-        (exit_code, output, error) = client.execute_command(
-            "systemctl list-unit-files | awk '$2~/static/ { print $1; " +
-            "next} $2~/enabled/ { print $1; next; }'", quiet=True)
-        assert exit_code == 0, f"no {error=} expected"
-        enabled = output
         missing_enabled = _check_missing(enabled, services)
         assert len(missing_enabled) == 0, (f"{', '.join(missing_enabled)} are " +
                 "not enabled as expected")
 
     # get list of disabled systemd services
     if state == "disabled":
-        (exit_code, output, error) = client.execute_command(
-            "systemctl list-unit-files | awk '$2~/disabled/ { print $1; " +
-            "next}'", quiet=True)
-        assert exit_code == 0, f"no {error=} expected"
-        disabled = output
         missing_disabled = _check_missing(disabled, services)
         assert len(missing_disabled) == 0, (f"{', '.join(missing_disabled)} are " +
                 "not disabled as expected")
@@ -31,3 +28,11 @@ def _check_missing(configured, expected):
         if not item in configured:
             missing.append(item)
     return missing
+
+def _return_services(services, states):
+    out =[]
+    for service in services.splitlines():
+        for state in states:
+            if state in service:
+                out.append(service.split(' ', 1)[0])
+    return out
