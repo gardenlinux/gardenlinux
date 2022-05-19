295af0a7805acffcce8e072637440e83bfdaff04
Christoph Senkel <christoph.senkel@credativ.de>
2022-05-19 17:18:25 +0200

    reenable capabilities positive and negativ test

--- features/_pxe/test/test_capabilities.py
+++ features/_pxe/test/test_capabilities.py
@@ -1,14 +1,1 @@
-from helper.tests.capabilities import capabilities
-import pytest
-
-@pytest.mark.parametrize(
-    "capability_list",
-    [
-        [
-            "/usr/bin/arping cap_net_raw=ep",
-            "/usr/bin/ping cap_net_raw=ep"
-        ]
-    ]
-)
-def test_capabilities(client, capability_list):
-    capabilities(client, capability_list)
+from helper.tests.capabilities import capabilities as test_capabilities
--- tests/helper/tests/capabilities.py
+++ tests/helper/tests/capabilities.py
@@ -1,17 +1,30 @@
-import logging
+from helper.utils import read_test_config
+import string
 
-logger = logging.getLogger(__name__)
-
-def capabilities(client, capabilities):
+def capabilities(client, testconfig):
     """ Test if only the defined capabilities are set"""
     (exit_code, output, error) = client.execute_command(
         "find /boot /etc /usr /var -type f -exec getcap {} \\;", quiet=True)
     assert exit_code == 0, f"no {error=} expected"
 
+    # get capabilities.list config from enabled features
+    features = testconfig["features"]
+    capabilities = read_test_config(features, "capabilities")
+    # and remove duplicates
+    capabilities = list(set(capabilities))
+
+    cap_found = []
     cap_notfound = []
     for line in output.splitlines():
+        line.strip(string.whitespace)
         if line not in capabilities:
             cap_notfound.append(line)
+        if line in capabilities:
+            cap_found.append(line)
+
+    assert len(cap_found) == len(capabilities), ("Found capabilities " +
+        "do not match expected capabilities. Found: " +
+        f"{', '.join(cap_found)} Expected: {', '.join(capabilities)}")
 
     assert len(cap_notfound) == 0, ("Capabilities " +
         f"{', '.join(cap_notfound)} are not in expected capabilities list")
