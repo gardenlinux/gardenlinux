b840ac135e2629bdddfc57f487352c99f0e33e45
Christoph Senkel <christoph.senkel@credativ.de>
2022-05-25 16:45:38 +0200

    simplify existing pytest password hashes

--- features/base/test/test_password_hashes.py
+++ features/base/test/test_password_hashes.py
@@ -1,10 +1,1 @@
-from helper.exception import NotPartOfFeatureError, DisabledBy
-from helper.tests.password_hashes import PasswordHashes
-import pytest
-
-def test_password_hashes(client, features):
-    """The test function executed by pytest"""
-    try:
-        PasswordHashes(client, features)
-    except (NotPartOfFeatureError, DisabledBy) as e:
-        pytest.skip(str(e))
+from helper.tests.password_hashes import password_hashes as test_password_hashes
