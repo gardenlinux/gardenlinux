1031437793989edd562ce01c0cf0e97e8459ec8e
Christoph Senkel <53080514+chrinorse@users.noreply.github.com>
2022-05-25 16:21:53 +0200

    simplify existing pytest debsums (#953)
    * simplify existing pytest debsums

--- features/base/test/test_debsums.py
+++ features/base/test/test_debsums.py
@@ -1,10 +1,15 @@
-from helper.exception import NotPartOfFeatureError, DisabledBy
-from helper.tests.debsums import Debsums
+from helper.tests.debsums import debsums
 import pytest
 
-def test_debsums(client, features):
-    """The test function executed by pytest"""
-    try:
-        Debsums(client, features)
-    except (NotPartOfFeatureError, DisabledBy) as e:
-        pytest.skip(str(e))
+@pytest.mark.parametrize(
+    "exclude",
+    [
+        [
+            "/lib/systemd/system/dbus.socket",
+            "/usr/share/pam-configs/cracklib"
+        ]
+    ]
+)
+
+def test_debsums(client, exclude):
+    debsums(client, exclude)
