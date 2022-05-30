62743cbc3d05c29bca305f0a3a47d6f1992b5e64
Christoph Senkel <christoph.senkel@credativ.de>
2022-05-30 09:24:21 +0200

    simplify existing pytest kernel config

--- features/base/test/test_kernel_config.py
+++ features/base/test/test_kernel_config.py
@@ -1,10 +1,1 @@
-from helper.exception import NotPartOfFeatureError, DisabledBy
-from helper.tests.kernel_config import KernelConfig
-import pytest
-
-def test_kernel_config(client, features):
-    """The test function executed by pytest"""
-    try:
-        KernelConfig(client, features)
-    except (NotPartOfFeatureError, DisabledBy) as e:
-        pytest.skip(str(e))
+from helper.tests.kernel_config import kernel_config as test_kernel_config
