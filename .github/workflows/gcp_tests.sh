aed52eccc85614b8354d80d71509bf7d8257e88b
Vincent Riesop <vincent.riesop@gmail.com>
2022-08-01 14:53:17 +0200

    use variable

--- .github/workflows/gcp_tests.sh
+++ .github/workflows/gcp_tests.sh
@@ -28,13 +28,12 @@ credFileName=$(find "`pwd`" -maxdepth 1 -type f -name "gha-creds-*.json" | xargs
 
 echo "### Start Integration Tests for gcp"
 sudo podman run -it --rm  -v `pwd`:/gardenlinux -v `pwd`/.build/:/build $containerName /bin/bash -s <<EOF
-ls
 mkdir /gardenlinux/tmp
 TMPDIR=/gardenlinux/tmp
 cd /gardenlinux
-ls
-gcloud auth application-default login --cred-file="/gardenlinux/$credFileName" || exit 1
-gcloud config set project ${gcp_project}
+export GOOGLE_APPLICATION_CREDENTIALS="/gardenlinux/$credFileName"
 pytest --iaas=gcp --configfile=/gardenlinux/$configFile || exit 1
 EOF
 
+#gcloud auth application-default login --client-id-file="/gardenlinux/$credFileName" || exit 1
+#gcloud config set project ${gcp_project}
