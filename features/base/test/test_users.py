5e6b08ce00c1531b0d53beb9d5e666737560afa3
Thomas Buchner <thomas.buchner@sap.com>
2022-09-01 15:03:17 +0200

    test: allow additional users on cloud platforms

--- features/base/test/test_users.py
+++ features/base/test/test_users.py
@@ -1,4 +1,4 @@
 from helper.tests.users import users
 
-def test_users(client, non_dev, non_feature_github_action_runner, non_vhost):
+def test_users(client, non_dev, non_feature_github_action_runner, non_vhost, non_aws, non_azure, non_ali, non_gcp):
      users(client)
