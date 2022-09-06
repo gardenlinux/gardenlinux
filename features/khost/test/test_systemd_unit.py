ab0fe73b5350d79a982e5c8096658e21b3c6c478
Florian Paul Azim Hoberg <florian.hoberg@credativ.de>
2022-09-06 09:28:07 +0200

    tests(khost): Add unit test for feature `khost`

---
+++ features/khost/test/test_systemd_unit.py
@@ -0,0 +1,16 @@
+import pytest
+from helper.utils import execute_remote_command
+from helper.utils import validate_systemd_unit
+
+
+@pytest.mark.parametrize(
+    "systemd_unit",
+    [
+        "kubelet"
+    ]
+)
+
+
+def test_systemd_unit(client, systemd_unit, non_chroot):
+    execute_remote_command(client, f"systemctl start {systemd_unit}")
+    validate_systemd_unit(client, f"{systemd_unit}")
