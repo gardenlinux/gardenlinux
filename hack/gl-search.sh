a98ec44a15e012bacf41920595d58a51cf9ddc7e
Vincent Riesop <v.riesop@sap.com>
2023-05-26 17:11:27 +0200

    hack(gl-search): fix bash subshell function export
    In bash it is possible to export functions with export -f my_cool_function.
    This is not possible on osx.
    
    This commit puts the functions in a separate file, which can be sourced on demand.

--- hack/gl-search.sh
+++ hack/gl-search.sh
@@ -12,6 +12,10 @@
 # - rdepends: (EXPERIMENTAL) find what package in garden linux depends on this package
 # - download: downloads the selected deb package
 
+THIS_DIR="$(dirname "$(readlink -f "$BASH_SOURCE")")"
+# shellcheck source=/dev/null
+source "${THIS_DIR}/.gl-search-functions.sh"
+
 gl_selected_action="$(echo -e "search\ndep-check\nrdepends\ndownload" | fzf --header 'Select Action' )"
 gls_gl_dist="$(echo "today" |fzf --header 'Enter the Garden Linux Version you are interested in, or select today' --print-query | tail -1)"
 export gls_gl_dist
@@ -44,75 +48,12 @@ curl -s "$base_url/dists/${gls_gl_dist}/main/binary-${gls_selected_arch}/Package
 
 export packages_file
 
-function filter_package_info() {
-  pkg=$1;
-  sed -n "/Package: $pkg$/,/^$/p" "$packages_file"
-}
-
-function get_dependencies(){
-  pkg=$1;
-  sed -n "/Package: $pkg$/,/^$/p" "$packages_file" | grep "^Depends:" | sed -e "s/^Depends://" | sed -e "s/(.*)//" | sed -e "s/|/,/" | sed -r "s/,/ /g" 
-}
-
-function does_pkg_exist(){
-  pkg=$1;
-
-  sed -n "/Package: $pkg$/,/^$/p" "$packages_file" | grep -q "Package"  
-}
-
-function dependency_search(){
-  dependencies="$(get_dependencies "$1"  | sed -r "s/,/ /g")"
-
-  for dep in ${dependencies}
-  do
-    if does_pkg_exist "$dep"; then
-      echo "Covered Dependency: $dep"
-    else 
-      echo "Foreign Dependency: $dep"
-    fi
-  done
-}
-
-function get_packages(){
-    grep "^Package: " "$packages_file"
-}
-
-
-function rdepends_package(){
-  pkg="$1"
-  gardenlinux_packages="$(get_packages "$pkg")"
-  # check all garden linux packages
-  for chk_pkg in ${gardenlinux_packages} 
-  do
-    if get_dependencies "$chk_pkg" | grep -q "$pkg"; then
-      echo "Reverse Dependency: ${chk_pkg}"
-    fi
-  done
-  
-}
-
-function get_filename(){
-  pkg="$1"
-  gardenlinux_packages="$(get_packages "$pkg")"
-  # check all garden linux packages
-  filename=$(sed -n "/Package: $pkg$/,/^$/p" "$packages_file" | grep "Filename:" | cut -d':' -f 2)
-  echo "$filename" | xargs
-   
-}
-# fzf preview requires function to be available in spawned subshell
-export -f filter_package_info
-export -f dependency_search
-export -f get_dependencies
-export -f does_pkg_exist
-export -f rdepends_package
-export -f get_packages
-export -f get_filename
 case "$gl_selected_action" in
   "dep-check")
     grep "^Package:.*" "$packages_file" |  cut -d' ' -f 2 | \
       fzf \
       --multi \
-      --preview "bash -c \"packages_file=$packages_file dependency_search {1}\"" \
+      --preview "bash -c \"source ${THIS_DIR}/.gl-search-functions.sh; packages_file=$packages_file dependency_search {1}\"" \
       --header 'Select Garden Linux Package to see details on the right window. You can type to filter.'
     ;;
   "download")
@@ -129,14 +70,14 @@ case "$gl_selected_action" in
     grep "^Package:.*" "$packages_file" | cut -d' ' -f 2 | \
       fzf \
       --multi \
-      --preview "bash -c \"packages_file=$packages_file filter_package_info {1}\""\
+      --preview "bash -c \"source ${THIS_DIR}/.gl-search-functions.sh; packages_file=$packages_file filter_package_info {1}\""\
       --header 'Select Garden Linux Package to see details on the right window. You can type to filter.'
     ;;
   "rdepends")
     grep "^Package:.*" "$packages_file" | cut -d' ' -f 2 | \
       fzf \
       --multi \
-      --preview "bash -c \"packages_file=$packages_file rdepends_package {1}\"" \
+      --preview "bash -c \"source ${THIS_DIR}/.gl-search-functions.sh; packages_file=$packages_file rdepends_package {1}\"" \
       --header 'Select Garden Linux Package to see details on the right window. You can type to filter.'
     ;;
   *)
