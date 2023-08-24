6328af94242b10d4c87b3b3f25733a3078a311b5
Vincent Riesop <v.riesop@sap.com>
2023-08-24 09:57:05 +0200

    fix(test): remove redundant test_sgid_suid_files test in vhost
    * base provides the exact same test_sgid_suid_files test as vhost does
    * the diff only shows that base test skips vhost, so that vhost will execute otherwise the exact same test

--- features/base/test/test_sgid_suid_files.py
+++ features/base/test/test_sgid_suid_files.py
@@ -36,5 +36,5 @@ import pytest
 
 # Run the test unit to perform the
 # final tests by the given artifact.
-def test_sgid_suid_files(client, test_type, whitelist_files, non_vhost):
+def test_sgid_suid_files(client, test_type, whitelist_files):
     sgid_suid_files(client, test_type, whitelist_files)
