54746efe725c50ca42bc7e6f0b7188a98f7195c6
Christoph Senkel <christoph.senkel@credativ.de>
2023-10-24 11:44:17 +0200

    add github workflow for testing the release note generation

---
+++ .github/workflows/release-test.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+set -Eeufo pipefail
+
+token="$1"; shift
+repo="$1"; shift
+
+curl_path="$(which curl)"
+
+function curl {
+	$curl_path -f -u "token:$token" "$@"
+}
+
+function get {
+	[ $# = 1 ]
+	>&2 /usr/bin/echo curl -X GET "https://api.github.com/repos/$repo/$1"
+	curl -X GET "https://api.github.com/repos/$repo/$1"
+}
+
+function post {
+	[ $# = 2 ]
+	>&2 /usr/bin/echo curl -X POST "https://api.github.com/repos/$repo/$1" --data "$2"
+}
+
+function delete {
+	[ $# = 1 ]
+	>&2 /usr/bin/echo curl -X DELETE "https://api.github.com/repos/$repo/$1"
+}
+
+function upload {
+	[ $# = 1 ]
+	>&2 /usr/bin/echo curl -X POST -H "Content-Type: application/octet-stream" "https://uploads.github.com/repos/$repo/$1" --data-binary @-
+}
+
+action="$1"; shift
+
+case "$action" in
+	"create")
+		tag="$1"; shift
+		commit="$1"; shift
+		name="$1"; shift
+		body="$(.github/workflows/generate_release_note.py generate --version "$name" --commitish "$commit")"
+		#body="Placeholder."
+		#>&2 /usr/bin/echo '---'
+		#>&2 /usr/bin/echo $body
+		#>&2 /usr/bin/echo '---'
+		release="$(get "releases/tags/$tag" | jq -r '.id' || true)"
+		[ ! "$release" ] || delete "releases/$release"
+
+		release="$(post "releases" '{
+			"tag_name": "'"$tag"'",
+			"target_commitish": "'"$commit"'",
+			"name": "'"$name"'",
+			"body": "'"$body"'",
+			"prerelease": true
+		}' | jq -r '.id')"
+
+		>&2 echo "$release"
+
+		;;
+	"upload")
+		release="$1"; shift
+
+		while read asset_file; do
+			asset_name="$(basename "$asset_file")"
+			upload "releases/$release/assets?name=$asset_name" < "$asset_file" > /dev/null
+			>&2 echo "uploaded $asset_file to $release"
+		done
+
+		;;
+esac
