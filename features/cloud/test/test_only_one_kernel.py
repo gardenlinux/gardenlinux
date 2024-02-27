f58d2db6a76e786f2de8e82e150265a585cd2e49
Vincent Riesop <v.riesop@sap.com>
2024-02-27 14:35:18 +0100

    Update features/cloud/test/test_only_one_kernel.py
    Co-authored-by: Florian Wilhelm <2292245+fwilhe@users.noreply.github.com>

--- features/cloud/test/test_only_one_kernel.py
+++ features/cloud/test/test_only_one_kernel.py
@@ -4,6 +4,6 @@ import helper.utils as utils
 def test_number_installed_kernels(client):
     """ Only one kernel should be installed """
     installed_kernels = utils.get_installed_kernel_versions(client)
-    nr_kernels = len(installed_kernels)
-    assert nr_kernels == 1, f"Number of installed kernels {nr_kernels} != 1"
+    number_of_kernels = len(installed_kernels)
+    assert number_of_kernels == 1, f"Number of installed kernels {nr_kernels} != 1"
 
