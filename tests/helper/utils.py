1387a7101da5ff3b5f1996f4d8d9080e48bf7ba5
Vincent Riesop <vincent.riesop@gmail.com>
2024-02-29 10:06:49 +0100

    refactor: add rational, arch and key/value tuple

--- tests/helper/utils.py
+++ tests/helper/utils.py
@@ -238,7 +238,7 @@ def install_package_deb(client, pkg):
         f"apt-get install -y --no-install-recommends {pkg}", quiet=True)
     assert exit_code == 0, f"Could not install Debian Package: {error}"
 
-def check_kernel_config_exact(client, kernel_config_path, kernel_config_item ):
+def check_kernel_config_exact(client, kernel_config_path, kernel_config_item):
     """ Checks if the given kernel_config_item is set in kernel_config_path """
 
     assert check_file(client, kernel_config_path), f"Kernel config does not exist - {kernel_config_path}"
