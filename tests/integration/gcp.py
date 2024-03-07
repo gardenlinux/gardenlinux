59f24ba1c35d44e802971c234be8459a1328883a
Thomas Buchner <thomas.buchner@sap.com>
2024-03-07 10:08:50 +0100

    Revert "tests: add GVNIC and VIRTIO_SCSI_MULTIQUEUE to GCP tests (#2002)" (#2014)
    This reverts commit e3c6eda9845c64836b3063317d4d5490a7be760b because of errors that appeared with GCP platform tests after this PR got merged. Needs more investigation.

--- tests/integration/gcp.py
+++ tests/integration/gcp.py
@@ -411,10 +411,8 @@ class GCP:
             'labels': self._tags,
         }
 
-        guest_os_features = "GVNIC,VIRTIO_SCSI_MULTIQUEUE"
         if self.config['uefi'] or self.config['secureboot']:
-            guest_os_features += ",UEFI_COMPATIBLE"
-        config['guest_os_features'] = [{'type': guest_os_features}]
+            config['guest_os_features'] = [{'type_': "UEFI_COMPATIBLE"}]
 
         if self.config['secureboot']:
             cert_file_type = self.config['secureboot_parameters']['cert_file_type']
