149b9a5bcba15c9091e3c0a7e8ec55f8a5766d58
Vincent Riesop <v.riesop@sap.com>
2024-04-17 08:47:29 +0200

    tests: add kernel config tests (#1983)
    * tests: add kernel config tests
    
    Tests for cloud and metal feature. Check if
    SGX configs are enabled and check if MAGIC_SYSRQ config is disabled
    
    MAGIC_SYSRQ must be disabled for security audit reasons
    
    * remove old unused kernel config test
    
    * add exact test
    
    * refactor: add rational, arch and key/value tuple
    
    * also change test_kernel_config of metal

--- features/base/test/test_kernel_config.py
+++ features/base/test/test_kernel_config.py
@@ -3,30 +3,6 @@ from helper.sshclient import RemoteClient
 from helper.tests.file_content import file_content
 
 
-@pytest.mark.parametrize(
-    "args",
-    [
-        (   {
-            # example expected kernel config options:
-            #"CONFIG_INIT_ENV_ARG_LIMIT": "32",
-            #"CONFIG_COMPILE_TEST": "is not set",
-            #"CONFIG_WERROR": "n",
-            #"CONFIG_LOCALVERSION": "",
-            #"CONFIG_LOCALVERSION_AUTO": "is not set",
-            #"CONFIG_HAVE_KERNEL_GZIP": "y"
-            }
-        )
-    ]
-)
-
-def test_kernel_config(client, args, non_container):
-    """compare kernel config options from the parametrize section with the
-    kernel config options in the /boot/config-* file. The values 'is not set'
-    and 'n' are treated as the same."""
-    file = "/boot/config-*"
-    file_content(client, file, args, ignore_comments=True)
-
-
 def test_nvme_kernel_parameter(client, aws):
     """ Test for NVME kernel params """
     (exit_code, output, error) = client.execute_command("grep -c nvme_core.io_timeout=4294967295 /proc/cmdline")
