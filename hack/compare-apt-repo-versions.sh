0110fdc3cb771404fa3bb0014982a9836ec3e36d
Florian Wilhelm <2292245+fwilhe@users.noreply.github.com>
2024-08-06 14:17:43 +0200

    Add script to compare apt repo versions (#2246)
    Helpful script to investigate differences in repo versions between different Garden Linux versions.
    
    ---------
    
    Co-authored-by: Vincent Riesop <v.riesop@sap.com>

---
+++ hack/compare-apt-repo-versions.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -o errexit
+
+SCRIPT_NAME="${0##*/}"
+readonly SCRIPT_NAME
+
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+readonly SCRIPT_DIR
+
+usage() {
+    echo "Script to compare the apt repo version of two Garden Linux releases."
+    echo "Usage: $SCRIPT_NAME VERSION_A VERSION_B"
+    echo "Example: $SCRIPT_NAME 1443.10 1587.0"
+    exit 1
+}
+
+main() {
+    [[ $# -ge 2 ]] || usage
+    [[ -n "$1" ]] || usage
+    [[ -n "$2" ]] || usage
+    local VERSION_A="${1}"; shift
+    local VERSION_B="${1}"; shift
+
+    TEMP_DIR=$(mktemp -d)
+
+    trap 'rm -rf $TEMP_DIR' EXIT
+
+    curl -s https://packages.gardenlinux.io/gardenlinux/dists/"$VERSION_A"/main/binary-amd64/Packages.gz | gunzip > "$TEMP_DIR"/"$VERSION_A"
+    curl -s https://packages.gardenlinux.io/gardenlinux/dists/"$VERSION_B"/main/binary-amd64/Packages.gz | gunzip > "$TEMP_DIR"/"$VERSION_B"
+
+    python3 "$SCRIPT_DIR"/parse-aptsource.py "$TEMP_DIR"/"$VERSION_A" > "$TEMP_DIR"/a
+    python3 "$SCRIPT_DIR"/parse-aptsource.py "$TEMP_DIR"/"$VERSION_B" > "$TEMP_DIR"/b
+
+    diff -U 0 --minimal  "$TEMP_DIR"/a "$TEMP_DIR"/b | grep -v '^@'
+}
+
+main "${@}"
+
