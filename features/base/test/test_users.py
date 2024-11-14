dd43550433a57d23a2cf18b34d16f32eadcdaa29
akendo <akendo@akendo.eu>
2024-11-14 16:17:29 +0100

    Put a number into it

--- features/base/test/test_users.py
+++ features/base/test/test_users.py
@@ -1,5 +1,5 @@
 from helper.tests.users import users
 
-@pytest.mark.security_id(007)
+@pytest.mark.security_id(1)
 def test_users(client, non_dev, non_feature_githubActionRunner, non_vhost, non_hyperscalers, non_container, non_ccee):
      users(client)
