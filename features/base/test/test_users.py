0097b549eab72037a5ae1f07ec17526586b362f0
Akendo <akendo@akendo.eu>
2024-11-14 18:55:50 +0100

    Import pytest for marking

--- features/base/test/test_users.py
+++ features/base/test/test_users.py
@@ -1,4 +1,5 @@
 from helper.tests.users import users
+import pytest
 
 @pytest.mark.security_id(1)
 def test_users(client, non_dev, non_feature_githubActionRunner, non_vhost, non_hyperscalers, non_container, non_ccee):
