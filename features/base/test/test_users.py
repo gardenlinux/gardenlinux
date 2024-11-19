df445683c4d2d4b854a5dbcba0364de68522f56d
Akendo <akendo@akendo.eu>
2024-11-19 15:34:53 +0100

    Add gl-sec-id 179 test, fix id test and correct docu

--- features/base/test/test_users.py
+++ features/base/test/test_users.py
@@ -13,13 +13,13 @@ def test_for_root(client):
      passwd.remove("")
 
      # passwd format, we only care for the frist and third entry.
-     # 1. login name
-     # 2. optional encrypted password
-     # 3. numerical user ID
-     # 4. numerical group ID
-     # 5. user name or comment field
-     # 6. user home directory
-     # 7. optional user command interpreter
+     # 0. login name
+     # 1. optional encrypted password
+     # 2. numerical user ID
+     # 3. numerical group ID
+     # 4. user name or comment field
+     # 5. user home directory
+     # 6. optional user command interpreter
 
      login_name = 0
      numeric_user_id = 2
@@ -27,11 +27,12 @@ def test_for_root(client):
      # test if we have only a single user with root and id 0.
      entries_with_root_name = [entry for entry in passwd if 'root' in
                                entry.split(":")[login_name]]
-     entries_with_root_id   = [entry for entry in passwd if 'root' in
+     entries_with_root_id   = [entry for entry in passwd if '0' ==
                                entry.split(":")[numeric_user_id]]
      assert len(entries_with_root_name), 1
      assert len(entries_with_root_id), 1
 
 
+@pytest.mark.security_id(179)
 def test_users(client, non_dev, non_feature_githubActionRunner, non_vhost, non_hyperscalers, non_container, non_ccee):
      users(client)
