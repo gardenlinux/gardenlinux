766fb9819d6b8ba621385e296ca41f4af0c517db
Florian Wilhelm <florian.wilhelm02@sap.com>
2024-12-03 17:11:34 +0100

    Remove mentions of and config for GRUB
    As GRUB is not part of Garden Linux for some time, those are leftovers.
    
    Intentionally left out examples/ignition/install.yaml and features/cis/test/check_scripts/* for now.

--- features/vmware/test/test_check_files.py
+++ features/vmware/test/test_check_files.py
@@ -7,7 +7,6 @@ import pytest
     [
         "/usr/bin/dscheck_VMwareGuestInfo",
         "/usr/lib/python3/dist-packages/cloudinit/sources/DataSourceVMwareGuestInfo.py",
-        "/etc/default/grub.cfg",
         "/etc/cloud/cloud.cfg.d/01_debian-cloud.cfg",
         "/etc/cloud/cloud.cfg.d/99_disable-network-config.cfg",
         "/etc/cloud/cloud.cfg.d/99_enabled-datasources.cfg"
