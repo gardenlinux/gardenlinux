f0a7f5ef0f92071e7ae9eaf5bea94e29cf041678
Eike Waldt <waldt@b1-systems.de>
2024-12-05 16:32:39 +0100

    use cert/gardenlinux-secureboot.db.crt for azure secureboot

--- tests/platformSetup/tofu/modules/azure/main.tf
+++ tests/platformSetup/tofu/modules/azure/main.tf
@@ -152,7 +152,7 @@ resource "azurerm_shared_image_version" "shared_image_version" {
           certificate_data = replace(
             replace(
               replace(
-                join("", split("\n", file("cert/gardenlinux-secureboot.db.pem"))),
+                join("", split("\n", file("cert/gardenlinux-secureboot.db.crt"))),
                 "-----BEGIN CERTIFICATE-----",
                 ""
               ),
