77766752affb837127832f928cfd3c0ff4207f7e
Akendo <akendo@akendo.eu>
2024-12-07 01:09:34 +0100

    add addtional test for security_test 168

--- features/base/test/test_password_shadow.py
+++ features/base/test/test_password_shadow.py
@@ -1,4 +1,5 @@
 from helper.tests.password_shadow import password_shadow
+from helper.utils import execute_remote_command
 import pytest
 
 
@@ -6,3 +7,5 @@ import pytest
 @pytest.mark.security_id(170)
 def test_password_shadow(client):
     password_shadow(client)
+    # We're missing some of the default users and that's why pwck will raises an error message?
+    assert '' == execute_remote_command(client, "pwck")
