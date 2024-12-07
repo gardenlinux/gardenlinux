b885aa08e16eca05cd605e22415f528d9e286fef
Akendo <akendo@akendo.eu>
2024-12-08 00:12:08 +0100

    add addtional test for security_test 166

---
+++ features/base/test/test_su.py
@@ -0,0 +1,15 @@
+import pytest
+import pdb
+from helper.utils import read_file_remote
+
+@pytest.mark.security_id (166)
+def test_su_restriction(client):
+    """Validate that we have access to su restircted by default."""
+    pam_configuration  = read_file_remote(client, "/etc/pam.d/su", remove_comments=True)
+    auth_config = [config for config in pam_configuration if 'auth' in config]
+    for entry in auth_config:
+        if 'pam_wheel.so' in entry:
+            assert 'required pam_wheel.so' in entry, "Error configuration for the pam_wheel.so"
+            test_successfully = True
+    if not test_successfully:
+      assert False, "Error pam_wheel.so not found!"
