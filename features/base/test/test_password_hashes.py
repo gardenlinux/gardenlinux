8abba7a10d0067911ae79a2b8325469e0f7529c5
Akendo <akendo@akendo.eu>
2024-12-09 16:06:48 +0100

    add addtional test for security_test 325

--- features/base/test/test_password_hashes.py
+++ features/base/test/test_password_hashes.py
@@ -1,1 +1,7 @@
-from helper.tests.password_hashes import password_hashes as test_password_hashes
+from helper.tests.password_hashes import password_hashes 
+import pytest 
+
+
+@pytest.mark.security_id(325)
+def test_password_hashes(client):
+    password_hashes(client)
