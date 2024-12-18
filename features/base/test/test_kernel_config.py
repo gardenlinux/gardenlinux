d7ce35342df92a13c2cab06f9a1b33f8a2c411c2
Akendo <akendo@akendo.eu>
2024-12-18 14:29:24 +0100

    Fix for arm64

--- features/base/test/test_kernel_config.py
+++ features/base/test/test_kernel_config.py
@@ -1,5 +1,5 @@
 import pytest
-from helper.utils import execute_remote_command
+from helper.utils import execute_remote_command, get_architecture
 from helper.sshclient import RemoteClient
 from helper.tests.file_content import file_content
 
@@ -14,15 +14,29 @@ def test_nvme_kernel_parameter(client, aws):
 def test_for_kernel_address_space_layout_randomization(client):
     """
        We have to ensure that we have aslr enabled.
-       Also, we ensure that we do not have a regression as we
-       have seen it with the CVE-2024-6387 where a mistake combian
-       with a regression causes a serias issue. 
+
+    """
+
+    randomize_va_space = check_for_kernel_setting(client, "kernel.randomize_va_space")
+    assert randomize_va_space == "2", "ASLR wasn't set!"
+
+
+
+def test_for_regression_in_kernel_address_space_layout_randomization(client):
+    """
+       Also, we ensure that we do not have a regression, as we 
+       have seen it with the CVE-2024-6387 where a mistake in SSH 
+       in combination with weak or no ALSR caused a series vulnerability.
 
        This was done with the help of the blog post from Justin Miller.
        https://zolutal.github.io/aslrnt/
     """
-
-    check_for_kernel_setting()
+    arch = get_architecture(client)
+    match arch:
+        case "arm64":
+            expected_value = 0xffffffff0000
+        case "amd64":
+            expected_value = 0x7ffffffff000
 
     result = 0x0
     for _ in range(0,1000):
@@ -31,7 +45,7 @@ def test_for_kernel_address_space_layout_randomization(client):
         base_address = int(output.split('-')[0], 16)
         result |= base_address
 
-    assert 0x7ffffffff000 == result, "ALSR regression detected!"
+    assert expected_value == result, "ASLR regression detected!"
 
     result = 0x0
     for _ in range(0,1000):
@@ -39,14 +53,18 @@ def test_for_kernel_address_space_layout_randomization(client):
         output = execute_remote_command(client, "cat /proc/self/maps | grep ld | head -n1")
         base_address = int(output.split('-')[0], 16)
         result |= base_address
-    assert 0x7ffffffff000 == result, "ALSR regression detected!"
+    assert expected_value == result, "ASLR regression detected!"
 
 
 
-def check_for_kernel_setting():
+def check_for_kernel_setting(client, sysctl_variable):
     """
        This will first check for the running configuration and 
        then for a written configuration. Only when there is 
 
+       1. check the running configuration for the given parameter
+       2. check for a sysctl related cofiguration file.
+
     """
-    pass
+    output = execute_remote_command(client, f"sysctl {sysctl_variable}")
+    return output.split("=")[1].strip()
