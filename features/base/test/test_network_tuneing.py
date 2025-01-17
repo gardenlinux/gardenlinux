e8ca28b8e907a17f42f8d0c23b6b177d52ff016d
Akendo <akendo@akendo.eu>
2025-01-17 13:09:34 +0100

    add test for security_test 1121

---
+++ features/base/test/test_network_tuneing.py
@@ -0,0 +1,83 @@
+import pytest
+from helper.utils import check_for_kernel_setting
+  
+
+@pytest.mark.security_id(1121)
+def test_ipv4(client, non_firecracker, non_chroot, non_container, non_gcp):
+  """
+     Test that we have IPv4 network settings to best options..
+
+     net.ipv4.ip_forward
+     Ensure that we can forward IPv4 packages. By default this is enabled.
+     Defaul value is 1.
+
+     tcp_max_syn_backlog 
+     This is tracking open connections and will scale with the memory usage of the given Linux box.
+     We set this to a lower set since is the time window between an open connection is establied,
+     but not acknowledged. 
+     Defaul value is 1.
+
+     tcp_syncookies
+     Prevent against the 'SYN flood attack', but considered a  fallback feature. Hence this could
+     be disabled.
+     Use tcp_max_syn_backlog, tcp_synack_retries or tcp_abort_on_overflow instead. 
+     Defaul value is 1.
+
+     icmp_echo_ignore_broadcasts
+     Ignore nay ICMP broadcasts to avoid participating in Smurf attacks, since we're not in a
+     conventional LAN there is no use-case for boradcasts ping. 
+     Defaul value is 1.
+
+     icmp_ignore_bogus_error_responses
+     Some routes are nasty and sending bad icmp packages. By default the kernel would log this. We
+     disalbe this. 
+     Default value is 1.
+
+     accept_source_route
+     This is like a wildcard. This allows process of packages with a source list of IPs it want to
+     be routed with. However, this might allow an attacker to redirect network traffic. 
+     Our default setting is 0.
+
+     default.accept_source_route
+     Just like the parameter before, however, for the container uscase we need to enalbe it since
+     every newer interface for a vm or container will direct this.  This is mandatory for a
+     container setup since the different container want to have the ability to define it's routing
+     via source list. 
+     Our default setting is 1.
+
+     rp_filter
+     This is like a wildcard.  The  is for reverse path filtering. Necessary for container systems.
+     This way a package remains router-able. We can have this to 0 for non testing, which is our
+     default.  1 for strict testing or 2 for losely testing. 
+     Default is 0.
+
+     default.rp_filter
+     Set the setting for unnamed devices that yet has to show. See the rp_filter for more insights.
+     THis should share the same setting as before. 
+     Default is 0.
+     
+
+  """
+  list_of_ipv4_controls = [
+     ("net.ipv4.net.ipv4.ip_forward"               ,'1'),
+     ("net.ipv4.tcp_max_syn_backlog"            ,'2048'),
+     ("net.ipv4.tcp_syncookies"                    ,'1'),
+     ("net.ipv4.icmp_echo_ignore_broadcasts"       ,'1'),
+     ("net.ipv4.icmp_ignore_bogus_error_responses" ,'1'),
+     ("net.ipv4.conf.all.accept_source_route"      ,"0"),
+     ("net.ipv4.conf.default.accept_source_route"  ,"1"),
+     ("net.ipv4.conf.all.rp_filter"                ,"0"),
+     ("net.ipv4.conf.default.rp_filter"            ,"0")]
+
+
+  for control in list_of_ipv4_controls:
+    control_value = check_for_kernel_setting(client, control[0])
+    assert control[1] == control_value, f"Control {control[0]} does not have the right value. It's {control_value} and should be {control[1]}"
+  
+
+#def test_ipv4_forward(client, gcp, non_firecracker, non_chroot, non_container):
+#  """
+#      Test that we have IPv4 forwardning disabled. This is only the case of GCP.
+#      Most likely this isn't necesary.
+#  """
+#  assert DISABLE == check_for_kernel_setting(client, "net.ipv4.ip_forward"), "IPv4 forward is enabled where it shoudn't."
