30a4220adaf48040b54a3545b0c386b05a8f52b7
akendo <akendo@akendo.eu>
2025-02-06 15:49:27 +0100

    Add tests to see if pwck and grpck are okay.

--- features/base/test/test_password_shadow.py
+++ features/base/test/test_password_shadow.py
@@ -1,1 +1,18 @@
-from helper.tests.password_shadow import password_shadow as test_password_shadow
+from helper.tests.password_shadow import password_shadow
+from helper.utils import execute_remote_command
+import pytest
+
+
+@pytest.mark.security_id(167)
+@pytest.mark.security_id(168)
+@pytest.mark.security_id(169)
+@pytest.mark.security_id(170)
+@pytest.mark.security_id(324)
+def test_password_shadow(client):
+    """This ensure that not only the passwd and shadow is as expected,
+       it also validates that it's entry are consistent. And that's the
+       necessary files are set correctly.
+    """
+    password_shadow(client)
+    assert '' == execute_remote_command(client, "pwck")
+    assert '' == execute_remote_command(client, "grpck")
