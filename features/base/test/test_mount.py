7b20e1f3092071688acbf5752bda43917ff8c288
Akendo <Akendo@users.noreply.github.com>
2025-02-10 08:15:44 +0100

    Feature/test mount (#2667)
    In this commit, we re-introduces an old test we used to have that was cleaned up during a refactor.
    This tests for our mount points, we need to validate for some more mount parameters, but we need to get the test first.
    
    * Add tests for mount points
    * Remove a newline.

---
+++ features/base/test/test_mount.py
@@ -0,0 +1,14 @@
+from helper.tests.mount import mount
+from helper.tests.mount import MOUNT_TEST_TYPE_VERIFY_OPTION
+import pytest
+
+
+@pytest.mark.security_id(645)
+@pytest.mark.parametrize(
+    "mount_point,opt,test_type,test_val",
+    [
+        ("/", "rw", MOUNT_TEST_TYPE_VERIFY_OPTION, True)
+    ]
+)
+def test_mount(client, mount_point, opt, test_type, test_val, non_chroot):
+     mount(client, mount_point, opt, test_type, test_val)
