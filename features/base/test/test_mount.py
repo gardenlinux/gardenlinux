7c44399561d1afbe3edfcbca48b1e78b78f65325
akendo <akendo@akendo.eu>
2025-02-11 10:32:29 +0100

    Integrate feedback

--- features/base/test/test_mount.py
+++ features/base/test/test_mount.py
@@ -8,18 +8,8 @@ import pytest
     "mount_point,opt,test_type,test_val",
     [
         ("/", "rw", MOUNT_TEST_TYPE_VERIFY_OPTION, True)
+        ("/", "ro", MOUNT_TEST_TYPE_VERIFY_OPTION, False)
     ]
 )
-def test_mount(client, mount_point, opt, test_type, test_val, non_chroot, non_trustedboot, non_ephemeral):
-     mount(client, mount_point, opt, test_type, test_val)
-
-@pytest.mark.security_id(645)
-@pytest.mark.parametrize(
-    "mount_point,opt,test_type,test_val",
-    [
-        ("/", "ro", MOUNT_TEST_TYPE_VERIFY_OPTION, True)
-    ]
-)
-def test_mount_ro(client, mount_point, opt, test_type, test_val, trustedboot, ephemeral):
-    # Testing that we only havinga ro filesystem on trustedboot and ephemeral disk.
-     mount(client, mount_point, opt, test_type, test_val)
+def test_mount(client, mount_point, opt, test_type, test_val, non_chroot):
+    mount(client, mount_point, opt, test_type, test_val)
