39d0183dbb9bda4409d42faf8e312ad4afd844bc
akendo <akendo@akendo.eu>
2025-02-12 13:41:48 +0100

    Move conftest into a subfolder.

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,6 +22,8 @@ from platformSetup.firecracker import FireCracker
 from platformSetup.kvm import KVM
 from platformSetup.manual import Manual
 
+from conftest.architecture import *
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
@@ -506,27 +508,6 @@ def non_usi(testconfig):
         pytest.skip(f"test not supported on usi")    
 
 
-####
-# Feature ARCHITECTUR
-####
-
-@pytest.fixture
-def non_amd64(client):
-    (exit_code, output, error) = client.execute_command("dpkg --print-architecture", quiet=True)
-    if exit_code != 0:
-        logger.error(error)
-        sys.exit(exit_code)
-    if "amd64" in output:
-        pytest.skip('test not supported on amd64 architecture')
-
-@pytest.fixture
-def non_arm64(client):
-    (exit_code, output, error) = client.execute_command("dpkg --print-architecture", quiet=True)
-    if exit_code != 0:
-        logger.error(error)
-        sys.exit(exit_code)
-    if "arm64" in output:
-        pytest.skip('test not supported on arm64 architecture')
 
 
 #######
---
+++ tests/conftest/architecture.py
@@ -0,0 +1,25 @@
+"""In here we keep the architecture we support or not"""
+
+
+from sys import exit as sys_exit
+from logger import error as log_error
+
+import pytest
+
+@pytest.fixture
+def non_amd64(client):
+    (exit_code, output, error) = client.execute_command("dpkg --print-architecture", quiet=True)
+    if exit_code != 0:
+        log_error(error)
+        sys_exit(exit_code)
+    if "amd64" in output:
+        pytest.skip('test not supported on amd64 architecture')
+
+@pytest.fixture
+def non_arm64(client):
+    (exit_code, output, error) = client.execute_command("dpkg --print-architecture", quiet=True)
+    if exit_code != 0:
+        log_error(error)
+        sys_exit(exit_code)
+    if "arm64" in output:
+        pytest.skip('test not supported on arm64 architecture')
