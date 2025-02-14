acf081e05abb175067653d38c0caee427311e3d0
Moritz Kirchner <moritz.kirchner@sap.com>
2025-02-14 15:37:59 +0100

    Fix broken nightly by fixing the test shadow password test (#2714)
    * try to increase pytest output verbosity
    
    * check the output of the test first to get a hint of what's wrong
    
    addtitionally fix a local unbound error in one of the helper function
    
    * Debugging change, should be reverted
    
    * Revert of the debug change, will have to use sudo
    
    * Adapt the test to check known users and their modified home folder
    
    * Other than previously expected the output should be empty as
    
    /nonexistent either exists or is a convention
    
    * Selectivly use sudo only on systems on which it exists
    
    otherwise run command without sudo
    
    * implement PR findings + black formatting
    
    * Remove stdin option for sudo
    
    * long cli option --stdin it is

--- features/base/test/test_password_shadow.py
+++ features/base/test/test_password_shadow.py
@@ -10,7 +10,7 @@ import pytest
 @pytest.mark.security_id(324)
 @pytest.mark.parametrize(
     "command, expected_exit_code, expected_output",
-    [("/usr/sbin/pwck -r", 0, ""), ("/usr/sbin/grpck -r", 0, "")],
+    [("pwck -r", 0, ""), ("grpck -r", 0, "")],
 )
 def test_password_shadow(client, command, expected_exit_code, expected_output):
     """This ensure that not only the passwd and shadow is as expected,
@@ -21,6 +21,10 @@ def test_password_shadow(client, command, expected_exit_code, expected_output):
     imply we execute read-only.
     """
     password_shadow(client)
-    exit_code, output = execute_remote_command(client, command, skip_error=True)
+    exit_code, output = execute_remote_command(
+        client,
+        f"if [ $(id -u) = 0 ]; then {command}; else sudo --stdin {command}; fi",
+        skip_error=True,
+    )
+    assert output == ""
     assert exit_code == expected_exit_code
-    assert output == expected_output
