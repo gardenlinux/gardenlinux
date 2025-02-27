cfd2f8cae6e2213baa8fe5ea37d3e32cd520875f
akendo <akendo@akendo.eu>
2025-02-27 09:41:08 +0100

    Add security_id 172 for AWS.

--- features/aws/test/test_users.py
+++ features/aws/test/test_users.py
@@ -1,6 +1,15 @@
 from helper.tests.users import users
 
-additional_user="admin"
+additional_user = "admin"
 
+
+@pytest.mark.security_id(172)
 def test_users(client, aws):
-     users(client=client, additional_user=additional_user, additional_sudo_users=[additional_user])
+    """
+    AWS injects via cloud-init a user. The default username is admin and we
+    have to make sure that's present. With this, we ensure that the user fulfill
+    the security requirements.
+    """
+    users(client=client,
+          additional_user=additional_user,
+          additional_sudo_users=[additional_user])
