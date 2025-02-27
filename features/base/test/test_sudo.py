cc97f27f9741211742034ed0055fb9cf79c3bd5a
akendo <akendo@akendo.eu>
2025-02-27 21:40:40 +0100

    Add tests for sudo files

---
+++ features/base/test/test_sudo.py
@@ -0,0 +1,20 @@
+from helper.utils import read_file_remote, execute_remote_command
+import pytest
+
+
+@pytest.mark.security_id(165)
+def test_sudoers(client, non_container):
+    """
+    Validate that we have the necessary %sudo configration entry in sudoers.
+    Note: we do not use wheel, but instead the sudo group.
+    """
+
+    output = execute_remote_command(client, "getent group sudo")
+    assert output.split(":")[0] == 'sudo', "Group 'sudo' is missing!"
+
+    sudoers = read_file_remote(client,
+                               file="/etc/sudoers",
+                               remove_comments=True,
+                               remove_newline=True)
+    _ = [config for config in sudoers if '%sudo' in config]
+    assert len(_) == 1, "Missing sudo configuration in '/etc/sudoers'!"
