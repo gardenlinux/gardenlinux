59d5c47e920b354d30bb1941166296ca4b3a12ca
akendo <akendo@akendo.eu>
2025-02-28 22:34:21 +0100

    Add sudo permission

--- features/base/test/test_su.py
+++ features/base/test/test_su.py
@@ -8,10 +8,12 @@ def test_su_restriction(client, non_container):
     Validate that we have access to su restircted by default.
     """
 
+    client._default_to_sudo = True
     pam_configuration = read_file_remote(client,
                                          file="/etc/pam.d/su",
                                          remove_comments=True)
 
+    client._default_to_sudo = False
     auth_config = [config for config in pam_configuration if 'auth' in config]
     for entry in auth_config:
         if 'pam_wheel.so' in entry:
