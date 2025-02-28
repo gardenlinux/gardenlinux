734ab5c68bdfcab19ae70d7b1ebe47d7434cd611
akendo <akendo@akendo.eu>
2025-02-28 14:53:24 +0100

    Enable sudo permissions for the sudo test

--- features/base/test/test_sudo.py
+++ features/base/test/test_sudo.py
@@ -8,7 +8,8 @@ def test_sudoers(client, non_container):
     Validate that we have the necessary %sudo configration entry in sudoers.
     Note: we do not use wheel, but instead the sudo group.
     """
-
+    # Ensure we have sudo permissions.
+    client._default_to_sudo = True
     output = execute_remote_command(client, "getent group sudo")
     assert output.split(":")[0] == 'sudo', "Group 'sudo' is missing!"
 
@@ -16,5 +17,7 @@ def test_sudoers(client, non_container):
                                file="/etc/sudoers",
                                remove_comments=True,
                                remove_newlines=True)
+    # Drop sudo permissions.
+    client._default_to_sudo = False
     _ = [config for config in sudoers if '%sudo' in config]
     assert len(_) == 1, "Missing sudo configuration in '/etc/sudoers'!"
