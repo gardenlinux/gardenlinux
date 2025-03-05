195b1aeda83f5a6fcb0e4cfcbe4c54f7921f83c2
akendo <akendo@akendo.eu>
2025-03-05 10:50:36 +0100

    Fix sudo, make it more readiable

--- features/base/test/test_su.py
+++ features/base/test/test_su.py
@@ -1,5 +1,5 @@
 import pytest
-from helper.utils import read_file_remote
+from helper.utils import read_file_remote as read
 
 
 @pytest.mark.security_id(166)
@@ -9,11 +9,10 @@ def test_su_restriction(client, non_container):
     """
 
     client._default_to_sudo = True
-    pam_configuration = read_file_remote(client,
-                                         file="/etc/pam.d/su",
-                                         remove_comments=True)
+    pam_file = read(client, file="/etc/pam.d/su", remove_comments=True)
+    client._default_to_sudo = False
 
     assert any(
             'auth' in entry and 'pam_wheel.so' in entry
-            for entry in pam_configuration), \
+            for entry in pam_file), \
         "Control value of PAM Module pam_wheel.so must be set to required"
