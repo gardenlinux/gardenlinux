8d84281a0cec1a67d42e5e6f99ddabbaaeaa4005
akendo <akendo@akendo.eu>
2025-03-05 11:05:21 +0100

    Update doc string

--- features/base/test/test_su.py
+++ features/base/test/test_su.py
@@ -5,7 +5,13 @@ from helper.utils import read_file_remote as read
 @pytest.mark.security_id(166)
 def test_su_restriction(client, non_container):
     """
-    Validate that we have access to su restircted by default.
+    Validate that we have access to su restircted by default.  The test will
+    read the file from `/etc/pam.d/su` and check if it has the configuration
+    parameter we exepct.
+
+    A user needs to be configured to be admitted to use `su`. By default, this
+    is realized by a specify group, the `wheel` group. We have to ensure that
+    this behavior is enabled.
     """
 
     client._default_to_sudo = True
