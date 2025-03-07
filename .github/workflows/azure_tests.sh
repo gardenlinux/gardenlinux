97fd20acabb3160f3f450151a30a6a31a5c8f225
Florian Wilhelm <florian.wilhelm02@sap.com>
2025-03-07 10:56:18 +0100

    Configure architecture for azure
    Follow up for #2739
    
    We're trying to start an amd64 machine using the arm64 image which leads to a situation where the test waits forever because the vm does not boot.

--- .github/workflows/azure_tests.sh
+++ .github/workflows/azure_tests.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 #set -Eeuo pipefail
 
+arch=amd64
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --arch)
+      arch="$2"
+      shift 2
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
 # Name of Image to test
 cname="${@: -1}"
 
@@ -8,8 +22,6 @@ configFile="azure_test_config.yaml"
 containerName="ghcr.io/gardenlinux/gardenlinux/integration-test:today"
 artifact_dir="/tmp/gardenlinux-build-artifacts"
 
-azure_hyper_v_generation="V1"
-azure_vm_size="Standard_D4_v4"
 
 pushd "$artifact_dir" || exit 1
 tar -xzf "$cname.tar.gz" "$cname.vhd"
@@ -24,12 +36,30 @@ if [[ ! -e $image_file ]]; then
     exit 1
 fi
 
+case "$arch" in
+  amd64)
+    azure_hyper_v_generation="V1"
+    azure_vm_size="Standard_D4_v4"
+    azure_arch=x64
+    ;;
+  arm64)
+    azure_hyper_v_generation="V2"
+    azure_vm_size="Standard_D4ps_v5"
+    azure_arch=Arm64
+    ;;
+  *)
+    echo "unsupported architecture $arch" >&2
+    exit 1
+    ;;
+esac
+
 cat << EOF > "$configFile"
 azure:
     location: westeurope
     subscription_id: ${azure_subscription_id}
     vm_size: ${azure_vm_size}
     hyper_v_generation: ${azure_hyper_v_generation}
+    architecture: ${azure_arch}
     image: file:///artifacts/$(basename "$image_file")
     ssh:
       user: azureuser
