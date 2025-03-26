a709103df6b093827e18fdc1448a79715fdb55e9
Tobias Wolf <wolf@b1-systems.de>
2025-03-26 17:16:16 +0100

    Add support to retry failed workflow runs automatically
    Signed-off-by: Tobias Wolf <wolf@b1-systems.de>

--- .github/workflows/github.mjs
+++ .github/workflows/github.mjs
@@ -1,4 +1,56 @@
+export async function dispatchRetryWorkflow(core, githubActions, context, refName, retries = 1) {
+    if (context.runAttempt >= retries) {
+        core.setFailed("Workflow run failed permanently");
+        return false;
+    }
+
+    if (refName == "") {
+        core.setFailed("Workflow run retry requested refName is invalid");
+        return false;
+    }
+
+    githubActions.createWorkflowDispatch({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        workflow_id: "github_rerun_workflow.yml",
+        ref: refName,
+        inputs: {
+            run_id: String(context.runId),
+            retries: String(retries)
+        }
+    });
+
+    return true;
+}
+
 export function getPlatformForCName(cname) {
     const platform = cname.split("-", 1)[0];
     return platform;
+}
+
+export async function retryWorkflow(core, githubActions, context, runID, retries) {
+    if (isNaN(retries)) {
+        core.setFailed("Workflow run retry requested retries are invalid");
+        return false;
+    }
+
+    const workflowRun = await githubActions.getWorkflowRun({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        run_id: runID,
+        exclude_pull_requests: true
+    });
+
+    if (workflowRun.data.run_attempt >= retries) {
+        core.setFailed("Workflow run failed permanently");
+        return false;
+    }
+
+    await githubActions.reRunWorkflowFailedJobs({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        run_id: runID
+    });
+
+    return true;
 }
