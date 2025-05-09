e4e47e66b6bff379c0ba69a9ee7ed98a7954797b
Moritz Kirchner <moritz.kirchner@sap.com>
2025-05-09 13:50:24 +0000

    Use the test case without Namespace but with a pytest fixtures setup and teardown

--- features/iscsi/test/test_iscsi.py
+++ features/iscsi/test/test_iscsi.py
@@ -6,41 +6,53 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger()
 
 
-def test_iscsi_setu_o(client, non_provisioner_chroot):
+@pytest.fixture
+def iscsi_device(client):
     logger.info("Starting iSCSI setup test")
-    execute_remote_command(client, 
-        "start-stop-daemon --start --quiet --exec /usr/sbin/tgtd"
+    execute_remote_command(
+        client, "start-stop-daemon --start --exec /usr/sbin/tgtd || /bin/true"
     )
 
-    execute_remote_command(client, 
-        "dd if=/dev/zero of=/srv/iscsi_disk.img bs=1 count=0 seek=1G"
+    execute_remote_command(
+        client, "dd if=/dev/zero of=/srv/iscsi_disk.img bs=1 count=0 seek=1G"
     )
 
     iscsi_config = """<target iqn.2025-04.localhost:storage.disk1>
             backing-store /srv/iscsi_disk.img
             initiator-address 127.0.0.1
         </target>"""
-    execute_remote_command(client, 
-        f"echo '{iscsi_config}' > /etc/tgt/conf.d/iscsi_target.conf"
+    execute_remote_command(
+        client, f"echo '{iscsi_config}' > /etc/tgt/conf.d/iscsi_target.conf"
     )
     execute_remote_command(client, "/usr/sbin/tgt-admin --update ALL")
     execute_remote_command(client, "tgtadm --mode target --op show")
     execute_remote_command(client, "iscsiadm -m discovery -t sendtargets -p 127.0.0.1")
-    execute_remote_command(client, "iscsiadm -m node --login")
+    output = execute_remote_command(client, "iscsiadm -m node --login || /bin/true")
 
+    yield
+
+    execute_remote_command(client, "iscsiadm --mode node --logout")
+    execute_remote_command(
+        client, "start-stop-daemon --stop --quiet --exec /usr/sbin/tgtd"
+    )
+
+
+def test_iscsi_setup(client, non_provisioner_chroot, iscsi_device):
     output = execute_remote_command(client, "lsblk")
-    # logger.info(f"Block devices before rescan: {output}")
+    logger.info(f"Block devices before rescan: {output}")
     assert "sdb" not in output, "Unexpected /dev/sdb before rescan"
 
-    session_id, _ = execute_remote_command(client, "iscsiadm -m session | awk '{print $2}'")
+    session_id = execute_remote_command(
+        client, "iscsiadm -m session | awk '{print $2}'"
+    )
     session_id = session_id.strip("[]")
     assert session_id, "Failed to get session ID"
 
-    output = execute_remote_command(client, 
+    output = execute_remote_command(
         client, f"iscsiadm -m session -r {session_id} --rescan"
     )
     logger.info(f"Rescan output: {output}")
 
-    output = execute_remote_command(client, client, "lsblk")
+    output = execute_remote_command(client, "lsblk")
     logger.info(f"Block devices after rescan: {output}")
     assert "sdb" in output, "Expected /dev/sdb after rescan"
