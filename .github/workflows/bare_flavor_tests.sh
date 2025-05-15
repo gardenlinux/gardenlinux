22c695d5ff51a52afc657f4ea85ad48af268ec78
Tobias Wolf <wolf@b1-systems.de>
2025-05-15 06:45:29 +0200

    Disable bare flavors testing in `flavors.yml` and GitHub workflow
    Signed-off-by: Tobias Wolf <wolf@b1-systems.de>

--- .github/workflows/bare_flavor_tests.sh
+++ .github/workflows/bare_flavor_tests.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 #set -Eeuo pipefail
 
-cname="$@"
-
-arch="$(gl-features-parse --cname "${cname}" arch)"
-cname_base="$(gl-features-parse --cname "${cname}" cname_base)"
-bare_flavor="${cname_base//bare-/}"
-
-image="$(podman load < .build/bare_flavors/${bare_flavor}-${arch}.oci | awk '{ print $NF }')"
-cd bare_flavors/${bare_flavor}/test
-podman build -t test --build-arg image="$image" .
-podman run --rm test
+exit 1
