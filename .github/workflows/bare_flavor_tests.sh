da82eec54f92d5579a15f52412d1048e8d108d0f
Tobias Wolf <wolf@b1-systems.de>
2025-05-15 06:47:51 +0200

    Add bare flavor test logic to `rel-1592`
    Signed-off-by: Tobias Wolf <wolf@b1-systems.de>

--- .github/workflows/bare_flavor_tests.sh
+++ .github/workflows/bare_flavor_tests.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 #set -Eeuo pipefail
 
-exit 0
+cname="$@"
+
+arch="$(gl-features-parse --cname "${cname}" arch)"
+cname_base="$(gl-features-parse --cname "${cname}" cname_base)"
+bare_flavor="${cname_base//bare-/}"
+
+image="$(podman load < .build/bare_flavors/${bare_flavor}-${arch}.oci | awk '{ print $NF }')"
+cd bare_flavors/${bare_flavor}/test
+podman build -t test --build-arg image="$image" .
+podman run --rm test
