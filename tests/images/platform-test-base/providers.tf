b1d8499f2086a157517f72e56d321e70280526d7
nkraetzschmar <9020053+nkraetzschmar@users.noreply.github.com>
2025-05-19 10:57:44 +0200

    fix: don't rebuild platform test images

---
+++ tests/images/platform-test-base/providers.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = "~> 1.8.0"
+  required_providers {
+    alicloud = {
+      source  = "aliyun/alicloud"
+      version = "~> 1.235.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.84.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.3.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.4.0"
+    }
+  }
+}
