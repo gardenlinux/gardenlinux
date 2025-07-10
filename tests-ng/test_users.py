a549e31aab11eca7dfe38b32f18e6d55dd563253
nkraetzschmar <9020053+nkraetzschmar@users.noreply.github.com>
2025-07-10 23:45:28 +0200

    feat: test-ng: port test_users

---
+++ tests-ng/test_users.py
@@ -0,0 +1,24 @@
+import os
+import pwd
+import pytest
+import stat
+
+def test_service_accounts_have_nologin_shell():
+	for entry in pwd.getpwall():
+		if entry.pw_uid >= 1000:
+			continue
+		if entry.pw_name in {"root", "sync"}:
+			continue
+		assert entry.pw_shell in [ "/usr/sbin/nologin", "/bin/false" ], f"User {entry.pw_name} has unexpected shell: {entry.pw_shell}"
+
+def test_root_home_permissions():
+	mode = os.stat("/root").st_mode
+	perm = stat.S_IMODE(mode)
+	assert perm == 0o700, f"/root has incorrect permissions: {oct(perm)}"
+
+@pytest.mark.feature("not _dev")
+def test_no_extra_home_directories():
+	if os.path.islink("/home"):
+		return
+	entries = os.listdir("/home")
+	assert not entries, f"Unexpected entries in /home: {entries}"
