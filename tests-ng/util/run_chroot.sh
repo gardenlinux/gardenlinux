e32a561c5dd51899baefce9eeb22076c33c58411
nkraetzschmar <9020053+nkraetzschmar@users.noreply.github.com>
2025-07-21 18:26:50 +0200

    feat: test-ng: add script to run chroot tests on rootfs.tar

---
+++ tests-ng/util/run_chroot.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+set -eufo pipefail
+
+containerize=0
+
+if [ "$(uname -s)" != Linux ]; then
+	containerize=1
+fi
+
+while [ $# -gt 0 ]; do
+	case "$1" in
+		--containerize) containerize=1; shift ;;
+		--no-containerize) containerize=0; shift ;;
+		*) break ;;
+	esac
+done
+
+test_dist="$1"
+rootfs_tar="$2"
+
+if (( containerize )); then
+	dir="$(realpath "$(dirname "${BASH_SOURCE[0]}")"/../..)"
+	container_image="$("$dir/build" --print-container-image)"
+	exec podman run --rm -v "$(realpath -- "${BASH_SOURCE[0]}"):/init:ro" -v "$(realpath -- "$test_dist"):/mnt/test_dist:ro" -v "$(realpath -- "$rootfs_tar"):/mnt/rootfs:ro" "$container_image" fake_xattr /init --no-containerize /mnt/test_dist /mnt/rootfs
+fi
+
+tmpdir=
+
+cleanup () {
+	[ -z "$tmpdir" ] || [ ! -e "$tmpdir/chroot" ] || umount -l "$tmpdir/chroot" || rmdir "$tmpdir/chroot"
+	[ -z "$tmpdir" ] || rm -rf "$tmpdir"
+	tmpdir=
+}
+
+trap cleanup EXIT
+tmpdir="$(mktemp -d)"
+
+echo "creating chroot for test run"
+
+mkdir "$tmpdir/chroot"
+mount -t tmpfs -o mode=0755 none "$tmpdir/chroot"
+
+tar --extract --xattrs --xattrs-include 'security.*' --directory "$tmpdir/chroot" < "$rootfs_tar"
+
+mount --rbind --make-rprivate /proc "$tmpdir/chroot/proc"
+mount --rbind --make-rprivate /sys "$tmpdir/chroot/sys"
+mount --rbind --make-rprivate /dev "$tmpdir/chroot/dev"
+
+echo "setting up test framework"
+
+mkdir "$tmpdir/chroot/run/gardenlinux_tests"
+gzip -d < "$test_dist" | tar --extract --directory "$tmpdir/chroot/run/gardenlinux_tests"
+
+echo "running tests"
+
+env -i /sbin/chroot "$tmpdir/chroot" /bin/sh -c 'cd /run/gardenlinux_tests && ./run_tests'
