96d124e3996dae37b49753e4c41f5456b2846d39
Eike Waldt <waldt@b1-systems.de>
2025-07-22 07:27:54 +0200

    feat: test-ng: do not depend on any binary packages (pyhton wheels) Therefore, we only need to keep the site-packages directory in the root of the runtime and symlink it to the other architectures.

--- tests-ng/util/build_dist.sh
+++ tests-ng/util/build_dist.sh
@@ -8,8 +8,8 @@ fi
 
 tmpdir=
 
-cleanup () {
-	[ -z "$tmpdir" ] || rm -rf "$tmpdir"
+cleanup() {
+	[ -n "$tmpdir" ] || rm -rf "$tmpdir"
 	tmpdir=
 }
 
@@ -22,9 +22,19 @@ runtimes=("$@")
 
 mkdir -p "$tmpdir/dist/runtime"
 for runtime in "${runtimes[@]}"; do
-	IFS=: read runtime_arch runtime_tar <<< "$runtime"
+	IFS=: read runtime_arch runtime_tar <<<"$runtime"
 	mkdir "$tmpdir/dist/runtime/$runtime_arch"
-	gzip -d < "$runtime_tar" | tar -x -C "$tmpdir/dist/runtime/$runtime_arch"
+	gzip -d <"$runtime_tar" | tar -x -C "$tmpdir/dist/runtime/$runtime_arch"
+	# NOTE: as for now, we should not depend on any binary packages
+	# therefore, we only need to keep the site-packages directory in the root of the runtime
+	# and symlink it to the other architectures
+	PYTHON_LIB_DIR="$(find $tmpdir/dist/runtime/$runtime_arch/lib -type d -name python*)"
+	if [ "$runtime_arch" = "x86_64" ]; then
+		mv "$PYTHON_LIB_DIR/site-packages" "$tmpdir/dist/runtime/site-packages"
+	else
+		rm -rf "$PYTHON_LIB_DIR/site-packages"
+	fi
+	(cd "$PYTHON_LIB_DIR" && ln -s ../../site-packages)
 done
 
 set +f
@@ -32,7 +42,7 @@ set +f
 mkdir -p "$tmpdir/dist/tests"
 cp -r -t "$tmpdir/dist/tests" conftest.py plugins test_*.py
 
-cat > "$tmpdir/dist/run_tests" <<'EOF'
+cat >"$tmpdir/dist/run_tests" <<'EOF'
 #!/bin/sh
 
 set -e
@@ -47,4 +57,4 @@ COLUMNS=120 python -m pytest -rA --tb=short --color=yes "$@"
 EOF
 chmod +x "$tmpdir/dist/run_tests"
 
-tar -c -C "$tmpdir/dist" . | gzip > "$output"
+tar -c -C "$tmpdir/dist" . | gzip >"$output"
