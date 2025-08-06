f6ae042fe66882c28c71f16db859fbf74697018d
nkraetzschmar <9020053+nkraetzschmar@users.noreply.github.com>
2025-08-06 17:19:57 +0200

    formatting: use spaces in test-ng python code

--- tests-ng/conftest.py
+++ tests-ng/conftest.py
@@ -3,7 +3,7 @@ import pytest
 
 plugin_dir = os.path.join(os.path.dirname(__file__), "plugins")
 pytest_plugins = [
-	f"plugins.{f[:-3]}"
-	for f in os.listdir(plugin_dir)
-	if f.endswith(".py") and not f.startswith("_")
+    f"plugins.{f[:-3]}"
+    for f in os.listdir(plugin_dir)
+    if f.endswith(".py") and not f.startswith("_")
 ]
