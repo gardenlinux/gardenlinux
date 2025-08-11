ea78ac8793c0416d34fb3f9677f88300e370f2e6
Florian Wilhelm <florian.wilhelm02@sap.com>
2025-08-11 16:20:24 +0200

    review

--- tests-ng/plugins/utils.py
+++ tests-ng/plugins/utils.py
@@ -6,15 +6,9 @@ import pytest
 def equals_ignore_case(a: str, b: str) -> str:
     return a.casefold() == b.casefold()
 
-
-def get_normalized_sets(
-    actual_value: set,
-    expected_value: set,
-) -> tuple[set, set]:
-    actual_set = {str(v).casefold() for v in actual_value}
-    expected_set = {str(v).casefold() for v in expected_value}
-    return actual_set, expected_set
-
+def get_normalized_sets(*sets: set) -> tuple[set, ...]:
+    normalized_sets = tuple({str(v).casefold() for v in s} for s in sets)
+    return normalized_sets
 
 def is_set(obj) -> bool:
     return isinstance(obj, set)
