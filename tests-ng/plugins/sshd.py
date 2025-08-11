9b8b6f2d6b27e8937095820b065cedaae4e0b2be
Florian Wilhelm <florian.wilhelm02@sap.com>
2025-08-11 13:38:00 +0200

    move ssh test into plugin

--- tests-ng/plugins/sshd.py
+++ tests-ng/plugins/sshd.py
@@ -1,9 +1,58 @@
 import pytest
 from .shell import ShellRunner
 
+
 class Sshd:
     def __init__(self, shell: ShellRunner):
         self._shell = shell
+        result = shell("/usr/sbin/sshd -T", capture_output=True, ignore_exit_code=True)
+        assert (
+            result.returncode == 0
+        ), f"Expected return code 0, got {result.returncode}"
+        self._sshd_config_text = result.stdout
+        self._sshd_config = None
 
-    def get_config():
-        
+    def read_config(self) -> dict:
+        if self._sshd_config is not None:
+            return self._sshd_config
+        config = {}
+        for line in self._sshd_config_text.splitlines():
+            if not line.strip():
+                continue
+            parts = line.split(maxsplit=1)
+            if len(parts) == 2:
+                key, value = parts
+                # If key already exists, convert to set and append
+                if key in config:
+                    if isinstance(config[key], set):
+                        config[key].add(value)
+                    else:
+                        config[key] = {config[key], value}
+                else:
+                    # fixme: this is not ideal. what we want here is to have comma separated string values
+                    # as a set to make comparing them easier because the order of entries does not matter
+                    # the 3 is an arbitrary number to allow for real string values with commas
+                    if value.count(",") >= 3:
+                        config[key] = set(value.split(","))
+                    else:
+                        config[key] = value
+            elif len(parts) == 1:
+                key = parts[0]
+                config[key] = None
+        self._sshd_config = config
+        return config
+
+    def get_config_section(self, key: str) -> str:
+        config = self.read_config()
+        return config.get(str.lower(key))
+
+    def get_normalized_sets(
+        self,
+        sshd_config_item: str,
+        actual_value: set,
+        expected_value: set,
+    ) -> tuple[set, set]:
+        assert isinstance(actual_value, set), f"{sshd_config_item} should be a set"
+        actual_set = {str(v).lower() for v in actual_value}
+        expected_set = {str(v).lower() for v in expected_value}
+        return actual_set, expected_set
--- tests-ng/test_ssh.py
+++ tests-ng/test_ssh.py
@@ -1,81 +1,71 @@
 import pytest
 from plugins.shell import ShellRunner
 from plugins.systemd import Systemd
+from plugins.sshd import Sshd
 
-required_sshd_config = [
-    # Supported HostKey algorithms.
-    "HostKey /etc/ssh/ssh_host_ed25519_key",
-    "HostKey /etc/ssh/ssh_host_rsa_key",
-    # "HostKey /etc/ssh/ssh_host_ecdsa_key",
-    "KexAlgorithms sntrup761x25519-sha512,sntrup761x25519-sha512@openssh.com,mlkem768x25519-sha256,curve25519-sha256,curve25519-sha256@libssh.org,ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521",
-    "Ciphers chacha20-poly1305@openssh.com,aes128-ctr,aes192-ctr,aes256-ctr,aes128-gcm@openssh.com,aes256-gcm@openssh.com",
-    "MACs umac-64-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,hmac-sha1-etm@openssh.com,umac-64@openssh.com,umac-128@openssh.com,hmac-sha2-256,hmac-sha2-512,hmac-sha1",
-    # Password based logins are disabled - only public key based logins are allowed.
-    "AuthenticationMethods publickey",
-    # LogLevel VERBOSE logs user's key fingerprint on login. Needed to have a clear audit track of which key was using to log in.
-    "LogLevel VERBOSE",
-    # Log sftp level file access (read/write/etc.) that would not be easily logged otherwise - be aware that the path has to be adapted to the Distribution installed.
-    "Subsystem sftp /usr/lib/openssh/sftp-server -f AUTHPRIV -l INFO",
-    # Root login is not allowed for auditing reasons. This is because it's difficult to track which process belongs to which root user:
-    #
-    # On Linux, user sessions are tracking using a kernel-side session id, however, this session id is not recorded by OpenSSH.
-    # Additionally, only tools such as systemd and auditd record the process session id.
-    # On other OSes, the user session id is not necessarily recorded at all kernel-side.
-    # Using regular users in combination with /bin/su or /usr/bin/sudo ensure a clear audit track.
-    "PermitRootLogin No",
-    # Use kernel sandbox mechanisms where possible in unprivileged processes
-    # Systrace on OpenBSD, Seccomp on Linux, seatbelt on MacOSX/Darwin, rlimit elsewhere.
-    # "UsePrivilegeSeparation sandbox",
-    # Disable Tunneling and Forwarding
-    # AllowTcpForwarding no # required for Gardener
-    "X11Forwarding no",
-    # use PAM
-    "UsePAM yes",
-]
+required_sshd_config = {
+    "HostKey": {
+        "/etc/ssh/ssh_host_ed25519_key",
+        "/etc/ssh/ssh_host_rsa_key",
+    },
+    "KexAlgorithms": {
+        "sntrup761x25519-sha512",
+        "sntrup761x25519-sha512@openssh.com",
+        "mlkem768x25519-sha256",
+        "curve25519-sha256",
+        "curve25519-sha256@libssh.org",
+        "ecdh-sha2-nistp256",
+        "ecdh-sha2-nistp384",
+        "ecdh-sha2-nistp521",
+    },
+    "Ciphers": {
+        "chacha20-poly1305@openssh.com",
+        "aes128-ctr",
+        "aes192-ctr",
+        "aes256-ctr",
+        "aes128-gcm@openssh.com",
+        "aes256-gcm@openssh.com",
+    },
+    "MACs": {
+        "umac-64-etm@openssh.com",
+        "umac-128-etm@openssh.com",
+        "hmac-sha2-256-etm@openssh.com",
+        "hmac-sha2-512-etm@openssh.com",
+        "hmac-sha1-etm@openssh.com",
+        "umac-64@openssh.com",
+        "umac-128@openssh.com",
+        "hmac-sha2-256",
+        "hmac-sha2-512",
+        "hmac-sha1",
+    },
+    "AuthenticationMethods": "publickey",
+    "LogLevel": "VERBOSE",
+    "Subsystem": "sftp /usr/lib/openssh/sftp-server -f AUTHPRIV -l INFO",
+    "PermitRootLogin": "No",
+    "X11Forwarding": "no",
+    "UsePAM": "yes",
+}
 
-
-def _create_list_of_tuples(input: str) -> list[tuple[str, set[str]]]:
-    """Takes a multiline string and returns a list containing every line
-    as a tuple. The 1st value of the tuple is the ssh option, the 2nd is
-    value"""
-    out = []
-    for line in input.lower().splitlines():
-        l = line.split(" ", 1)
-        option = l[0]
-        value = l[1]
-        normalized_value = _normalize_value(value)
-        out.append((option, normalized_value))
-    return out
-
-
-def _normalize_value(string: str) -> set[str]:
-    """Convert a given string.
-    The string will be returned as a set. If the element contains a comma
-    separated string, it will be split into a list first."""
-    normalized = string.split(" ")
-    if len(normalized) == 1 and "," in normalized[0]:
-        value_as_set = set(normalized[0].split(","))
-    else:
-        value_as_set = set(normalized)
-    return value_as_set
 
 @pytest.mark.booted
 @pytest.mark.feature("ssh")
 def test_sshd_is_running(systemd: Systemd):
     assert systemd.is_running("sshd")
 
+
 @pytest.mark.booted
 @pytest.mark.root
 @pytest.mark.feature("ssh")
 @pytest.mark.parametrize("sshd_config_item", required_sshd_config)
 def test_sshd_has_required_config(sshd_config_item: str, shell: ShellRunner):
-    result = shell("/usr/sbin/sshd -T", capture_output=True, ignore_exit_code=True)
-    assert result.returncode == 0, f"Expected return code 0, got {result.returncode}"
-
-    sshd_config = _create_list_of_tuples(result.stdout)
-
-    expected = _create_list_of_tuples(sshd_config_item)
-
-    assert all(
-        option in sshd_config for option in expected
-    ), f"{expected} not found in sshd_config"
+    sshd = Sshd(shell)
+    actual_value = sshd.get_config_section(sshd_config_item)
+    expected_value = required_sshd_config[sshd_config_item]
+    if isinstance(expected_value, set):
+        actual_set, expected_set = sshd.get_normalized_sets(sshd_config_item, actual_value, expected_value)
+        missing_sshd_configuration = expected_set - actual_set
+        assert not missing_sshd_configuration, f"{sshd_config_item}: missing values {missing_sshd_configuration}"
+    else:
+        actual_str = str(actual_value).lower()
+        expected_str = str(expected_value).lower()
+        assert actual_str == expected_str, f"{sshd_config_item}: expected {expected_str}, got {actual_str}"
