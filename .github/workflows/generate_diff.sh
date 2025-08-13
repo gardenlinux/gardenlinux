c25581791c49b4c0b16a48d50b40515b97694963
Leon Kniffki <leon.kniffki@sap.com>
2025-08-13 15:20:35 +0200

    allow different cnames

--- .github/workflows/generate_diff.sh
+++ .github/workflows/generate_diff.sh
@@ -6,10 +6,12 @@ whitelist=("dummypath_abcd...")
 
 if [ -z ${2+x} ]; then
     # $2 is unset
-    basefile="${1/bare-/}.oci"
+    basefile_a="${1/bare-/}.oci"
+    basefile_b="${1/bare-/}.oci"
 else
     # $2 is set
-    basefile="$2.tar"
+    basefile_a="$2.tar"
+    basefile_b="$3.tar"
 fi
 
 sedcommands=()
@@ -19,7 +21,7 @@ for file in "${whitelist[@]}"; do
     sedcommands+=("\|$file|d")
 done
 
-if ! cmp "A/$basefile" "B/$basefile" > /dev/null; then
+if ! cmp "A/$basefile_a" "B/$basefile_b" > /dev/null; then
     # Difference detected
 
     files=$(diff -qrN ./A/unpacked ./B/unpacked 2> /dev/null \
