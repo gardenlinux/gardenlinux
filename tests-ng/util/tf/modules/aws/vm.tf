4612d22f4a22a538c6fb831334c96bca0c6a6997
Eike Waldt <waldt@b1-systems.de>
2025-08-14 14:01:13 +0200

    feat: test-ng: rename tofu resources to be constant throughout cloud providers

--- tests-ng/util/tf/modules/aws/vm.tf
+++ tests-ng/util/tf/modules/aws/vm.tf
@@ -1,77 +1,11 @@
-resource "aws_vpc" "net" {
-  cidr_block           = var.provider_vars.vpc_cidr
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-
-  tags = merge(
-    local.labels,
-    { Name = local.vpc_name }
-  )
-
-}
-
-resource "aws_subnet" "subnet" {
-  vpc_id     = aws_vpc.net.id
-  cidr_block = var.provider_vars.public_subnet_cidr
-
-  tags = merge(
-    local.labels,
-    { Name = local.subnet_name }
-  )
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.net.id
-
-  tags = merge(
-    local.labels,
-    { Name = local.igw_name }
-  )
-}
-
-resource "aws_route_table" "igw" {
-  vpc_id = aws_vpc.net.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = merge(
-    local.labels,
-    { Name = local.rt_name }
-  )
-}
-
-resource "aws_route_table_association" "igw_vm" {
-  route_table_id = aws_route_table.igw.id
-  subnet_id      = aws_subnet.subnet.id
-}
-
-resource "aws_security_group" "default" {
-  name   = local.sg_name
-  vpc_id = aws_vpc.net.id
-
-  ingress {
-    cidr_blocks = [
-      "${var.my_ip}/32"
-    ]
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_key_pair" "ssh" {
-  key_name   = local.key_name
+  key_name   = local.ssh_key_name
   public_key = file(var.ssh_public_key_path)
+
+  tags = merge(
+    local.labels,
+    { Name = local.ssh_key_name }
+  )
 }
 
 resource "aws_instance" "vm" {
