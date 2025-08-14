323d2d2e195cebd72cd87d5f21ac86f83f54926c
Eike Waldt <waldt@b1-systems.de>
2025-08-14 13:32:19 +0200

    feat: test-ng: tofu provider locking

--- tests-ng/util/tf/main.tf
+++ tests-ng/util/tf/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_version = "~> 1.0"
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
-    http = {
-      source  = "hashicorp/http"
-      version = "~> 3.0"
-    }
-  }
-}
-
 locals {
   provider_modules = {
     aws   = "./modules/aws"
