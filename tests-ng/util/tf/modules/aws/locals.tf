eef8d2e6af8254ac00c56a271d278325bf5f0ca1
Eike Waldt <waldt@b1-systems.de>
2025-08-14 13:35:20 +0200

    feat: test-ng: tagging of tofu resources

--- tests-ng/util/tf/modules/aws/locals.tf
+++ tests-ng/util/tf/modules/aws/locals.tf
@@ -1,12 +1,18 @@
 locals {
-  bucket_name = "${var.prefix}-disk-upload"
-  ami_name    = "${var.prefix}-image"
-  vpc_name    = "${var.prefix}-vpc"
-  subnet_name = "${var.prefix}-subnet"
-  igw_name    = "${var.prefix}-igw"
-  rt_name     = "${var.prefix}-rt"
-  sg_name     = "${var.prefix}-sg"
-  key_name    = "${var.prefix}-ssh-key"
+  test_name   = replace(var.prefix, "_", "-")
+  bucket_name = "${local.test_name}-disk-upload"
+  ami_name    = "${local.test_name}-image"
+  vpc_name    = "${local.test_name}-vpc"
+  subnet_name = "${local.test_name}-subnet"
+  igw_name    = "${local.test_name}-igw"
+  rt_name     = "${local.test_name}-rt"
+  sg_name     = "${local.test_name}-sg"
+  key_name    = "${local.test_name}-ssh-key"
+  labels = {
+    component = "gardenlinux"
+    test-type = "platform-test"
+    test-name = local.test_name
+  }
 
   root_disk_object_key  = basename(var.root_disk_path)
   test_disk_object_key  = basename(var.test_disk_path)
