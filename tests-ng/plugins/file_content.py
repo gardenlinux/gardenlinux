3e4aa29cf6a57c24d9a84e6944986a6eac7319cd
SujanaSubr <sujana.subramaniam@sap.com>
2025-08-18 10:51:11 +0000

    CI failure fix

--- tests-ng/plugins/file_content.py
+++ tests-ng/plugins/file_content.py
@@ -1,12 +1,10 @@
-import re
-import os
 
 """ This function checks each line and return true if the args is found in the file """
-def check_val_in_file(fname, args):
+def is_line_in_file(fname, value):
     """ Performing unit test to find key/val in files """
     with open(fname, 'r') as file:
         for line in file:
-            if args in line.strip():
+            if value in line.strip():
                 return True
     return False
     
--- tests-ng/test_vmware.py
+++ tests-ng/test_vmware.py
@@ -1,12 +1,12 @@
 import pytest
 import os
-from plugins.file_content import check_val_in_file
+from plugins.file_content import is_line_in_file
 
-val_to_check = [ ("/etc/cloud/cloud.cfg", "ntp"),
+forbidden_config_values = [ ("/etc/cloud/cloud.cfg", "ntp"),
              ("/etc/cloud/cloud.cfg", "resizefs"),
              ("/etc/cloud/cloud.cfg", "growpart") ] 
 
-vmware_files = [
+vmware_required_files = [
         "/usr/bin/dscheck_VMwareGuestInfo",
         "/usr/lib/python3/dist-packages/cloudinit/sources/DataSourceVMwareGuestInfo.py",
         "/etc/cloud/cloud.cfg.d/01_debian-cloud.cfg",
@@ -15,15 +15,14 @@ vmware_files = [
     ]
 
 @pytest.mark.feature("vmware")
-@pytest.mark.parametrize("filename,content", val_to_check)
-
-def test_file_content(filename: str, content: str):
+@pytest.mark.parametrize("filename,content", forbidden_config_values)
+def test_config_for_forbidden_value(filename: str, content: str):
     assert os.path.isfile(filename), "File does not exist"
-    assert not check_val_in_file(filename, content), (
+    assert not is_line_in_file(filename, content), (
             f"Found {content} in {filename} which is not expected." )
 
-@pytest.mark.parametrize("file_to_check", vmware_files)
-
-def test_check_file(file_to_check: str):
+@pytest.mark.feature("vmware")
+@pytest.mark.parametrize("file_to_check", vmware_required_files)
+def test_required_files_exist(file_to_check: str):
     assert os.path.exists(file_to_check), (
             f"File {file_to_check} could not be found." )
