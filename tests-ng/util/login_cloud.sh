b459dfec26e648ae02a12300e04a83035d602f50
Eike Waldt <waldt@b1-systems.de>
2025-08-25 09:54:54 +0200

    feat: test-ng: generate ssh key if missing

--- tests-ng/util/login_cloud.sh
+++ tests-ng/util/login_cloud.sh
@@ -20,6 +20,6 @@ tf_dir="$(realpath -- "$(dirname -- "${BASH_SOURCE[0]}")/tf")"
 
 vm_ip="$(cd "$tf_dir" && tofu workspace select "$workspace" >/dev/null && tofu output --raw vm_ip)"
 ssh_user="$(cd "$tf_dir" && tofu workspace select "$workspace" >/dev/null && tofu output --raw ssh_user)"
-ssh_opts=(-o ConnectTimeout=5 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no)
+ssh_opts=(-o ConnectTimeout=5 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/.ssh/id_ed25519_gl)
 
 exec ssh "${ssh_opts[@]}" "$ssh_user@$vm_ip" "$@"
