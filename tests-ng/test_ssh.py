e097de659967aebb2cde4bb770c4a2ec1ff3f3e9
Eike Waldt <waldt@b1-systems.de>
2025-08-25 10:15:36 +0200

    feat: test-ng: do not check authorized_keys for expected_users

--- tests-ng/test_ssh.py
+++ tests-ng/test_ssh.py
@@ -65,13 +65,15 @@ def test_sshd_has_required_config(sshd_config_item: str, sshd: Sshd):
         assert equals_ignore_case(actual_value, expected_value), f"{sshd_config_item}: expected {expected_value}, got {actual_value}"
 
 @pytest.mark.feature("ssh")
-def test_users_have_no_authorized_keys():
+def test_users_have_no_authorized_keys(expected_users=[]):
     skip_users = {"nologin", "sync"}
     skip_shells = {"/bin/false"}
     files_to_check = ["authorized_keys", "authorized_keys2"]
 
     for entry in pwd.getpwall():
-        if entry.pw_name in skip_users or entry.pw_shell in skip_shells:
+        if any([entry.pw_name in skip_users,
+               entry.pw_shell in skip_shells,
+               entry.pw_name in expected_users]):
             continue
 
         ssh_dir = os.path.join(entry.pw_dir, ".ssh")
