f1efd291be520e2d5c76f10d7fd9250bffe12cc6
Vivus Ignis <ignis@posteo.de>
2025-09-01 09:09:46 +0200

    using a fixture for ctr calls, latest tag for docker images

--- tests-ng/test_containers.py
+++ tests-ng/test_containers.py
@@ -1,27 +1,18 @@
 import pytest
-from plugins.shell import ShellRunner
+from plugins.containerd import CtrRunner
 
 TEST_IMAGES = [
-    "ghcr.io/gardenlinux/gardenlinux:1592.12",    # Github Container Registry, https://github.com/orgs/gardenlinux/packages?ecosystem=container
-    "docker.io/library/debian:12.11-slim",        # Docker Hub, https://hub.docker.com/_/debian
-    "public.ecr.aws/debian/debian:11",            # AWS ECR, https://gallery.ecr.aws/debian/debian
+    "ghcr.io/gardenlinux/gardenlinux:latest", # Github Container Registry, https://github.com/orgs/gardenlinux/packages?ecosystem=container
+    "docker.io/library/debian:latest",        # Docker Hub, https://hub.docker.com/_/debian
+    "public.ecr.aws/debian/debian:latest",    # AWS ECR, https://gallery.ecr.aws/debian/debian
 ]
 
 
-@pytest.fixture
-def containerd(shell: ShellRunner):
-    shell("systemctl start containerd", capture_output=False)
-    yield
-    shell("systemctl stop containerd", capture_output=False)
-
-
-def test_basic_container_functionality(shell: ShellRunner, containerd):
+def test_basic_container_functionality(ctr: CtrRunner):
     for uri in TEST_IMAGES:
-        pull_command = f"ctr image pull {uri}"
-        shell(pull_command, capture_output=False)
+        ctr.pull(uri)
 
         container_name = uri.split("/")[0].replace(".", "-")
-        command = f"ctr run --rm {uri} {container_name} uname"
-        out = shell(command, capture_output=True, ignore_exit_code=True)
+        out = ctr.run(uri, container_name, "uname", capture_output=True, ignore_exit_code=True)
 
-        assert "Linux" in out.stdout, "Command {} failed: {}".format(command, out.stderr)
+        assert "Linux" in out.stdout, f"Command failed: {out.stderr}"
