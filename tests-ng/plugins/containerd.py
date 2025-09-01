d5b1225ea2c465bb671d3ca9c406b6f729557851
Vivus Ignis <ignis@posteo.de>
2025-09-01 09:09:46 +0200

    using a fixture for ctr calls, latest tag for docker images

---
+++ tests-ng/plugins/containerd.py
@@ -0,0 +1,22 @@
+import pytest
+from .shell import ShellRunner
+from .systemd import Systemd
+
+
+class CtrRunner:
+    def __init__(self, shell: ShellRunner, systemd: Systemd):
+        self.shell = shell
+        systemd.start_unit("containerd")
+
+    def pull(self, uri):
+        command = f"ctr image pull {uri}"
+        self.shell(command, capture_output=False)
+
+    def run(self, uri, container_name, cmd, capture_output=False, ignore_exit_code=False):
+        command = f"ctr run --rm {uri} {container_name} {cmd}"
+        return self.shell(command, capture_output=capture_output, ignore_exit_code=ignore_exit_code)
+
+
+@pytest.fixture
+def ctr(shell: ShellRunner, systemd: Systemd):
+    return CtrRunner(shell, systemd)
