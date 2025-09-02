f73c16f0f647c7def977c86722587a5dbd1d7eb3
Vivus Ignis <ignis@posteo.de>
2025-09-02 20:12:00 +0200

    container_name to be generated in CrtRunner

--- tests-ng/plugins/containerd.py
+++ tests-ng/plugins/containerd.py
@@ -19,8 +19,10 @@ class CtrRunner:
         command = f"ctr image pull {uri}"
         self.shell(command, capture_output=False)
 
-    def run(self, uri, container_name, cmd, capture_output=False, ignore_exit_code=False):
+    def run(self, uri, cmd, capture_output=False, ignore_exit_code=False):
         validators.url(uri)
+
+        container_name = uri.split("/")[0].replace(".", "-")
         command = f"ctr run --rm {uri} {container_name} {cmd}"
         return self.shell(command, capture_output=capture_output, ignore_exit_code=ignore_exit_code)
 
