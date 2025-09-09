af8cb223fc6b7dc0930e1eef987daa5ebd500a8c
sujanas <sujana.subramaniam@sap.com>
2025-09-09 13:43:03 +0000

    Port nvme test to new test framework

--- tests-ng/test_nvme.py
+++ tests-ng/test_nvme.py
@@ -1,7 +1,10 @@
 import pytest
 import subprocess
 from plugins.shell import ShellRunner
+<<<<<<< HEAD
 from handlers.configure_nvme import nvme_device
+=======
+>>>>>>> c3b66280 (Port nvme test to new test framework)
 import os
 import json
 
@@ -9,6 +12,17 @@ module = [
     "nvme-tcp" 
     ]
 
+<<<<<<< HEAD
+=======
+# Define variables for the IP address, NVMe device, and subsystem name
+IP_ADDRESS="127.0.0.1"
+NVME_DEVICE="/tmp/nvme.img"
+SUBSYSTEM_NAME="testnqn"
+PORT_NUMBER="4420"
+TRTYPE="tcp"
+ADRFAM="ipv4"
+
+>>>>>>> c3b66280 (Port nvme test to new test framework)
 @pytest.mark.booted
 @pytest.mark.root
 @pytest.mark.feature("nvme")
@@ -20,6 +34,7 @@ def test_kernel_module_availability(module_name, shell: ShellRunner):
 
 @pytest.mark.booted
 @pytest.mark.root
+<<<<<<< HEAD
 @pytest.mark.modify
 @pytest.mark.feature("nvme")
 def test_nvme_locally(nvme_device, shell: ShellRunner):
@@ -29,3 +44,15 @@ def test_nvme_locally(nvme_device, shell: ShellRunner):
     assert mount_info[0] == device
     assert mount_info[1] == size
     assert ((shell(f"cat /mnt/bar", capture_output=True, ignore_exit_code=False)).stdout.strip() == 'foo')
+=======
+@pytest.mark.feature("nvme")
+def test_nvme_locally(shell: ShellRunner):
+    configure_nvme_out = (shell("/bin/bash ./configure_nvme.sh connect", capture_output=True, ignore_exit_code=False)).stdout.strip() 
+    device, mount_point, size = [x.strip().strip(',') for x in configure_nvme_out.split(",")]
+    mount_info_line = shell(f"df -m | grep {mount_point}", capture_output=True, ignore_exit_code=False)
+    mount_info = [x.strip() for x in mount_info_line.stdout.split(" ") if x]
+    assert mount_info[0] == device, ("Nvme Mount Failed")
+    assert mount_info[1] == size, ("Nvme Mount failed")
+    assert ((shell(f"cat /mnt/bar", capture_output=True, ignore_exit_code=False)).stdout.strip() == 'foo')
+    shell("./configure_nvme.sh disconnect", capture_output=True, ignore_exit_code=False)
+>>>>>>> c3b66280 (Port nvme test to new test framework)
