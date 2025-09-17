02f947b599cf5c55eb3fc399392d6b3d7466d182
Tiara Lena Hock <hock@b1-systems.de>
2025-09-17 17:32:01 +0200

    Create class abstraction for PAM entries

--- tests-ng/test_password_hashes.py
+++ tests-ng/test_password_hashes.py
@@ -1,32 +1,41 @@
-import re
-
 import pytest
 
 
 @pytest.mark.security_id(325)
-def test_password_entry_present(pam_candidates):
+def test_password_entry_present(pam_config):
     """
     Ensure that exactly one password entry with [success=... default=ignore]
     exists in PAM config.
     """
+    candidates = pam_config.find_entries(
+        type_="password", success="*", default="ignore"
+    )
     assert (
-        len(pam_candidates) == 1
-    ), f"Expected exactly one password entry, found {len(pam_candidates)}: {pam_candidates}"
+        len(candidates) == 1
+    ), f"Expected exactly one password entry, found {len(candidates)}: {candidates}"
 
 
 @pytest.mark.security_id(325)
-def test_password_entry_uses_strong_hash(pam_candidates):
+def test_password_entry_uses_strong_hash(pam_config):
     """
     Ensure that the password entry uses a strong hash algorithm (yescrypt or sha512).
     """
-    assert (
-        len(pam_candidates) == 1
-    ), "No valid password entry to check for hash algorithm"
-    test_line = pam_candidates[0]
+    candidates = pam_config.find_entries(
+        type_="password", success="*", default="ignore"
+    )
 
-    has_yescrypt = re.search(r"\byescrypt\b", test_line, re.IGNORECASE)
-    has_sha512 = re.search(r"\bsha512\b", test_line, re.IGNORECASE)
-
+    # Validate that this is only defined a single time
+    # to ensure no feature has appended different options
+    # multiple times
     assert (
-        has_yescrypt or has_sha512
-    ), f"No 'yescrypt' or 'sha512' found in password entry: {test_line}"
+        len(candidates) == 1
+    ), "Expected exactly one 'password ... [success=... default=ignore] ...' entry"
+    entry = candidates[0]
+
+    # Validate the entry for 'sha512' or 'yescrypt'
+    # We're using YESCRYPT instead of sha512 because it offers more
+    # resistance to offline attacks.
+    # https://www.openwall.com/yescrypt/
+    assert (
+        "yescrypt" in entry.options or "sha512" in entry.options
+    ), f"Weak or unknown hash algorithm used: {entry.options}"
