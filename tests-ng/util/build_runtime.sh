1e880fceb8a82b6cdf3f21227d06c559f6afebb3
Vivus Ignis <ignis@posteo.de>
2025-09-25 15:18:16 +0200

    retry install in the test container

--- tests-ng/util/build_runtime.sh
+++ tests-ng/util/build_runtime.sh
@@ -21,7 +21,7 @@ if [ "$0" != /init ]; then
 	cat >"$tmpdir/Containerfile" <<-'EOF'
 		FROM debian:stable
 		RUN apt-get update \
-		&& DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends ca-certificates curl
+		&& DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends ca-certificates curl retry
 	EOF
 
 	podman build -q --iidfile "$tmpdir/image_id" "$tmpdir" >/dev/null
