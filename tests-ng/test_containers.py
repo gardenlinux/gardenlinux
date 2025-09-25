6f9b6783d9b3d80d270a91333ea5fc1ba7074710
Eike Waldt <waldt@b1-systems.de>
2025-09-25 17:08:21 +0200

    fix: test-ng: fix test_containers.py teardown

--- tests-ng/test_containers.py
+++ tests-ng/test_containers.py
@@ -1,5 +1,6 @@
 import pytest
 from plugins.containerd import CtrRunner
+from handlers.containerd import containerd
 
 TEST_IMAGES = [
     "docker.io/library/busybox:latest",                       # Docker Hub, https://hub.docker.com/_/busybox
@@ -10,6 +11,6 @@ TEST_IMAGES = [
 @pytest.mark.root(reason="Needs to start containerd")
 @pytest.mark.feature("(gardener or chost or _debug) and not _pxe", reason="containerd is not installed, pxe has tmpfs for /")
 @pytest.mark.parametrize("uri", TEST_IMAGES)
-def test_basic_container_functionality(container_image_setup, uri: str, ctr: CtrRunner):
+def test_basic_container_functionality(containerd, container_image_setup, uri: str, ctr: CtrRunner):
     out = ctr.run(uri, "uname", capture_output=True, ignore_exit_code=True)
     assert "Linux" in out.stdout, f"Command failed: {out.stderr}"
