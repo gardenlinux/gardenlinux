7817b8c62b8171146ecefdce2fef7f14af85b6dd
Eike Waldt <waldt@b1-systems.de>
2025-09-25 11:00:20 +0200

    fix: test-ng: test_nvme.py: use writable directory to not fail on trustedboot and USI flavors

--- tests-ng/test_nvme.py
+++ tests-ng/test_nvme.py
@@ -24,9 +24,9 @@ def test_kernel_module_availability(module_name, shell: ShellRunner):
 @pytest.mark.modify
 @pytest.mark.feature("nvme")
 def test_nvme_locally(nvme_device, shell: ShellRunner):
-    device, mount_point, size = nvme_device
-    mount_info_line = shell(f"df -m | grep {mount_point}", capture_output=True)
+    device, mount_dir, size = nvme_device
+    mount_info_line = shell(f"df -m | grep {mount_dir}", capture_output=True)
     mount_info = [x.strip() for x in mount_info_line.stdout.split(" ") if x]
     assert mount_info[0] == device
     assert mount_info[1] == size
-    assert Path("/mnt/nvme/bar").read_text().strip() == "foo", "NVME Test failed"
+    assert Path(f"{mount_dir}/bar").read_text().strip() == "foo", "NVME Test failed"
