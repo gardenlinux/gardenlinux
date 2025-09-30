6f7a7b1920854423f5f4433ced3c168a1a48ed15
Florian Wilhelm <2292245+fwilhe@users.noreply.github.com>
2025-09-30 16:58:34 +0200

    tests-ng: Fix type in equals function (#3527)
    Extracted from #3518 by @ByteOtter 
    
    Good finding, thanks!

--- tests-ng/plugins/utils.py
+++ tests-ng/plugins/utils.py
@@ -3,7 +3,7 @@ import pytest
 # Various utility functions to make tests more readable
 # This should not contain test-assertions, but only abstract details that make tests harder to read
 
-def equals_ignore_case(a: str, b: str) -> str:
+def equals_ignore_case(a: str, b: str) -> bool:
     return a.casefold() == b.casefold()
 
 def get_normalized_sets(*sets: set) -> tuple[set, ...]:
