07e5e43151bc95aaf080b6d733c4b8351fb69602
Leon Kniffki <leon.kniffki@sap.com>
2025-10-01 13:13:23 +0200

    add modify marker and assertion message

--- tests-ng/test_systemd_unit.py
+++ tests-ng/test_systemd_unit.py
@@ -12,10 +12,11 @@ units = {
 
 
 @pytest.mark.parametrize(
-    "systemd_unit", [pytest.param(units[feature], marks=pytest.mark.feature(feature)) for feature in units]
+    "systemd_unit,feature", [pytest.param(units[feature], feature, marks=pytest.mark.feature(feature)) for feature in units]
 )
 @pytest.mark.booted(reason="Test runs systemd")
 @pytest.mark.root(reason="To start the systemd service")
-def test_systemd_unit(systemd: Systemd, systemd_unit):
+@pytest.mark.modify(reason="Starts systemd service")
+def test_systemd_unit(systemd: Systemd, systemd_unit, feature):
     systemd.start_unit(systemd_unit)
-    assert systemd.is_active(systemd_unit)
+    assert systemd.is_active(systemd_unit), f"Required systemd unit {systemd_unit} for {feature} feature is not running"
