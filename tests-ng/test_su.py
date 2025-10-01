18084a3f29a34a0b6a2b4a2cfbae9d5792deb8d4
Bernd Ritter <ritter@b1-systems.de>
2025-10-01 11:08:02 +0200

    adopt indirect parameters for PamConfig test

--- tests-ng/test_su.py
+++ tests-ng/test_su.py
@@ -1,7 +1,10 @@
 import pytest
 
+from plugins.password_hashes import PamConfig
+
 @pytest.mark.root
-def test_pam_wheel_is_required():
+@pytest.mark.parametrize("pam_config", [("/etc/pam.d/su")], indirect=["pam_config"])
+def test_pam_wheel_is_required(pam_config: PamConfig):
     """
     Validate that we have access to su restircted by default.  The test will
     read the file from `/etc/pam.d/su` and check if it has the configuration
@@ -10,11 +13,27 @@ def test_pam_wheel_is_required():
     A user needs to be configured to be admitted to use `su`. By default, this
     is realized by a specify group, the `wheel` group. We have to ensure that
     this behavior is enabled.
-    """    
-    with open("/etc/pam.d/su", "r") as f:
-        # ignore comments and blank lines
-        entries = [line.strip() for line in f.readlines() if not line.strip().startswith("#") and len(line.strip()) > 0]
 
-        assert any(
-            "auth" in entry and "pam_wheel.so" in entry for entry in entries
-        ), "Control value of PAM Module pam_wheel.so must be set to required"
+    This line should be defined:
+    
+        auth       required pam_wheel.so
+
+    """
+    pam_config.find_entries()
+
+    # TODO: Lacks test for "required"
+    candidates = pam_config.find_entries(
+        type_="auth", module_contains="pam_wheel.so"
+    )
+
+    assert (
+        len(candidates) == 1
+    ), "Control value of PAM Module pam_wheel.so must be set to required"
+
+    # with open("/etc/pam.d/su", "r") as f:
+    #     # ignore comments and blank lines
+    #     entries = [line.strip() for line in f.readlines() if not line.strip().startswith("#") and len(line.strip()) > 0]
+
+    #     assert any(
+    #         "auth" in entry and "pam_wheel.so" in entry for entry in entries
+    #     ), "Control value of PAM Module pam_wheel.so must be set to required"
