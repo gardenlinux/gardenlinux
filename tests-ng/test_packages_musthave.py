c178ed552d49cb7c0107f57b38a3b0dc7dc25956
Leon Kniffki <leon.kniffki@sap.com>
2025-10-07 16:06:14 +0200

    remove booted mark

--- tests-ng/test_packages_musthave.py
+++ tests-ng/test_packages_musthave.py
@@ -58,7 +58,6 @@ features = {
 @pytest.mark.parametrize(
     "current,ignore", [pytest.param(feature, features[feature], marks=pytest.mark.feature(feature), id=feature) for feature in features]
 )
-@pytest.mark.booted(reason="Test uses dpkg")
 def test_packages_musthave(shell: ShellRunner, dpkg: Dpkg, current, ignore):
     """Test if the packages defined in pkg.include are installed"""
     arch = dpkg.architecture()
@@ -69,6 +68,7 @@ def test_packages_musthave(shell: ShellRunner, dpkg: Dpkg, current, ignore):
         # Some pkg.include files use the $arch variable or bash if statements, therefore this cannot be replaced by a simple pyhton function
         output = shell(f'arch="{arch}"; line="{package}"; eval "echo $line"', capture_output=True)
         package = output.stdout.strip(string.whitespace)
+        print(package)
         if package != "":
             packages.add(package)
 
@@ -79,6 +79,11 @@ def test_packages_musthave(shell: ShellRunner, dpkg: Dpkg, current, ignore):
         exclude = exclude.union(parse_pkg(feature, "pkg.exclude"))
 
     required = packages - exclude
+
+    
+
+    print(packages)
+    print(exclude)
     
     missing = set()
     for package in required:        
@@ -87,3 +92,82 @@ def test_packages_musthave(shell: ShellRunner, dpkg: Dpkg, current, ignore):
 
     assert len(missing) == 0, \
             f"{', '.join(sorted(missing))} should be installed, but are missing"
+
+
+"""
+
+FAILED test_packages_musthave.py::test_packages_musthave[aws] - AssertionError: cloud-init, amazon-ec2-utils should be installed, but are missing
+FAILED test_packages_musthave.py::test_packages_musthave[base] - AssertionError: garden-repo-manager should be installed, but are missing
+FAILED test_packages_musthave.py::test_packages_musthave[cloud] - AssertionError: linux-image-cloud-$arch should be installed, but are missing
+FAILED test_packages_musthave.py::test_packages_musthave[server] - AssertionError: ca-certificates, dnsutils, lsb-release, netbase, sosreport should be installed, but are missing
+
+
+FAILED test_packages_musthave.py::test_packages_musthave[aws] - AssertionError: amazon-ec2-utils, cloud-init should be installed, but are missing
+FAILED test_packages_musthave.py::test_packages_musthave[base] - AssertionError: garden-repo-manager should be installed, but are missing
+FAILED test_packages_musthave.py::test_packages_musthave[cloud] - AssertionError: linux-image-cloud-$arch should be installed, but are missing
+FAILED test_packages_musthave.py::test_packages_musthave[server] - AssertionError: ca-certificates, dnsutils, lsb-release, netbase, sosreport should be installed, but are missing
+
+
+SKIPPED [1] test_packages_musthave.py:164: excluded by feature condition: _bfpxe
+SKIPPED [1] test_packages_musthave.py:164: excluded by feature condition: _iso
+SKIPPED [1] test_packages_musthave.py:164: excluded by feature condition: aide
+SKIPPED [1] test_packages_musthave.py:164: excluded by feature condition: azure
+SKIPPED [1] test_packages_musthave.py:164: excluded by feature condition: bluefield
+SKIPPED [1] test_packages_musthave.py:164: excluded by feature condition: clamav
+SKIPPED [1] test_packages_musthave.py:164: excluded by feature condition: fedramp
+SKIPPED [1] test_packages_musthave.py:164: excluded by feature condition: openstack
+SKIPPED [1] test_packages_musthave.py:164: excluded by feature condition: openstackbaremetal
+SKIPPED [1] test_packages_musthave.py:164: excluded by feature condition: vmware
+
+
+aws
+PASSED test_packages_musthave.py::test_packages_musthave[aws]
+PASSED test_packages_musthave.py::test_packages_musthave[base]
+PASSED test_packages_musthave.py::test_packages_musthave[cloud]
+PASSED test_packages_musthave.py::test_packages_musthave[gardener]
+PASSED test_packages_musthave.py::test_packages_musthave[iscsi]
+PASSED test_packages_musthave.py::test_packages_musthave[nvme]
+PASSED test_packages_musthave.py::test_packages_musthave[server]
+
+metal-capi-arm64
+PASSED test_packages_musthave.py::test_packages_musthave[_pxe]
+PASSED test_packages_musthave.py::test_packages_musthave[base]
+PASSED test_packages_musthave.py::test_packages_musthave[chost]
+PASSED test_packages_musthave.py::test_packages_musthave[khost]
+PASSED test_packages_musthave.py::test_packages_musthave[metal]
+PASSED test_packages_musthave.py::test_packages_musthave[server]
+
+metal-vhost-arm64
+PASSED test_packages_musthave.py::test_packages_musthave[_selinux]
+PASSED test_packages_musthave.py::test_packages_musthave[base]
+PASSED test_packages_musthave.py::test_packages_musthave[firewall]
+PASSED test_packages_musthave.py::test_packages_musthave[metal]
+PASSED test_packages_musthave.py::test_packages_musthave[server]
+
+kvm-vhost_dev-arm64
+PASSED test_packages_musthave.py::test_packages_musthave[_dev]
+PASSED test_packages_musthave.py::test_packages_musthave[_selinux]
+PASSED test_packages_musthave.py::test_packages_musthave[base]
+PASSED test_packages_musthave.py::test_packages_musthave[cloud]
+PASSED test_packages_musthave.py::test_packages_musthave[firewall]
+PASSED test_packages_musthave.py::test_packages_musthave[kvm]
+PASSED test_packages_musthave.py::test_packages_musthave[server]
+PASSED test_packages_musthave.py::test_packages_musthave[vhost]
+
+gcp-gardener_prod-arm64
+PASSED test_packages_musthave.py::test_packages_musthave[base]
+PASSED test_packages_musthave.py::test_packages_musthave[cloud]
+PASSED test_packages_musthave.py::test_packages_musthave[gardener]
+PASSED test_packages_musthave.py::test_packages_musthave[gcp]
+PASSED test_packages_musthave.py::test_packages_musthave[iscsi]
+PASSED test_packages_musthave.py::test_packages_musthave[nvme]
+PASSED test_packages_musthave.py::test_packages_musthave[server]
+
+PASSED test_packages_musthave.py::test_packages_musthave[ali]
+PASSED test_packages_musthave.py::test_packages_musthave[base]
+PASSED test_packages_musthave.py::test_packages_musthave[cloud]
+PASSED test_packages_musthave.py::test_packages_musthave[gardener]
+PASSED test_packages_musthave.py::test_packages_musthave[iscsi]
+PASSED test_packages_musthave.py::test_packages_musthave[nvme]
+PASSED test_packages_musthave.py::test_packages_musthave[server]
+"""
