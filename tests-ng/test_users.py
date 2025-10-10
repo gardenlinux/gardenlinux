d6b9f86f5e7feafd25674758b886d95484cf981e
sujanas <sujana.subramaniam@sap.com>
2025-10-10 13:37:10 +0000

    Review comments updated

--- tests-ng/test_users.py
+++ tests-ng/test_users.py
@@ -6,9 +6,9 @@ import shutil
 import pytest
 
 
-def test_service_accounts_have_nologin_shell(get_uid_range):
+def test_service_accounts_have_nologin_shell(regular_user_uid_range):
     for entry in pwd.getpwall():
-        if get_uid_range.uid_min <= entry.pw_uid <= get_uid_range.uid_max:
+        if entry.pw_uid in regular_user_uid_range:
             continue
         if entry.pw_name in {"root", "sync"}:
             continue
@@ -32,21 +32,24 @@ def test_no_extra_home_directories(expected_users):
     unexpected = [e for e in entries if e not in expected_users]
     assert not unexpected, f"Unexpected entries in /home: {entries}"
 
-@pytest.mark.skipif(sudo_binary is None, reason="sudo does not exist")
+@pytest.mark.skipif((shutil.which("sudo")) is None, reason="sudo does not exist")
 @pytest.mark.booted
 @pytest.mark.root
-def test_sudo_permission_for_users(expected_users, shell: ShellRunner):
-    for user in expected_users:
-        output = shell(f"sudo -s sudo -l -U {user}", capture_output=True)
+def test_users_sudo_capability(expected_users, shell: ShellRunner):
+    for entry in pwd.getpwall():
+        output = shell(f"sudo --list --other-user={entry.pw_name}", capture_output=True)
         output_lines = output.stdout.strip().splitlines()
-        assert (len(output_lines) > 2 and 
-            "may run the following commands on" in output_lines[-2], 
-            f"User: {user} sudo permission doesn't match requirement : {output_lines[-1]}")
+        if len(output_lines) > 2:
+            if (entry.pw_name == "root") or (entry.pw_name in expected_users):
+                assert any("may run the following commands on" in line for line in output_lines), f"User: {entry.pw_name} doesn't have sudo permission"
+            else:
+                assert False, f"System User {entry.pw_name} has sudo permission"
+
 
 @pytest.mark.booted
 @pytest.mark.root
-def test_available_users(expected_users, get_uid_range):
+def test_available_users(expected_users, regular_user_uid_range):
     for entry in pwd.getpwall():
-        if get_uid_range.uid_min <= entry.pw_uid <= get_uid_range.uid_max:
+        if entry.pw_uid in regular_user_uid_range:
             assert entry.pw_name in ["dev", "nobody"] + list(expected_users), \
                     f"Unexpected user account found in /etc/passwd: {entry.pw_name}"
