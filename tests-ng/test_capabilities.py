61acdb057e1cd935f3456f966557d3f9daa4fb49
Florian Wilhelm <florian.wilhelm02@sap.com>
2025-10-15 10:50:01 +0200

    PR Feedback: Don't base on features, rather use global allowlist

--- tests-ng/test_capabilities.py
+++ tests-ng/test_capabilities.py
@@ -8,28 +8,16 @@ from plugins.capabilities import Capabilities
 # /usr/bin/arping is provided by iputils-arping
 # /usr/sbin/arping is provided by arping
 # We only use iputils-arping
-expected_capabilities_by_feature = {
-    "kvm": ["/usr/bin/arping cap_net_raw=ep"],
-    "vmware": ["/usr/bin/arping cap_net_raw=ep"],
-    "capi": ["/usr/bin/arping cap_net_raw=ep"],
-    "_pxe": ["/usr/bin/arping cap_net_raw=ep"],
-}
+capabilities_allowlist = set(["/usr/bin/arping cap_net_raw=ep"])
 
 
 @pytest.mark.root(reason="Need to read files not readable by unprivileged user")
 def test_only_expected_capabilities_are_set(
-    active_features, capabilities: Capabilities
+    capabilities: Capabilities
 ):
-    expected_capabilities = set(
-        [
-            capability
-            for feature in active_features
-            for capability in expected_capabilities_by_feature.get(feature, [])
-        ]
-    )
 
     actual_capabilities = set(capabilities.list())
 
     assert (
-        actual_capabilities == expected_capabilities
-    ), f"Expected this list of capabilities {expected_capabilities} but got {actual_capabilities}"
+        actual_capabilities - capabilities_allowlist == set()
+    ), f"Cannot have more capabilities than {capabilities_allowlist} but got {actual_capabilities}"
