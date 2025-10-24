d26036c52d51727b94d377052e9d4b582f20acf6
SujanaSubr <sujana.subramaniam@sap.com>
2025-10-24 10:53:40 +0200

    Test: Port users test to new test framework (#3634)
    * users test porting
    
    ---------
    
    Co-authored-by: Florian Wilhelm <2292245+fwilhe@users.noreply.github.com>

--- tests-ng/test_users.py
+++ tests-ng/test_users.py
@@ -3,11 +3,12 @@ import pwd
 import stat
 
 import pytest
+from plugins.users import User
 
 
-def test_service_accounts_have_nologin_shell():
+def test_service_accounts_have_nologin_shell(regular_user_uid_range):
     for entry in pwd.getpwall():
-        if entry.pw_uid >= 1000:
+        if entry.pw_uid in regular_user_uid_range:
             continue
         if entry.pw_name in {"root", "sync"}:
             continue
@@ -30,3 +31,26 @@ def test_no_extra_home_directories(expected_users):
     entries = os.listdir("/home")
     unexpected = [e for e in entries if e not in expected_users]
     assert not unexpected, f"Unexpected entries in /home: {entries}"
+
+
+@pytest.mark.booted
+@pytest.mark.root(reason="Using sudo comamnd to check the access")
+def test_users_sudo_capability(get_all_users, expected_users, user: User):
+    users_with_sudo_capabilities = set(
+        [u for u in get_all_users if user.is_user_sudo(u)]
+    )
+    allowed_sudo_users = set(expected_users) | {"root", "dev"}
+    unexpected_sudo_users = users_with_sudo_capabilities - allowed_sudo_users
+
+    assert (
+        not unexpected_sudo_users
+    ), f"Unexpected sudo capability {unexpected_sudo_users}"
+
+
+def test_available_regular_users(get_regular_users, expected_users):
+    allowed_users = ["dev", "nobody"] + list(expected_users)
+    unexpected_user = [user for user in get_regular_users if user not in allowed_users]
+
+    assert (
+        not unexpected_user
+    ), f"Unexpected user account found in /etc/passwd, {unexpected_user}"
