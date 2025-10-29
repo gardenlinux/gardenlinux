72b2e48cd5f4bd385675184abf5393b7d7416346
akendo <akendo@akendo.eu>
2025-10-29 15:57:25 +0100

    Add basic tests for libgcrypt fips configuration

--- tests-ng/test_fips.py
+++ tests-ng/test_fips.py
@@ -23,3 +23,25 @@ def test_gnutls_fips_file_is_empty():
     gnutls_fips_file = os.stat("/etc/system-fips")
 
     assert gnutls_fips_file.st_size == 0, f"The /etc/system-fips is not empty."
+
+
+def test_libgcrypt_fips_file_was_created():
+    """
+    Libcgrypt requieres to have the /etc/gcrypt/fips_enabled to be present as prerequisite 
+    to enable the FIPS mode.
+
+    https://www.gnupg.org/documentation/manuals/gcrypt/Enabling-FIPS-mode.html
+    """
+    libgcrypt_fips_file = os.stat("/etc/gcrypt/fips_enabled")
+    libgcrypt_fips_file_state = S_ISREG(libgcrypt_fips_file.st_mode)
+
+    assert libgcrypt_fips_file_state, f"The /etc/system-fips file does not exist."
+
+
+def test_libgcrypt_fips_file_is_empty():
+    """
+    The /etc/gcrypt/fips_enabled should be without any content.
+    """
+    gnutls_fips_file = os.stat("/etc/gcrypt/fips_enabled")
+
+    assert gnutls_fips_file.st_size == 0, f"The /etc/gcrypt/fips_enabled is not empty."
