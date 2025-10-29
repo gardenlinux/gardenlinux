2b610ece5e7ecb16e53c22dc2452ebb725ed4d38
akendo <akendo@akendo.eu>
2025-10-29 15:56:34 +0100

    Add basic tests for gnutls fips configuration

--- tests-ng/test_fips.py
+++ tests-ng/test_fips.py
@@ -1,4 +1,25 @@
+import os
 import pytest
 
+from stat import S_ISREG
+
 def test_gnutls_fips_file_was_created():
-    pass
+    """
+    GnuTLS requieres to have the /etc/system-fips to be present as prerequisite 
+    to enable the FIPS mode.
+
+    https://www.gnutls.org/manual/html_node/FIPS140_002d2-mode.html
+    """
+    gnutls_fips_file = os.stat("/etc/system-fips")
+    gnutls_fips_file_state = S_ISREG(gnutls_fips_file.st_mode)
+
+    assert gnutls_fips_file, f"The /etc/system-fips file does not exist."
+
+
+def test_gnutls_fips_file_is_empty():
+    """
+    The /etc/system-fips should be without any content.
+    """
+    gnutls_fips_file = os.stat("/etc/system-fips")
+
+    assert gnutls_fips_file.st_size == 0, f"The /etc/system-fips is not empty."
