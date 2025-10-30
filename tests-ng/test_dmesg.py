d49ddea4265d81b7417d1693c6c1dec8915bd266
Vivus Ignis <ignis@posteo.de>
2025-10-30 11:35:55 +0100

    one test per assert, inline comments for the regex

--- tests-ng/test_dmesg.py
+++ tests-ng/test_dmesg.py
@@ -3,18 +3,29 @@ from pathlib import Path
 
 import pytest
 
+CONFIG_FILE = "/etc/sysctl.d/40-allow-nonroot-dmesg.conf"
 
-@pytest.mark.feature("server and not metal", reason="needs server feature")
-def test_dmesg_sysctl_config():
-    config_file = "/etc/sysctl.d/40-allow-nonroot-dmesg.conf"
-    assert Path(config_file).exists()
-    assert re.search(
-        r"(?m)^\s*(?!#)kernel\.dmesg_restrict\s*=\s*0", Path(config_file).read_text()
+
+@pytest.mark.feature("server and not metal")
+def test_dmesg_sysctl_config_file_exists():
+    assert Path(CONFIG_FILE).exists()
+
+
+@pytest.mark.feature("server and not metal")
+def test_dmesg_sysctl_no_restrictions_on_accessing_dmesg():
+    pattern = re.compile(
+        r"""
+            (?m)   # go multiline mode
+            ^\s*   # any whitespace in the beginning of the line is allowed
+            (?!#)  # fail the search if a comment character is found
+            kernel\.dmesg_restrict\s*=\s*0
+        """,
+        re.VERBOSE,
     )
+    assert re.search(pattern, Path(CONFIG_FILE).read_text())
 
 
-@pytest.mark.feature(
-    "server and booted and not metal", reason="sysctl needs a booted system"
-)
+@pytest.mark.booted(reason="sysctl needs a booted system")
+@pytest.mark.feature("server and not metal")
 def test_dmesg_systctl_runtime(sysctl):
     assert sysctl["kernel.dmesg_restrict"] == 0
