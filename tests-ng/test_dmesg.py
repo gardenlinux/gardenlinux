a539bed398986584a4569e11b96121ba84793ec4
Vivus Ignis <ignis@posteo.de>
2025-11-03 18:18:22 +0100

    improved features handling and coverage

--- tests-ng/test_dmesg.py
+++ tests-ng/test_dmesg.py
@@ -3,16 +3,31 @@ from pathlib import Path
 
 import pytest
 
-CONFIG_FILE = "/etc/sysctl.d/40-allow-nonroot-dmesg.conf"
+CONFIG_FILES = {
+    "gardener": "/etc/sysctl.d/40-allow-nonroot-dmesg.conf",
+    "server and not gardener": "/etc/sysctl.d/40-restric-dmesg.conf",
+    "stig": "/etc/sysctl.d/99-stig.conf",
+}
+
+# server adds /etc/sysctl.d/40-restric-dmesg.conf, gardener excludes it
+# and adds /etc/sysctl.d/40-allow-nonroot-dmesg.conf
 
 
-@pytest.mark.feature("server")
-def test_dmesg_sysctl_config_file_exists():
-    assert Path(CONFIG_FILE).exists()
+@pytest.mark.parametrize(
+    "config_file",
+    [pytest.param(value, marks=pytest.mark.feature(key)) for key, value in CONFIG_FILES.items()],
+    ids=list(CONFIG_FILES.keys()),
+)
+def test_dmesg_sysctl_config_file_exists(config_file):
+    assert Path(config_file).exists()
 
 
-@pytest.mark.feature("server")
-def test_dmesg_sysctl_no_restrictions_on_accessing_dmesg():
+@pytest.mark.parametrize(
+    "config_file",
+    [pytest.param(value, marks=pytest.mark.feature(key)) for key, value in CONFIG_FILES.items()],
+    ids=list(CONFIG_FILES.keys()),
+)
+def test_dmesg_sysctl_no_restrictions_on_accessing_dmesg(config_file):
     pattern = re.compile(
         r"""
             ^\s*    # any whitespace in the beginning of the line is allowed
@@ -21,17 +36,17 @@ def test_dmesg_sysctl_no_restrictions_on_accessing_dmesg():
         """,
         re.VERBOSE | re.MULTILINE,
     )
-    assert re.search(pattern, Path(CONFIG_FILE).read_text())
+    assert re.search(pattern, Path(config_file).read_text())
 
 
 @pytest.mark.booted(reason="sysctl needs a booted system")
-@pytest.mark.feature("server or stig")
+@pytest.mark.feature("server or gardener or stig")
 def test_dmesg_systctl_runtime(sysctl):
     assert sysctl["kernel.dmesg_restrict"] == "0"
 
 
 @pytest.mark.booted(reason="needs a booted system with dmesg restrictions loaded")
-@pytest.mark.feature("server or stig")
+@pytest.mark.feature("server or gardener or stig")
 def test_dmesg_call_by_unprivileged_user_works(shell):
     assert shell("id -u", capture_output=True).stdout.strip() != "0"
     res = shell("dmesg", capture_output=False, ignore_exit_code=True)
