d3bdf10c923c6ba1c42e79db396240984a7ce214
Bernd Ritter <ritter@b1-systems.de>
2025-11-05 16:15:41 +0100

    Test: Migrate test_groups.py to new testing framework.
    refs #3786
    
    Signed-off-by: Bernd Ritter <ritter@b1-systems.de>
    On-behalf-of: SAP <b.ritter@sap.com>

--- tests-ng/plugins/password_shadow.py
+++ tests-ng/plugins/password_shadow.py
@@ -26,3 +26,15 @@ def shadow_entries() -> List[Dict[str, str | List[str]]]:
     """
     path = Path("/etc/shadow")
     return parse_etc_file(path, field_names=["user", "passwd"], min_fields=2)
+
+
+@pytest.fixture
+def group_entries() -> List[Dict[str, str | List[str]]]:
+    """
+    Return parsed entries from /etc/group.
+    Fields: group_name, password, gid, user_list
+    """
+    path = Path("/etc/group")
+    return parse_etc_file(
+        path, field_names=["group_name", "password", "gid", "user_list"], min_fields=4
+    )
