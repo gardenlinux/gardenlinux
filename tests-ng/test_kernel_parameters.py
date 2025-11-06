c07dd25e4635afcf918f66244000819fdf570227
Vivus Ignis <ignis@posteo.de>
2025-11-06 16:13:57 +0100

    sysctl plugin returns strings

--- tests-ng/test_kernel_parameters.py
+++ tests-ng/test_kernel_parameters.py
@@ -4,16 +4,16 @@ import pytest
 @pytest.mark.feature("cloud or openstackbaremetal")
 @pytest.mark.booted(reason="sysctl needs a booted system")
 def test_kernel_parameters_cannot_hardlink_what_you_do_not_own(sysctl):
-    assert sysctl["fs.protected_hardlinks"] == 1
+    assert sysctl["fs.protected_hardlinks"] == "1"
 
 
 @pytest.mark.feature("cloud or openstackbaremetal")
 @pytest.mark.booted(reason="sysctl needs a booted system")
 def test_kernel_parameters_cannot_symlink_what_you_do_not_own(sysctl):
-    assert sysctl["fs.protected_symlinks"] == 1
+    assert sysctl["fs.protected_symlinks"] == "1"
 
 
 @pytest.mark.feature("cloud or openstackbaremetal")
 @pytest.mark.booted(reason="sysctl needs a booted system")
 def test_kernel_parameters_randomize_memory_allocation(sysctl):
-    assert sysctl["kernel.randomize_va_space"] == 2
+    assert sysctl["kernel.randomize_va_space"] == "2"
