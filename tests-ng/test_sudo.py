13bb148ba63409d73f101e656243e620fe931f05
Vivus Ignis <ignis@posteo.de>
2025-11-06 19:42:51 +0100

    better formatting for the multiline regexp

--- tests-ng/test_sudo.py
+++ tests-ng/test_sudo.py
@@ -7,7 +7,13 @@ import pytest
 @pytest.mark.feature("server", reason="needs server feature")
 def test_sudo_resets_user_environment():
     config_file = "/etc/sudoers"
-    assert Path(config_file).exists()
-    assert re.search(
-        r"(?m)^\s*(?!#)Defaults\s*env_reset", Path(config_file).read_text()
+    pattern = re.compile(
+        r"""
+        ^\s*    # any whitespace in the beginning of the line is allowed
+        (?!\#)  # fail the search if a comment character is found
+        Defaults\s*env_reset
+        """,
+        re.VERBOSE | re.MULTILINE,
     )
+    assert Path(config_file).exists()
+    assert re.search(pattern, Path(config_file).read_text())
