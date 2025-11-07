ccd076b97c75ff605f0488f3a46e169add7838a6
Vivus Ignis <ignis@posteo.de>
2025-11-07 11:36:15 +0100

    added pam faillock tests for common-password file

--- tests-ng/test_pam_faillock.py
+++ tests-ng/test_pam_faillock.py
@@ -17,3 +17,14 @@ def test_common_account_pam_faillock(file_content):
     assert file_content.check_line(
         "/etc/pam.d/common-account", "account required pam_faillock.so"
     )
+
+
+@pytest.mark.feature("server")
+def test_common_password_pam_faillock(file_content):
+    assert file_content.check_lines(
+        "/etc/pam.d/common-password",
+        [
+            "password required pam_passwdqc.so min=disabled,disabled,12,8,8 passphrase=4 similar=deny retry=5",
+            "password required pam_pwhistory.so use_authtok remember=5 retry=5",
+        ],
+    )
