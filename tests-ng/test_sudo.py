96e5e754e1712499d90b6360de45bbb342edc871
Vivus Ignis <ignis@posteo.de>
2025-11-07 11:16:19 +0100

    using file_content plugin

--- tests-ng/test_sudo.py
+++ tests-ng/test_sudo.py
@@ -1,19 +1,6 @@
-import re
-from pathlib import Path
-
 import pytest
 
 
-@pytest.mark.feature("server", reason="needs server feature")
-def test_sudo_resets_user_environment():
-    config_file = "/etc/sudoers"
-    pattern = re.compile(
-        r"""
-        ^\s*    # any whitespace in the beginning of the line is allowed
-        (?!\#)  # fail the search if a comment character is found
-        Defaults\s*env_reset
-        """,
-        re.VERBOSE | re.MULTILINE,
-    )
-    assert Path(config_file).exists()
-    assert re.search(pattern, Path(config_file).read_text())
+@pytest.mark.feature("server")
+def test_sudo_resets_user_environment(file_content):
+    assert file_content.check_line("/etc/sudoers", "Defaults env_reset")
