e7d182a1eb6e3162bfa8c40e09063034c8a2f80f
Vivus Ignis <ignis@posteo.de>
2025-11-07 11:28:39 +0100

    add pam faillock test

---
+++ tests-ng/test_pam_faillock.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+@pytest.mark.feature("server")
+def test_common_auth_pam_faillock(file_content):
+    file_content.check_lines(
+        "/etc/pam.d/common-auth",
+        [
+            "auth required pam_faillock.so preauth silent audit deny=5 unlock_time=900",
+            "auth [default=die] pam_faillock.so authfail silent audit deny=5 unlock_time=900",
+        ],
+    )
+
+
+@pytest.mark.feature("server")
+def test_common_account_pam_faillock(file_content):
+    assert file_content.check_line(
+        "/etc/pam.d/common-account", "account required pam_faillock.so"
+    )
