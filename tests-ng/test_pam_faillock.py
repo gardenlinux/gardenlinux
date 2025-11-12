989317e276d4edf8c13f9ab3e8ec71121c87a544
Vivus Ignis <ignis@posteo.de>
2025-11-12 15:09:29 +0100

    using pam module instead of file_content

--- tests-ng/test_pam_faillock.py
+++ tests-ng/test_pam_faillock.py
@@ -1,30 +1,79 @@
 import pytest
 
 
+@pytest.mark.parametrize(
+    "pam_config", ["/etc/pam.d/common-auth"], indirect=["pam_config"]
+)
 @pytest.mark.feature("server")
-def test_common_auth_pam_faillock(file_content):
-    file_content.check_lines(
-        "/etc/pam.d/common-auth",
-        [
-            "auth required pam_faillock.so preauth silent audit deny=5 unlock_time=900",
-            "auth [default=die] pam_faillock.so authfail silent audit deny=5 unlock_time=900",
+def test_common_auth_pam_faillock(pam_config):
+
+    results = pam_config.find_entries(
+        type_="auth",
+        control_contains="required",
+        module_contains="pam_faillock.so",
+        arg_contains=["preauth", "silent", "audit", "deny=5", "unlock_time=900"],
+    )
+    assert (
+        len(results) == 1
+    ), "Logins should be blocked for 900 seconds after 5 failed attempts (preauth check configured)"
+
+    pam_config.find_entries(
+        type_="auth",
+        control_contains="default=die",
+        module_contains="pam_faillock.so",
+        arg_contains=["authfail", "silent", "audit", "deny=5", "unlock_time=900"],
+    )
+    assert (
+        len(results) == 1
+    ), "Control value of PAM Module pam_faillock.so must be set to required (postauth action configured)"
+
+
+@pytest.mark.parametrize(
+    "pam_config", ["/etc/pam.d/common-account"], indirect=["pam_config"]
+)
+@pytest.mark.feature("server")
+def test_common_account_pam_faillock(pam_config):
+    results = pam_config.find_entries(
+        type_="account", control_contains="required", module_contains="pam_faillock.so"
+    )
+    assert (
+        len(results) == 1
+    ), "User account should be validated for its lock status before session starts"
+
+
+@pytest.mark.parametrize(
+    "pam_config", ["/etc/pam.d/common-password"], indirect=["pam_config"]
+)
+@pytest.mark.feature("server")
+def test_common_password_passwdqc_pam_faillock(pam_config):
+    results = pam_config.find_entries(
+        type_="password",
+        control_contains="required",
+        module_contains="pam_passwdqc.so",
+        arg_contains=[
+            "min=disabled,disabled,12,8,8",
+            "passphrase=4",
+            "similar=deny",
+            "retry=5",
         ],
     )
+    assert (
+        len(results) == 1
+    ), "User password should conform to password strength requirements"
 
 
+@pytest.mark.parametrize(
+    "pam_config", ["/etc/pam.d/common-password"], indirect=["pam_config"]
+)
 @pytest.mark.feature("server")
-def test_common_account_pam_faillock(file_content):
-    assert file_content.check_line(
-        "/etc/pam.d/common-account", "account required pam_faillock.so"
+def test_common_password_pwhistory_pam_faillock(pam_config):
+    results = pam_config.find_entries(
+        type_="password",
+        control_contains="required",
+        module_contains="pam_pwhistory.so",
+        arg_contains=["use_authtok", "remember=5", "retry=5"],
     )
+    assert (
+        len(results) == 1
+    ), "Last 5 user passwords should not be reused"
 
-
-@pytest.mark.feature("server")
-def test_common_password_pam_faillock(file_content):
-    assert file_content.check_lines(
-        "/etc/pam.d/common-password",
-        [
-            "password required pam_passwdqc.so min=disabled,disabled,12,8,8 passphrase=4 similar=deny retry=5",
-            "password required pam_pwhistory.so use_authtok remember=5 retry=5",
-        ],
-    )
