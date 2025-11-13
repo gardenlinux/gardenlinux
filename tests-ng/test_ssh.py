d367d09ab06ae4cc7997db5f6ecdd3afec75c1d3
akendo <akendo@akendo.eu>
2025-11-13 08:57:17 +0100

    Using the pymark instead of skipif for excluding FIPS from SSH test.

--- tests-ng/test_ssh.py
+++ tests-ng/test_ssh.py
@@ -54,7 +54,7 @@ required_sshd_config = {
 @pytest.mark.root(reason="Calling sshd -T requires root")
 @pytest.mark.feature("ssh")
 @pytest.mark.parametrize("sshd_config_item", required_sshd_config)
-@pytest.mark.skipif(feature="_fips", reason="FIPS uses different values")
+@pytest.mark.feature("not _fips", reason="FIPS uses different values for the KEX and Cipher.")
 def test_sshd_has_required_config(sshd_config_item: str, sshd: Sshd):
     actual_value = sshd.get_config_section(sshd_config_item)
     expected_value = required_sshd_config[sshd_config_item]
