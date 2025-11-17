dd93ae2ceb5e8bfe220e0669d63f53e063309841
Bernd Ritter <ritter@b1-systems.de>
2025-11-17 13:55:40 +0100

    Tests: Removed complexity from group-tests by integrating feedback from PR.
    refs #3786
    
    Signed-off-by: Bernd Ritter <ritter@b1-systems.de>
    On-behalf-of: SAP <b.ritter@sap.com>

--- tests-ng/test_groups.py
+++ tests-ng/test_groups.py
@@ -1,42 +1,43 @@
 from typing import Dict, List
 
 import pytest
-from plugins.password_shadow import group_entries
+from plugins.linux_etc_files import Group, group_entries
+from plugins.utils import check_for_duplicates
+
+
+def test_groups_are_unique(group_entries: List[Group]):
+    """
+    Check if there are any duplicates in /etc/group.
+    """
+    duplicates = check_for_duplicates(group_entries)
+    assert (
+        len(duplicates) == 0
+    ), f"Groups from /etc/group should not have duplicates: {duplicates}"
+
+
+def test_groups_find_duplicate(group_entries: List[Group]):
+    """
+    Check if there are any duplicates in /etc/group, by adding a duplicate.
+    """
+    duplicate_group = group_entries[0]
+    group_entries.append(duplicate_group)
+
+    duplicates = check_for_duplicates(group_entries)
+    assert (
+        len(duplicates) == 1
+    ), f"Group #{duplicate_group.groupname} should be found as duplicate, but was not."
 
 
 @pytest.mark.parametrize("group_name,user_list", [("root", []), ("wheel", [])])
 @pytest.mark.feature("not container")
 def test_groups_with_no_users(
-    group_entries: List[Dict[str, str | List[str]]], group_name, user_list
+    group_entries: List[Group], group_name: str, user_list: List[str]
 ):
     """
     Check if parameterized groups have the expected users.
     """
-    # find the parameterized group in the dataset
-    matching_groups = [
-        group_entry
-        for group_entry in group_entries
-        if group_entry["group_name"] == group_name
-    ]
-    assert (
-        len(matching_groups) == 1
-    ), f"No exact match found for group {group_name}. Found {len(matching_groups)}, expected only 1"
-
-    # we have only group available
-    grp = matching_groups[0]
-
-    if isinstance(grp["user_list"], list):
-        # list, should not happen here, as group_entries will split by : by default and not comma as expected
-        actual_user_list = grp["user_list"]
-    elif isinstance(grp["user_list"], str):
-        # single entry or empty
-        actual_user_list = grp["user_list"].strip().split(",")
-    else:
-        raise ValueError(f"user_list is of unhandled type")
-
-    # python split on an empty array will return [''] instead of []
-    actual_user_list = [a for a in actual_user_list if a.strip() != ""]
+    group = [group for group in group_entries if group.groupname == group_name][0]
 
     assert (
-        actual_user_list == user_list
-    ), f"assigned users from group {grp["group_name"]} dont match expected {user_list}. Was: {actual_user_list}"
+        group.user_list == user_list
+    ), f"assigned users from group {group.groupname} dont match expected {user_list}. Was: {group.user_list}"
