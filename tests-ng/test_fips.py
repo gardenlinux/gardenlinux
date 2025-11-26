f067279b4e6046f0f41d6bd710b35df524caa7fc
Akendo <Akendo@users.noreply.github.com>
2025-11-26 15:46:56 +0100

    Add more tests for GnuTLS (#3899)
    In this commit, we add some more advanced tests for the FIPS feature.
    
    - test_gnutls_fips_dot_hmac_file_is_presented
    - test_gnutls_fips_dot_hmac_file_is_vaild
    - test_kernel_has_fips_entry_in_procfs
    
    GnuTLS will perform a self-check based on the FIPS requirements. A file that contains an HMAC needs to be present on the target system. The tests ensures that the file was installed and contains a valid HMAC. One problem in shipping GnuTLS packages was that the computed HMAC was not valid for the shipped version of the library since the HMAC was computed for an unstripped version. 
    
    Lastly, we clean up some of the other tests to be more readable and use a plugin instead of reading the kernel cmd ourselves.
    
    The following commits were squashed in:  
    
    * Add more tests for GnuTLS
    * Remove misplace ignore for pyright.
    * Update tests-ng/test_fips.py
    * Remove trace of refactor code
    * Simplify tests for file check and clean up
    * Using os.path.ispath for test_gnutls_fips_dot_hmac_file_is_presented
    * Remove unused import
    * Refactor the test_kernel_was_boot_with_fips_mode, add extra test for procfs
    * Some love for black.
    * Sorting imports
    * Add missing import for pyright
    * Fix the path for gnutls test
    * The cleanup was to much, fix it 😛
    * Feedback
    * Remove newline from test
    
    ---------
    
    Co-authored-by: Moritz Kirchner <moritz.kirchner@sap.com>
    
    Relates: #3898

--- tests-ng/test_fips.py
+++ tests-ng/test_fips.py
@@ -1,7 +1,15 @@
+import configparser
+import hmac
 import os
-from stat import S_ISREG
+from hashlib import _hashlib  # type: ignore
+from hashlib import md5 as MD5
+from hashlib import sha1 as SHA1
+from hashlib import sha256 as SHA256
+from platform import machine as arch
+from typing import List
 
 import pytest
+from plugins.kernel_cmdline import kernel_cmdline
 
 
 @pytest.mark.feature("_fips")
@@ -12,10 +20,10 @@ def test_gnutls_fips_file_was_created():
 
     https://www.gnutls.org/manual/html_node/FIPS140_002d2-mode.html
     """
-    gnutls_fips_file = os.stat("/etc/system-fips")
-    gnutls_fips_file_state = S_ISREG(gnutls_fips_file.st_mode)
-
-    assert gnutls_fips_file, f"The /etc/system-fips file does not exist."
+    gnutls_fips_file = "/etc/system-fips"
+    assert os.path.isfile(
+        gnutls_fips_file
+    ), f"The {gnutls_fips_file} file does not exist."
 
 
 @pytest.mark.feature("_fips")
@@ -25,7 +33,52 @@ def test_gnutls_fips_file_is_empty():
     """
     gnutls_fips_file = os.stat("/etc/system-fips")
 
-    assert gnutls_fips_file.st_size == 0, f"The /etc/system-fips is not empty."
+    assert gnutls_fips_file.st_size == 0, f"The /etc/system-fips is not empty!"
+
+
+@pytest.mark.feature("_fips")
+def test_gnutls_fips_dot_hmac_file_is_presented():
+    """
+    GnuTLS will perform a self check based on the FIPS requirements. A file that
+    contains an HMAC needs to be present on the target system. This test ensures
+    that the file was installed.
+
+    https://www.gnutls.org/manual/html_node/FIPS140_002d2-mode.html
+    """
+    gnutls_fips_hmac_file = f"/usr/lib/{arch()}-linux-gnu/.libgnutls.so.30.hmac"
+    assert os.path.isfile(
+        gnutls_fips_hmac_file
+    ), f"The f{gnutls_fips_hmac_file} file does not exist!"
+
+
+@pytest.mark.feature("_fips")
+def test_gnutls_fips_dot_hmac_file_is_vaild():
+    """
+    One problem in shipping GnuTLS packages was that the computed HMAC was not valid for the shipped
+    version of the library since the HMAC was computed for an unstripped version. This test ensures
+    that the HMAC on the system fits with the shipped library version.
+
+    Without the HMAC the selftest will fail at second part with the follow error message:
+
+    |<1>| FIPS140-2 self testing part 2 failed
+
+    """
+    gnutls_lib_path = f"/usr/lib/{arch()}-linux-gnu/libgnutls.so.30"
+    gnutls_fips_hmac_file = f"/usr/lib/{arch()}-linux-gnu/.libgnutls.so.30.hmac"
+    # https://gitlab.com/gnutls/gnutls/-/blob/master/configure.ac?ref_type=heads#L677
+    SECRET = "orboDeJITITejsirpADONivirpUkvarP"
+
+    config = configparser.ConfigParser()
+    config.read(gnutls_fips_hmac_file)
+
+    fips_hmac = hmac.new(key=SECRET.encode("UTF-8"), msg=None, digestmod=SHA256)
+    # Need to read it 'b' since it's a binary file.
+    with open(gnutls_lib_path, mode="rb") as lib:
+        fips_hmac.update(lib.read())
+
+    assert (
+        config["libgnutls.so.30"]["hmac"] == fips_hmac.hexdigest()
+    ), "Compute HMAC is incorrect!"
 
 
 @pytest.mark.feature("_fips")
@@ -36,10 +89,10 @@ def test_libgcrypt_fips_file_was_created():
 
     https://www.gnupg.org/documentation/manuals/gcrypt/Enabling-FIPS-mode.html
     """
-    libgcrypt_fips_file = os.stat("/etc/gcrypt/fips_enabled")
-    libgcrypt_fips_file_state = S_ISREG(libgcrypt_fips_file.st_mode)
-
-    assert libgcrypt_fips_file_state, f"The /etc/system-fips file does not exist."
+    gcrypt_fips_file = "/etc/gcrypt/fips_enabled"
+    assert os.path.isfile(
+        gcrypt_fips_file
+    ), f"The {gcrypt_fips_file} file does not exist."
 
 
 @pytest.mark.feature("_fips")
@@ -48,8 +101,7 @@ def test_libgcrypt_fips_file_is_empty():
     The /etc/gcrypt/fips_enabled should be without any content.
     """
     gnutls_fips_file = os.stat("/etc/gcrypt/fips_enabled")
-
-    assert gnutls_fips_file.st_size == 0, f"The /etc/gcrypt/fips_enabled is not empty."
+    assert gnutls_fips_file.st_size == 0, f"The /etc/gcrypt/fips_enabled is not empty!"
 
 
 @pytest.mark.feature("_fips")
@@ -59,10 +111,10 @@ def test_kernel_cmdline_fips_file_was_created():
     as a prerequisite to enable they respected FIPS mode. The kernel can only be booted
     with the fips=1 parameter.
     """
-    kernel_fips_file = os.stat("/etc/kernel/cmdline.d/30-fips.cfg")
-    kernel_fips_file_state = S_ISREG(kernel_fips_file.st_mode)
-
-    assert kernel_fips_file_state, f"The /etc/kernel/cmdline.d/30-fips.cfg is missing!"
+    kernel_fips_file = "/etc/kernel/cmdline.d/30-fips.cfg"
+    assert os.path.isfile(
+        kernel_fips_file
+    ), f"The /etc/kernel/cmdline.d/30-fips.cfg is missing!"
 
 
 @pytest.mark.feature("_fips")
@@ -72,16 +124,26 @@ def test_kernel_cmdline_fips_file_content():
     """
     with open("/etc/kernel/cmdline.d/30-fips.cfg") as kernel_cmd_file:
         assert (
-            kernel_cmd_file.read() == 'CMDLINE_LINUX="$CMDLINE_LINUX fips=1"\n'
+            kernel_cmd_file.read().strip() == 'CMDLINE_LINUX="$CMDLINE_LINUX fips=1"'
         ), "fips=1 wasn't set in the kernel cmdline"
 
 
 @pytest.mark.feature("_fips")
 @pytest.mark.booted(reason="Kernel test makes sense only on booted system")
-def test_kernel_was_boot_with_fips_mode():
+def test_kernel_was_boot_with_fips_mode(kernel_cmdline: List[str]):
     """
     Validate that the kernel was booted with the FIPS mode enabled.
     """
+    assert "fips=1" in kernel_cmdline, f"Kernel was not booted in FIPS mode!"
+
+
+@pytest.mark.feature("_fips")
+@pytest.mark.booted(reason="Kernel test makes sense only on booted system")
+def test_kernel_has_fips_entry_in_procfs():
+    """
+    Validate that the FIPS flag is exposed in procfs. Without applications can't detect if a system
+    has been booted into FIPS mode.
+    """
     with open("/proc/sys/crypto/fips_enabled", "r") as f:
         fips_enabled = f.read().strip()
-    assert fips_enabled == "1", f"Kernel was not booted in FIPS mode!"
+    assert fips_enabled == "1", f"Kernel does not expose FIPS in procfs!"
