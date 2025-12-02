13a4c8ba4b5ce00c2351ab95d8a7367197ef59f0
Bernd Ritter <ritter@b1-systems.de>
2025-12-02 13:32:22 +0100

    Test: Migrate test_groups.py to new testing framework. (#3808)
    * Test: Migrate test_groups.py to new testing framework.
    
    refs #3786
    
    Signed-off-by: Bernd Ritter <ritter@b1-systems.de>
    On-behalf-of: SAP <b.ritter@sap.com>

---
+++ tests-ng/test_groups.py
@@ -0,0 +1,58 @@
+from typing import List
+
+import pytest
+from plugins.linux_etc_files import Group
+from plugins.utils import check_for_duplicates
+
+
+def test_groups_are_unique(group_entries: List[Group]):
+    """
+    Check if there are any duplicate groups in /etc/group.
+    """
+    duplicates = check_for_duplicates(group_entries)
+    assert (
+        len(duplicates) == 0
+    ), f"Groups from /etc/group should not have duplicates: {duplicates}"
+
+
+def test_groups_find_duplicate(group_entries: List[Group]):
+    """
+    Check if duplication check works by adding a duplicate.
+    """
+    duplicate_group = group_entries[0]
+    group_entries.append(duplicate_group)
+
+    duplicates = check_for_duplicates(group_entries)
+    assert (
+        len(duplicates) == 1
+    ), f"Group #{duplicate_group.groupname} should be found as duplicate, but was not."
+
+
+@pytest.mark.feature("not _dev and not pythonDev")
+def test_group_root_has_no_users(group_entries: List[Group]):
+    """
+    Check if parameterized groups have the expected users.
+    """
+    group_list = [group for group in group_entries if group.groupname == "root"]
+    group = group_list[0] if group_list else None
+
+    assert group, "Group root is not present."
+
+    assert (
+        len(group.user_list) == 0
+    ), f"user group root is not empty as expected. Was: {group.user_list}"
+
+
+@pytest.mark.feature("not _dev and not pythonDev and not container")
+def test_group_wheel_has_no_users(group_entries: List[Group]):
+    """
+    Check if parameterized groups have the expected users.
+    """
+    group_list = [group for group in group_entries if group.groupname == "wheel"]
+    group = group_list[0] if group_list else None
+
+    assert group, "Group wheel is not present."
+
+    assert (
+        len(group.user_list) == 0
+    ), f"user group wheel is not empty as expected. Was: {group.user_list}"
