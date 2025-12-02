dab301d852a388d91feddbecad027f7ca59dd373
Bernd Ritter <ritter@b1-systems.de>
2025-12-02 08:25:58 +0100

    Tests: simplify test_group to avoid parameterize
    Signed-off-by: Bernd Ritter <ritter@b1-systems.de>
    On-behalf-of: SAP <b.ritter@sap.com>

--- tests-ng/test_groups.py
+++ tests-ng/test_groups.py
@@ -1,26 +1,13 @@
 from typing import List
 
 import pytest
-from plugins.linux_etc_files import Group, group_entries
+from plugins.linux_etc_files import Group
 from plugins.utils import check_for_duplicates
-
-
-@pytest.fixture(scope="function")
-def group(group_entries: List[Group], request: pytest.FixtureRequest):
-    """
-    Extract the group from the system group list.
-    """
-    group_name = request.param if hasattr(request, "param") else None
-    group_list = [group for group in group_entries if group.groupname == group_name]
-    group = group_list[0] if group_list else None
-
-    assert group, f"Group {group_name} is not present."
-    return group
 
 
 def test_groups_are_unique(group_entries: List[Group]):
     """
-    Check if there are any duplicates in /etc/group.
+    Check if there are any duplicate groups in /etc/group.
     """
     duplicates = check_for_duplicates(group_entries)
     assert (
@@ -41,27 +28,31 @@ def test_groups_find_duplicate(group_entries: List[Group]):
     ), f"Group #{duplicate_group.groupname} should be found as duplicate, but was not."
 
 
-@pytest.mark.parametrize("group, user_list", [("root", [])], indirect=["group"])
 @pytest.mark.feature("not _dev and not pythonDev")
-def test_group_root_has_no_users(
-    group_entries: List[Group], group: Group, user_list: List[str]
-):
+def test_group_root_has_no_users(group_entries: List[Group]):
     """
     Check if parameterized groups have the expected users.
     """
+    group_list = [group for group in group_entries if group.groupname == "root"]
+    group = group_list[0] if group_list else None
+
+    assert group, "Group root is not present."
+
     assert (
-        group.user_list == user_list
-    ), f"assigned users from group {group.groupname} dont match expected {user_list}. Was: {group.user_list}"
+        len(group.user_list) == 0
+    ), f"user group root is not empty as expected. Was: {group.user_list}"
 
 
-@pytest.mark.parametrize("group, user_list", [("wheel", [])], indirect=["group"])
-@pytest.mark.feature("not _dev and not pythonDev and not container")
-def test_group_wheel_has_no_users(
-    group_entries: List[Group], group: Group, user_list: List[str]
-):
+@pytest.mark.feature("not _dev and not pythonDev")
+def test_group_wheel_has_no_users(group_entries: List[Group]):
     """
     Check if parameterized groups have the expected users.
     """
+    group_list = [group for group in group_entries if group.groupname == "wheel"]
+    group = group_list[0] if group_list else None
+
+    assert group, "Group wheel is not present."
+
     assert (
-        group.user_list == user_list
-    ), f"assigned users from group {group.groupname} dont match expected {user_list}. Was: {group.user_list}"
+        len(group.user_list) == 0
+    ), f"user group wheel is not empty as expected. Was: {group.user_list}"
