d37b0d0d65e39c9fcdf911a46e09c5a9a1c41dba
Tiara Lena Hock <hock@b1-systems.de>
2025-12-02 11:44:46 +0100

    Fix: Fix Azure startup time requirement in old test framework (#3991)

--- features/azure/test/test_azure_times.py
+++ features/azure/test/test_azure_times.py
@@ -1,11 +1,11 @@
 import pytest
-from helper.utils import get_architecture
 from helper.sshclient import RemoteClient
+from helper.utils import get_architecture
 
 
 def test_startup_time(client, non_provisioner_chroot, non_kvm):
-    """ Test for startup time """
-    tolerated_kernel_time = 30
+    """Test for startup time"""
+    tolerated_kernel_time = 40
     tolerated_userspace_time = 60
     (exit_code, output, error) = client.execute_command("systemd-analyze")
     assert exit_code == 0, f"no {error=} expected"
@@ -14,16 +14,20 @@ def test_startup_time(client, non_provisioner_chroot, non_kvm):
     time_initrd = 0
     for i, v in enumerate(items):
         if v == "(kernel)":
-            time_kernel = items[i-1]
+            time_kernel = items[i - 1]
         if v == "(initrd)":
-            time_initrd = items[i-1]
+            time_initrd = items[i - 1]
         if v == "(userspace)":
-            time_userspace = items[i-1]
-    if len(time_kernel) >2 and time_kernel[-2:] == "ms":
+            time_userspace = items[i - 1]
+    if len(time_kernel) > 2 and time_kernel[-2:] == "ms":
         time_kernel = str(float(time_kernel[:-2]) / 1000.0) + "s"
-    if len(time_initrd) >2 and time_initrd[-2:] == "ms":
+    if len(time_initrd) > 2 and time_initrd[-2:] == "ms":
         time_initrd = str(float(time_initrd[:-2]) / 1000.0) + "s"
     tf_kernel = float(time_kernel[:-1]) + float(time_initrd[:-1])
     tf_userspace = float(time_userspace[:-1])
-    assert tf_kernel < tolerated_kernel_time, f"startup time in kernel space too long: {tf_kernel} seconds =  but only {tolerated_kernel_time} tolerated."
-    assert tf_userspace < tolerated_userspace_time, f"startup time in user space too long: {tf_userspace}seconds but only {tolerated_userspace_time} tolerated."
+    assert (
+        tf_kernel < tolerated_kernel_time
+    ), f"startup time in kernel space too long: {tf_kernel} seconds =  but only {tolerated_kernel_time} tolerated."
+    assert (
+        tf_userspace < tolerated_userspace_time
+    ), f"startup time in user space too long: {tf_userspace}seconds but only {tolerated_userspace_time} tolerated."
