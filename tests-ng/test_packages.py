983d6acf4637ea74b02bd7caec836dfd9cfca883
Moritz Kirchner <moritz.kirchner@sap.com>
2025-12-10 15:46:09 +0100

    reason added for the irqbalance: performance

--- tests-ng/test_packages.py
+++ tests-ng/test_packages.py
@@ -19,7 +19,10 @@ def test_no_denylisted_packages(denied_package: str, dpkg: Dpkg):
     ), f"Denylisted package {denied_package} is installed"
 
 
-@pytest.mark.feature("gcp")
+@pytest.mark.feature(
+    "gcp",
+    reason="To ensure high performance network and disk capability, disable or remove the irqbalance daemon. This daemon does not correctly balance IRQ requests for the guest operating systems on virtual machine (VM) instances.",
+)  # https://docs.cloud.google.com/compute/docs/import/configuring-imported-images
 def test_package_irqbalance_not_installed_on_gcp(dpkg: Dpkg):
     assert not dpkg.package_is_installed(
         "irqbalance"
