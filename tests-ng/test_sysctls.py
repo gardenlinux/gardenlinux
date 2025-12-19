96f9e7f7fe1a68adc3299398a4ca71cc995b4fdb
Saubhik Datta <saubhikdatta@gmail.com>
2025-12-19 10:49:50 +0100

    skip 2 tests for CIS compliance

--- tests-ng/test_sysctls.py
+++ tests-ng/test_sysctls.py
@@ -2,6 +2,7 @@ import pytest
 
 
 @pytest.mark.booted
+@pytest.mark.feature("not cis", reason="CIS needs rp_filter to be strict")
 def test_sysctl_rp_filter(sysctl):
     assert sysctl["net.ipv4.conf.all.rp_filter"] != 1
     assert sysctl["net.ipv4.conf.default.rp_filter"] != 1
