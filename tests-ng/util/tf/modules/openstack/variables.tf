84e0998e732b7e7e478dd192c6eb1d0e1c9315d1
Eike Waldt <waldt@b1-systems.de>
2026-01-15 08:14:44 +0100

    fix: add missing skip_tests variable to all cloud providers
    Signed-off-by: Eike Waldt <waldt@b1-systems.de>
    On-behalf-of: SAP <eike.waldt@sap.com>

--- tests-ng/util/tf/modules/openstack/variables.tf
+++ tests-ng/util/tf/modules/openstack/variables.tf
@@ -72,3 +72,9 @@ variable "provider_vars" {
     # feature_trustedboot = optional(bool, false)
   })
 }
+
+variable "skip_tests" {
+  description = "Skip deploying test disk and running tests"
+  type        = bool
+  default     = false
+}
