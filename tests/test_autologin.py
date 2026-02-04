670c194da44aa72853f63d42631763574c4a6cf3
Eike Waldt <waldt@b1-systems.de>
2026-02-04 14:01:31 +0100

    feat: add setting identifiers to tests
    Signed-off-by: Eike Waldt <waldt@b1-systems.de>
    On-behalf-of: SAP <eike.waldt@sap.com>

--- tests/test_autologin.py
+++ tests/test_autologin.py
@@ -9,6 +9,12 @@ CONFIG_FILES = [
 ]
 
 
+@pytest.mark.setting_ids(
+    [
+        "GL-SET-_iso-service-getty-tty1-autologin",
+        "GL-SET-_iso-service-serial-getty-autologin",
+    ]
+)
 @pytest.mark.parametrize("config_file", CONFIG_FILES)
 @pytest.mark.feature("server and (_dev or _iso)", reason="needs systemd")
 def test_autologin(config_file, parse_file: ParseFile):
