a7a6eda0a28f777d44cfd0905b652f165a0236a7
Eike Waldt <waldt@b1-systems.de>
2026-02-04 14:01:31 +0100

    feat: add setting identifiers to tests
    Signed-off-by: Eike Waldt <waldt@b1-systems.de>
    On-behalf-of: SAP <eike.waldt@sap.com>

--- tests/test_lsm.py
+++ tests/test_lsm.py
@@ -1,6 +1,24 @@
+from typing import List
+
 import pytest
+from plugins.file import File
 
 
+# SELinux
+@pytest.mark.setting_ids(["GL-SET-_selinux-config-kernel-cmdline-lsm"])
+@pytest.mark.feature("_selinux")
+@pytest.mark.booted(reason="requires a running kernel to read /proc/cmdline")
+def test_selinux_cmdline(kernel_cmdline: List[str]):
+    assert "security=selinux" in kernel_cmdline
+
+
+@pytest.mark.feature("_selinux")
+@pytest.mark.booted(reason="requires a running kernel to access sysfs")
+def test_selinux_enabled(file: File):
+    assert file.exists("/sys/fs/selinux/enforce"), "SELinux not enabled"
+
+
+@pytest.mark.setting_ids(["GL-SET-_selinux-config-kernel-cmdline-lsm"])
 @pytest.mark.booted
 @pytest.mark.feature("_selinux")
 def test_lsm_selinux(lsm):
@@ -8,6 +26,15 @@ def test_lsm_selinux(lsm):
     assert "apparmor" not in lsm
 
 
+# Apparmor
+@pytest.mark.setting_ids(["GL-SET-gardener-config-kernel-cmdline-lsm"])
+@pytest.mark.feature("gardener")
+@pytest.mark.booted(reason="requires a running kernel to read /proc/cmdline")
+def test_apparmor_cmdline(kernel_cmdline: List[str]):
+    assert "security=apparmor" in kernel_cmdline
+
+
+@pytest.mark.setting_ids(["GL-SET-gardener-config-kernel-cmdline-lsm"])
 @pytest.mark.booted
 @pytest.mark.feature("gardener")
 def test_lsm_gardener(lsm):
