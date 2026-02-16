b9934422856a5f5ea5e9713e38c7cc1f21e2462a
Saubhik Datta <saubhikdatta@gmail.com>
2026-02-16 19:27:16 +0100

    SRG-OS-000755-GPOS-00220 TMOUT

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -1,3 +1,5 @@
+import re
+
 import pytest
 from plugins.parse_file import ParseFile
 
@@ -32,7 +34,9 @@ def test_profile_autologout_stig(parse_file: ParseFile):
     ), f"Could not find expected lines in order in {file}: {lines_list}"
 
 
-@pytest.mark.feature("openstack and metal", reason="enabled in openstack-metal feature")
+@pytest.mark.feature(
+    "openstackbaremetal", reason="enabled in openstackbaremetal feature"
+)
 def test_profile_autologout_openstackbaremetal(parse_file: ParseFile):
     """Test that the autologout profile is set correctly."""
     file = "/etc/profile.d/50-autologout.sh"
@@ -45,3 +49,96 @@ def test_profile_autologout_openstackbaremetal(parse_file: ParseFile):
     assert (
         lines_list in sorted_lines
     ), f"Could not find expected lines in order in {file}: {lines_list}"
+
+TMOUT_FILE_STIG = "/etc/profile.d/99-terminal_tmout.sh"
+@pytest.mark.feature("stig")
+def test_shell_tmout_file_exists_stig(parse_file: ParseFile):
+    """
+    As per DISA STIG requirement, this test validates that the shell inactivity
+    timeout configuration file exists.
+    Ref: SRG-OS-000755-GPOS-00220
+    """
+    assert parse_file.exists(
+        TMOUT_FILE_STIG
+    ), "stigcompliance: shell inactivity timeout configuration file is missing"
+
+
+@pytest.mark.feature("stig")
+def test_shell_tmout_is_configured_stig(parse_file: ParseFile):
+    """
+    As per DISA STIG requirement, this test verifies that TMOUT variable is configured.
+    Ref: SRG-OS-000755-GPOS-00220
+    """
+    assert re.compile(r"TMOUT\s*=\s*\d+") in parse_file.lines(
+        TMOUT_FILE_STIG
+    ), "stigcompliance: TMOUT is not configured"
+
+
+@pytest.mark.feature("stig")
+def test_shell_tmout_is_readonly_stig(parse_file: ParseFile):
+    """
+    As per DISA STIG requirement, this test verifies that TMOUT is marked ReadOnly.
+    Ref: SRG-OS-000755-GPOS-00220
+    """
+    assert re.compile(r"readonly\s+TMOUT") in parse_file.lines(
+        TMOUT_FILE_STIG
+    ), "stigcompliance: TMOUT is not marked as readonly"
+
+
+@pytest.mark.feature("stig")
+def test_shell_tmout_is_exported_stig(parse_file: ParseFile):
+    """
+    As per DISA STIG requirement, this test verifies that TMOUT is
+    exported to subshells.
+    Ref: SRG-OS-000755-GPOS-00220
+    """
+    assert re.compile(r"export\s+TMOUT") in parse_file.lines(
+        TMOUT_FILE_STIG
+    ), "stigcompliance: TMOUT is not exported"
+
+
+TMOUT_FILE_CLOUD = "/etc/profile.d/50-autologout.sh"
+@pytest.mark.feature("cloud and openstackbaremetal")
+def test_shell_tmout_file_exists_cloud(parse_file: ParseFile):
+    """
+    As per DISA STIG requirement, this test validates that the shell inactivity
+    timeout configuration file exists.
+    Ref: SRG-OS-000755-GPOS-00220
+    """
+    assert parse_file.exists(
+        TMOUT_FILE_CLOUD
+    ), "stigcompliance: shell inactivity timeout configuration file is missing"
+
+
+@pytest.mark.feature("cloud and openstackbaremetal")
+def test_shell_tmout_is_configured_cloud(parse_file: ParseFile):
+    """
+    As per DISA STIG requirement, this test verifies that TMOUT variable is configured.
+    Ref: SRG-OS-000755-GPOS-00220
+    """
+    assert re.compile(r"TMOUT\s*=\s*\d+") in parse_file.lines(
+        TMOUT_FILE_CLOUD
+    ), "stigcompliance: TMOUT is not configured"
+
+
+@pytest.mark.feature("cloud and openstackbaremetal")
+def test_shell_tmout_is_readonly_cloud(parse_file: ParseFile):
+    """
+    As per DISA STIG requirement, this test verifies that TMOUT is marked ReadOnly.
+    Ref: SRG-OS-000755-GPOS-00220
+    """
+    assert re.compile(r"readonly\s+TMOUT") in parse_file.lines(
+        TMOUT_FILE_CLOUD
+    ), "stigcompliance: TMOUT is not marked as readonly"
+
+
+@pytest.mark.feature("cloud and openstackbaremetal")
+def test_shell_tmout_is_exported_cloud(parse_file: ParseFile):
+    """
+    As per DISA STIG requirement, this test verifies that TMOUT is
+    exported to subshells.
+    Ref: SRG-OS-000755-GPOS-00220
+    """
+    assert re.compile(r"export\s+TMOUT") in parse_file.lines(
+        TMOUT_FILE_CLOUD
+    ), "stigcompliance: TMOUT is not exported"
