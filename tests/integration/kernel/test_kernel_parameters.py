e971b3b6957ade9e9abd7b8261c061572dc6e905
Eike Waldt <waldt@b1-systems.de>
2026-02-18 15:05:43 +0100

    disable more gcp sysctl tests

--- tests/integration/kernel/test_kernel_parameters.py
+++ tests/integration/kernel/test_kernel_parameters.py
@@ -90,18 +90,18 @@ def test_cloud_sysctl_network_config_ipv4(sysctl):
     settings = {
         "net.ipv4.tcp_syncookies": 1,
         "net.ipv4.conf.all.accept_source_route": 0,
-        "net.ipv4.conf.all.accept_redirects": 0, # TODO: Disabled until https://github.com/gardenlinux/gardenlinux/issues/4336 is fixed.
+        # "net.ipv4.conf.all.accept_redirects": 0, # TODO: Disabled until https://github.com/gardenlinux/gardenlinux/issues/4336 is fixed.
         "net.ipv4.conf.all.secure_redirects": 1,
         "net.ipv4.conf.default.secure_redirects": 1,
         "net.ipv4.icmp_echo_ignore_broadcasts": 1,
         "net.ipv4.icmp_ignore_bogus_error_responses": 1,
-        "net.ipv4.conf.default.accept_source_route": 1,
-        "net.ipv4.conf.default.accept_redirects": 1,
-        "net.ipv4.conf.all.log_martians": 0,
-        "net.ipv4.conf.all.forwarding": 1,
-        "net.ipv4.conf.default.forwarding": 1,
-        "net.ipv4.conf.default.send_redirects": 1,
-        "net.ipv4.conf.all.send_redirects": 1,
+        # "net.ipv4.conf.default.accept_source_route": 1, # TODO: Disabled until https://github.com/gardenlinux/gardenlinux/issues/4336 is fixed.
+        # "net.ipv4.conf.default.accept_redirects": 1, # TODO: Disabled until https://github.com/gardenlinux/gardenlinux/issues/4336 is fixed.
+        # "net.ipv4.conf.all.log_martians": 0, # TODO: Disabled until https://github.com/gardenlinux/gardenlinux/issues/4336 is fixed.
+        # "net.ipv4.conf.all.forwarding": 1, # TODO: Disabled until https://github.com/gardenlinux/gardenlinux/issues/4336 is fixed.
+        # "net.ipv4.conf.default.forwarding": 1, # TODO: Disabled until https://github.com/gardenlinux/gardenlinux/issues/4336 is fixed.
+        # "net.ipv4.conf.default.send_redirects": 1,
+        # "net.ipv4.conf.all.send_redirects": 1, # TODO: Disabled until https://github.com/gardenlinux/gardenlinux/issues/4336 is fixed.
         "net.ipv4.conf.all.promote_secondaries": 1,
     }
     missing = [key for key, value in settings.items() if sysctl[key] != value]
