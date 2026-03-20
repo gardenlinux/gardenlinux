9742535aaf45a75c51e305e050c9a846c8bbd012
akendo <akendo@akendo.eu>
2026-03-20 13:43:43 +0100

    Revert "Add basic test to see if curl can be init"
    This reverts commit cba8b8cd130947cc7ba1338417787d9c17cdeed6.

--- tests-ng/test_curl.py
+++
@@ -1,7 +0,0 @@
-from ctypes import CDLL, c_int, c_void_p
-
-import pytest
-
-def test_init_libcurl():
-    libcurl = CDLL("libcurl.so.4")
-    assert libcurl.curl_global_init() == 0, "Coudn't initializ libcurl"
