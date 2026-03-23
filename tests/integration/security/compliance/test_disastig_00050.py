9826d7a4b68afc4f90f70d598f9426d73759ace2
Saubhik Datta <50126327+saubhikdattagithub@users.noreply.github.com>
2026-03-23 09:22:18 +0100

    fix attempt

--- tests/integration/security/compliance/test_disastig_00050.py
+++ tests/integration/security/compliance/test_disastig_00050.py
@@ -12,11 +12,10 @@ FORBIDDEN_SERVICES = [
 
 
 @pytest.mark.feature(
-    "not container and not gardener and not lima and not capi and not baremetal"
+    "not container"
 )
 @pytest.mark.booted(reason="requires booted system")
 @pytest.mark.root(reason="requires audit operations")
-@pytest.mark.skip(reason="no way of currently testing this")
 def test_ports_protocols_and_services_restricted(shell, systemd):
     """
     As per DISA STIG compliance requirements, its needed to verify
