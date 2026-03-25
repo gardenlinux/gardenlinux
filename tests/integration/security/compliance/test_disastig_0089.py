e77ef3d659367dfbb7343545b52a6d33a90b42d7
Vivus Ignis <ignis@posteo.de>
2026-03-25 19:22:15 +0100

    actually adding the test file

---
+++ tests/integration/security/compliance/test_disastig_0089.py
@@ -0,0 +1,36 @@
+"""
+Ref: SRG-OS-000239-GPOS-00089
+
+Verify the operating system automatically audits account modification.
+"""
+
+
+def test_audit_calling_user_group_related_utilities(audit_rule):
+    for bin in [
+        "/usr/sbin/visudo",
+        "/usr/sbin/chgpasswd",
+        "/usr/sbin/chpasswd",
+        "/usr/sbin/groupadd",
+        "/usr/sbin/groupdel",
+        "/usr/sbin/groupmod",
+        "/usr/sbin/grpck",
+        "/usr/sbin/grpconv",
+        "/usr/sbin/grpunconv",
+        "/usr/sbin/newusers",
+        "/usr/sbin/pwck",
+        "/usr/sbin/pwconv",
+        "/usr/sbin/pwunconv",
+        "/usr/sbin/shadowconfig",
+        "/usr/sbin/useradd",
+        "/usr/sbin/userdel",
+        "/usr/sbin/usermod",
+        "/usr/sbin/vipw",
+        "/usr/sbin/vigr",
+        "/usr/bin/chage",
+        "/usr/bin/chfn",
+        "/usr/bin/chsh",
+        "/usr/bin/expiry",
+        "/usr/bin/gpasswd",
+        "/usr/bin/passwd",
+    ]:
+        assert audit_rule(binary_call=bin)
