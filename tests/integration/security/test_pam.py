12882763c7a17109816131462f4ad20e6b511dc4
Vivus Ignis <ignis@posteo.de>
2026-03-25 09:04:53 +0100

    reverted the change to test_pam

--- tests/integration/security/test_pam.py
+++ tests/integration/security/test_pam.py
@@ -68,18 +68,14 @@ def test_common_account_pam_faillock(pam_config):
 )
 @pytest.mark.feature("server")
 def test_common_password_passwdqc_pam_faillock(pam_config):
-    """
-    Requirements of SRG-OS-000078-GPOS-00046, SRG-OS-000072-GPOS-00040
-    """
     results = pam_config.find_entries(
         type_="password",
         control_contains="required",
         module_contains="pam_passwdqc.so",
         arg_contains=[
-            "min=disabled,disabled,15,15,8",
+            "min=disabled,disabled,12,12,8",
             "passphrase=4",
             "similar=deny",
-            "match=7",
             "retry=5",
         ],
     )
