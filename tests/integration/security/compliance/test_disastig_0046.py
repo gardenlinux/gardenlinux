bac04953ad8d5be58dc1bfe2da85613184ff1e0f
Vivus Ignis <ignis@posteo.de>
2026-03-26 11:24:10 +0100

    fix a double use of an arg_contains array

--- tests/integration/security/compliance/test_disastig_0046.py
+++ tests/integration/security/compliance/test_disastig_0046.py
@@ -11,7 +11,6 @@ def test_common_password_passwdqc_pam_faillock(pam_config):
         control_contains="required",
         module_contains="pam_passwdqc.so",
         arg_contains=[
-            "min=disabled,disabled,12,8,8",
             "min=disabled,disabled,15,15,8",
             "passphrase=4",
             "similar=deny",
