4465f1d1446c3c56167c4f40556d232ff5c4b960
Saubhik Datta <50126327+saubhikdattagithub@users.noreply.github.com>
2026-03-26 17:37:44 +0100

    Add modify marker for DISA STIG compliance test
    Added a marker for modification requirement in DISA STIG check.

--- tests/integration/security/compliance/test_disastig_00050.py
+++ tests/integration/security/compliance/test_disastig_00050.py
@@ -14,6 +14,7 @@ FORBIDDEN_SERVICES = [
 @pytest.mark.feature("not container")
 @pytest.mark.booted(reason="requires booted system")
 @pytest.mark.root(reason="requires audit operations")
+@pytest.mark.modify(reason="required for DISA STIG check")
 def test_ports_protocols_and_services_restricted(shell):
     """
     As per DISA STIG compliance requirements, it is needed to verify
