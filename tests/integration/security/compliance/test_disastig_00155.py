06ce6729b8eb899c7bb1f59ac44acf10100b914b
Saubhik Datta <50126327+saubhikdattagithub@users.noreply.github.com>
2026-04-01 12:58:15 +0200

    Use lsm plugin

--- tests/integration/security/compliance/test_disastig_00155.py
+++ tests/integration/security/compliance/test_disastig_00155.py
@@ -1,14 +1,12 @@
 import pytest
 
-LSM_FILE = "/sys/kernel/security/lsm"
-
 
 @pytest.mark.feature(
     "not container and not gardener and not lima and not capi and not baremetal"
 )
 @pytest.mark.booted(reason="requires booted system to verify LSM enforcement")
 @pytest.mark.root(reason="requires access to system security configuration")
-def test_deny_all_execution_mechanism_present(file):
+def test_deny_all_execution_mechanism_present(lsm):
     """
     As per DISA STIG compliance requirements, the operating system must employ a
     deny-all, permit-by-exception policy to allow the execution of authorized
@@ -17,11 +15,6 @@ def test_deny_all_execution_mechanism_present(file):
     execution control (AppArmor or SELinux) is present on the system.
     Ref: SRG-OS-000370-GPOS-00155
     """
-    assert file.exists(LSM_FILE), "stigcompliance: LSM interface file not found"
-
-    with open(LSM_FILE, "r") as f:
-        lsm_list = [lsm.strip() for lsm in f.read().split(",")]
-
     assert (
-        "selinux" in lsm_list
+        "selinux" in lsm
     ), "stigcompliance: no enforcement mechanism (SELinux) present for execution control"
