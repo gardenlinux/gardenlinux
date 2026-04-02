b8bd6a65d6025e7eedd9361162f3bc78bff48d67
Vivus Ignis <ignis@posteo.de>
2026-04-02 13:50:32 +0200

    extracted setuid binaries lookup function into a plugin

--- tests/integration/security/compliance/test_disastig_0067.py
+++ tests/integration/security/compliance/test_disastig_0067.py
@@ -1,5 +1,3 @@
-import os
-import pathlib
 import pwd
 
 import pytest
@@ -33,31 +31,6 @@ SETUID_BINARIES_WHITELIST = {
     },
     "lima": {"/usr/bin/fusermount3", "/usr/bin/fusermount"},
 }
-
-
-@pytest.fixture
-def exposed_setuid_binaries():
-    """
-    Returns a set of pathnames of root-owned binaries with setuid bit which are
-    also "others"-executable (i.e. binaries that give root privileges to every
-    user).
-    """
-    dirs = [
-        "/usr/sbin",
-        "/usr/bin",
-        "/usr/libexec",
-        "/usr/local/sbin",
-        "/usr/local/bin",
-    ]
-    return {
-        str(p)
-        for d in dirs
-        for root, _, files in os.walk(d)
-        for p in map(lambda n: pathlib.Path(root) / n, files)
-        if p.stat().st_uid == 0  # file belongs to root
-        and (m := p.stat().st_mode) & 0o4000  # set‑uid bit present
-        and ((m >> 3) & 0o111)  # “others” execute bit set
-    }
 
 
 def test_only_root_user_has_uid_zero():
