60e0dfbbc8f609f7fd9cf58dd3944e38dc216d37
Vivus Ignis <ignis@posteo.de>
2026-04-09 15:53:56 +0200

    debug prints

--- tests/integration/security/compliance/test_disastig_0143.py
+++ tests/integration/security/compliance/test_disastig_0143.py
@@ -34,8 +34,8 @@ def test_time_sync_ntp_daemon_running(systemd):
 
 @pytest.mark.booted(reason="requires running systemd")
 def test_time_is_actively_synced(timedatectl, shell):
-    shell("systemctl status systemd-timesyncd")
-    shell("systemctl status chrony")
+    shell("systemctl status systemd-timesyncd", ignore_exit_code=True)
+    shell("systemctl status chrony", ignore_exit_code=True)
     assert timedatectl.get_timesync_status().ntp_synchronized
 
 
