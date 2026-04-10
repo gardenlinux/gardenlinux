41a2b253d7b7fbd2f196f0dd35ca8b3f940efecb
Vivus Ignis <ignis@posteo.de>
2026-04-10 08:24:24 +0200

    removed duplicate test; reenabled ntp sync status test

--- tests/integration/security/compliance/test_disastig_0143.py
+++ tests/integration/security/compliance/test_disastig_0143.py
@@ -15,11 +15,6 @@ second.
 """
 
 
-@pytest.mark.booted(reason="requires running systemd")
-def test_time_sync_is_enabled(timedatectl):
-    assert timedatectl.is_timesyncd_active()
-
-
 @pytest.mark.feature("azure")
 @pytest.mark.hypervisor("microsoft", reason="Need PTP timesync sources to be reachable")
 @pytest.mark.booted(reason="requires running systemd")
@@ -33,11 +28,11 @@ def test_time_sync_ntp_daemon_running(systemd):
     assert systemd.is_active("systemd-timesyncd")
 
 
-# @pytest.mark.booted(reason="requires running systemd")
-# def test_time_is_actively_synced(timedatectl, shell):
-#     shell("systemctl status systemd-timesyncd", ignore_exit_code=True)
-#     shell("systemctl status chrony", ignore_exit_code=True)
-#     assert timedatectl.get_timesync_status().ntp_synchronized
+@pytest.mark.booted(reason="requires running systemd")
+def test_time_is_actively_synced(timedatectl, shell):
+    shell("systemctl status systemd-timesyncd", ignore_exit_code=True)
+    shell("systemctl status chrony", ignore_exit_code=True)
+    assert timedatectl.get_timesync_status().ntp_synchronized
 
 
 @pytest.mark.booted(reason="requires running systemd")
