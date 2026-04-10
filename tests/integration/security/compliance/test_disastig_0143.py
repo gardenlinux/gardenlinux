6abc32a2f8e3cd93c038514ff26ba2d09b267484
Vivus Ignis <ignis@posteo.de>
2026-04-10 11:51:18 +0200

    improved annotations

--- tests/integration/security/compliance/test_disastig_0143.py
+++ tests/integration/security/compliance/test_disastig_0143.py
@@ -15,14 +15,14 @@ second.
 """
 
 
-@pytest.mark.feature("azure")
+@pytest.mark.feature("azure or gdch")
 @pytest.mark.hypervisor("microsoft", reason="Need PTP timesync sources to be reachable")
 @pytest.mark.booted(reason="requires running systemd")
 def test_time_sync_ptp_daemon_running(systemd):
     assert systemd.is_active("chrony")
 
 
-@pytest.mark.feature("not azure")
+@pytest.mark.feature("not (azure or gdch)")
 @pytest.mark.booted(reason="requires running systemd")
 def test_time_sync_ntp_daemon_running(systemd):
     assert systemd.is_active("systemd-timesyncd")
@@ -34,8 +34,6 @@ def test_time_sync_ntp_daemon_running(systemd):
 )
 @pytest.mark.booted(reason="requires running systemd")
 def test_time_is_actively_synced(timedatectl, shell):
-    shell("systemctl status systemd-timesyncd", ignore_exit_code=True)
-    shell("systemctl status chrony", ignore_exit_code=True)
     assert timedatectl.get_timesync_status().ntp_synchronized
 
 
