5caebf234c9fdeb503dbd6184e68db2fcda5e3bb
Vivus Ignis <ignis@posteo.de>
2026-04-10 14:35:23 +0200

    using md5sum binary because python hashlib is trimmed down on fips

--- tests/integration/security/compliance/test_disastig_0108.py
+++ tests/integration/security/compliance/test_disastig_0108.py
@@ -1,5 +1,3 @@
-import hashlib
-
 import pytest
 
 """
@@ -10,19 +8,11 @@ integrity of audit tools.
 """
 
 
-def _checksum_file(filepath):
-    h = hashlib.md5()
-
-    with open(filepath, "rb") as f:
-        for chunk in iter(lambda: f.read(8192), b""):
-            h.update(chunk)
-    return h.hexdigest()
-
-
 @pytest.mark.feature("not fips")
-def test_auditd_is_not_tampered(dpkg):
+def test_auditd_is_not_tampered(dpkg, shell):
     for bin in ["auditd", "auditctl", "augenrules", "aureport", "ausearch"]:
         ideal_checksum = dpkg.package_files_checksums("auditd")[f"/usr/sbin/{bin}"]
-        assert ideal_checksum == _checksum_file(
-            f"/usr/sbin/{bin}"
+        on_disk_checksum = shell(f"md5sum /usr/sbin/{bin}", capture_output=True)
+        assert (
+            ideal_checksum == on_disk_checksum
         ), f"checksum of /usr/sbin/{bin} does not match the one from the corresponding deb package"
