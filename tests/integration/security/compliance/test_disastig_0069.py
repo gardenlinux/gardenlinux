3b2ee090cd69ac6b95efe86f3f1ea5e47d685ae5
Vivus Ignis <ignis@posteo.de>
2026-04-14 17:13:10 +0200

    removed debugging

--- tests/integration/security/compliance/test_disastig_0069.py
+++ tests/integration/security/compliance/test_disastig_0069.py
@@ -14,9 +14,7 @@ transfer via shared system resources.
 def test_tmp_mount_is_configured_securely(mount):
     required_mount_options = {"nosuid"}
 
-    m = mount("/tmp")
-    print(f"==========> {m=}")
-    real_mount_options = m.options
+    real_mount_options = mount("/tmp").options
     missing_mount_options = required_mount_options - real_mount_options
 
     assert (
