b8c1236441023a2472daf46cbda0b244c23f0047
Vivus Ignis <ignis@posteo.de>
2026-04-15 11:33:36 +0200

    root permission

--- tests/integration/security/compliance/test_disastig_0108.py
+++ tests/integration/security/compliance/test_disastig_0108.py
@@ -9,6 +9,9 @@ integrity of audit tools.
 
 
 @pytest.mark.booted(reason="Requires working networking")
+@pytest.mark.root(
+    reason="Unloading crypto modules in dpkg_checksums plugin requires elevated permissions"
+)
 def test_auditd_is_not_tampered(dpkg_checksums, shell):
     checksums = dpkg_checksums.for_package("auditd")
     for bin in ["auditd", "auditctl", "augenrules", "aureport", "ausearch"]:
