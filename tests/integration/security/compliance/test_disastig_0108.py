452b484407c574988b811f82365bae4c3e021b91
Vivus Ignis <ignis@posteo.de>
2026-04-15 15:04:53 +0200

    perl is a goat

--- tests/integration/security/compliance/test_disastig_0108.py
+++ tests/integration/security/compliance/test_disastig_0108.py
@@ -13,11 +13,21 @@ integrity of audit tools.
     reason="Unloading crypto modules in dpkg_checksums plugin requires elevated permissions"
 )
 def test_auditd_is_not_tampered(dpkg_checksums, shell):
+    """
+    On a FIPS-enabled system MD5 algos are not available
+    both for a python distribution used for testing and for a cli md5sum.
+    md5 module in perl work though because it switches to a pure-perl
+    algo implementation if its binary version fails to load.
+    see: https://perldoc.perl.org/Digest::MD5.txt
+    """
     checksums = dpkg_checksums.for_package("auditd")
     for bin in ["auditd", "auditctl", "augenrules", "aureport", "ausearch"]:
         ideal_checksum = checksums[f"/usr/sbin/{bin}"]
         on_disk_checksum = (
-            shell(f"md5sum /usr/sbin/{bin}", capture_output=True)
+            shell(
+                f"perl -e 'use Digest::MD5; print Digest::MD5::md5_hex(do {{ local $/; <STDIN> }}), \"\n\"' < /usr/sbin/{bin}",
+                capture_output=True,
+            )
             .stdout.strip()
             .split()[0]
         )
