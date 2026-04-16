5c0e6e317aaccff589edc95441efe9249dae7bed
Vivus Ignis <ignis@posteo.de>
2026-04-16 09:58:28 +0200

    debug shell call

--- tests/integration/security/compliance/test_disastig_0108.py
+++ tests/integration/security/compliance/test_disastig_0108.py
@@ -12,7 +12,7 @@ integrity of audit tools.
 @pytest.mark.root(
     reason="Unloading crypto modules in dpkg_checksums plugin requires elevated permissions"
 )
-def test_auditd_is_not_tampered(dpkg_checksums, shell):
+def test_auditd_is_not_tampered(dpkg, dpkg_checksums, shell):
     """
     On a FIPS-enabled system MD5 algos are not available
     both for a python distribution used for testing and for a cli tool md5sum.
@@ -20,17 +20,22 @@ def test_auditd_is_not_tampered(dpkg_checksums, shell):
     algo implementation if its binary version fails to load.
     see: https://perldoc.perl.org/Digest::MD5.txt
     """
+    if not dpkg.package_is_installed("auditd"):
+        return
+
     checksums = dpkg_checksums.for_package("auditd")
     for bin in ["auditd", "auditctl", "augenrules", "aureport", "ausearch"]:
         ideal_checksum = checksums[f"/usr/sbin/{bin}"]
-        on_disk_checksum = (
-            shell(
-                f"perl -e 'use Digest::MD5; print Digest::MD5::md5_hex(do {{ local $/; <STDIN> }}), \"\n\"' < /usr/sbin/{bin}",
-                capture_output=True,
-            )
-            .stdout.strip()
-            .split()[0]
+        # <debug>
+        shell(
+            f"perl -e 'use Digest::MD5; print Digest::MD5::md5_hex(do {{ local $/; <STDIN> }}), \"\n\"' < /usr/sbin/{bin}"
         )
+        # </debug>
+        on_disk_checksum = shell(
+            f"perl -e 'use Digest::MD5; print Digest::MD5::md5_hex(do {{ local $/; <STDIN> }}), \"\n\"' < /usr/sbin/{bin}",
+            capture_output=True,
+        ).stdout.strip()
+
         assert (
             ideal_checksum == on_disk_checksum
         ), f"checksum of /usr/sbin/{bin} does not match the one from the corresponding deb package"
