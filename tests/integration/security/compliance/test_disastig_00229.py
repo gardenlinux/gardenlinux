101c1485e2ef320fc552396111afd2bee2b2e40d
Vivus Ignis <ignis@posteo.de>
2026-04-21 13:04:42 +0200

    autologin test

---
+++ tests/integration/security/compliance/test_disastig_00229.py
@@ -0,0 +1,14 @@
+"""
+Ref: SRG-OS-000480-GPOS-00229
+
+Verify the operating system does not allow an unattended or automatic logon to
+the system.
+"""
+
+
+def test_systemd_getty_autologin_is_not_enabled(systemd):
+    for i in range(7):
+        props = systemd.get_unit_properties(f"getty@tty{i}")
+        assert "--autologin" not in props.get(
+            "ExecStart", ""
+        ), f"Autologin enabled on getty@tty{i}"
