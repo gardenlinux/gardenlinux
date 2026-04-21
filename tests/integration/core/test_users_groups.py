a9860cab43fdc0b49f0190a37ede8270589b692c
Saubhik Datta <50126327+saubhikdattagithub@users.noreply.github.com>
2026-04-21 14:32:16 +0200

    Update test-users-groups tests for disaSTIG feature restructure

--- tests/integration/core/test_users_groups.py
+++ tests/integration/core/test_users_groups.py
@@ -29,7 +29,7 @@ def test_root_home_permissions(file: File):
     assert file.has_mode("/root", "0700"), "/root has incorrect permissions"
 
 
-@pytest.mark.feature("not _dev")
+@pytest.mark.feature("not _dev and not disaSTIGmedium")
 def test_no_extra_home_directories(expected_users, file: File):
     if file.is_symlink("/home"):
         return
@@ -41,6 +41,7 @@ def test_no_extra_home_directories(expected_users, file: File):
 @pytest.mark.testcov(["GL-TESTCOV-aws-config-cloud-user-sudo"])
 @pytest.mark.booted
 @pytest.mark.root(reason="Using sudo command to check the access")
+@pytest.mark.feature("not disaSTIGmedium")
 def test_users_sudo_capability(get_all_users, expected_users, user: User):
     users_with_sudo_capabilities = set(
         [u for u in get_all_users if user.is_user_sudo(u)]
@@ -53,6 +54,7 @@ def test_users_sudo_capability(get_all_users, expected_users, user: User):
     ), f"Unexpected sudo capability {unexpected_sudo_users}"
 
 
+@pytest.mark.feature("not disaSTIGmedium")
 def test_available_regular_users(get_regular_users, expected_users):
     allowed_users = ["dev", "nobody"] + list(expected_users)
     unexpected_user = [user for user in get_regular_users if user not in allowed_users]
