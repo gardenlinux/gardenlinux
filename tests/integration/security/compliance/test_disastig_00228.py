5b82398c345c85e7420fb86c4b3291ecd36a78a2
Vivus Ignis <ignis@posteo.de>
2026-04-22 18:04:34 +0200

    user can only read his files

---
+++ tests/integration/security/compliance/test_disastig_00228.py
@@ -0,0 +1,19 @@
+import glob
+
+"""
+Ref: SRG-OS-000480-GPOS-00228
+
+Verify the operating system defines default permissions for all authenticated
+users in such a way that the user can only read and modify their own files.
+"""
+
+
+def test_umask_is_restrictive_enough(parse_file):
+    config = parse_file.parse("/etc/login.defs", format="spacedelim")
+    assert config["UMASK"] == "027"
+
+
+def test_skeleton_files_are_not_world_writable(file):
+    assert file.has_permissions("/etc/skel", "755")
+    for f in glob.glob("/etc/skel/.*"):
+        assert file.has_permissions(f, "640")
