75fc51eb357a99b73cf8b7d1787bbdaaaf45fb3e
Saubhik Datta <50126327+saubhikdattagithub@users.noreply.github.com>
2026-04-27 16:04:38 +0200

    fix: remove test_x11_use_localhost_is_enabled as X11UseLocalhost is not part of STIG 00227 requirement

--- tests/integration/security/compliance/test_disastig_00227.py
+++ tests/integration/security/compliance/test_disastig_00227.py
@@ -15,18 +15,3 @@ def test_x11_forwarding_is_disabled(sshd: Sshd):
     assert (
         sshd.get_config_section("x11forwarding") == "no"
     ), "stigcompliance: X11Forwarding must be set to no in sshd configuration"
-
-
-@pytest.mark.feature("disaSTIGmedium")
-@pytest.mark.booted(reason="requires SSH runtime configuration")
-@pytest.mark.root(reason="requires access to SSH configuration")
-def test_x11_use_localhost_is_enabled(sshd: Sshd):
-    """
-    As per DISA STIG compliance requirements, the operating system must be configured
-    so that the SSH daemon does not allow X11 Forwarding.
-    This test verifies that X11UseLocalhost is set to yes in the SSH daemon configuration.
-    Ref: SRG-OS-000480-GPOS-00227
-    """
-    assert (
-        sshd.get_config_section("x11uselocalhost") == "yes"
-    ), "stigcompliance: X11UseLocalhost must be set to yes in sshd configuration"
