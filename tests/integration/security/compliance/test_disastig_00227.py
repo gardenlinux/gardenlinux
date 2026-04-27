1b2db5f5c81a83e35eded7e10b82f65ebebb8750
Saubhik Datta <50126327+saubhikdattagithub@users.noreply.github.com>
2026-04-27 13:51:24 +0200

    style: apply black formatting to test_disastig_00227.py

--- tests/integration/security/compliance/test_disastig_00227.py
+++ tests/integration/security/compliance/test_disastig_00227.py
@@ -12,9 +12,9 @@ def test_x11_forwarding_is_disabled(sshd: Sshd):
     This test verifies that X11Forwarding is set to no in the SSH daemon configuration.
     Ref: SRG-OS-000480-GPOS-00227
     """
-    assert sshd.get_config_section("x11forwarding") == "no", (
-        "stigcompliance: X11Forwarding must be set to no in sshd configuration"
-    )
+    assert (
+        sshd.get_config_section("x11forwarding") == "no"
+    ), "stigcompliance: X11Forwarding must be set to no in sshd configuration"
 
 
 @pytest.mark.feature("disaSTIGmedium")
@@ -27,6 +27,6 @@ def test_x11_use_localhost_is_enabled(sshd: Sshd):
     This test verifies that X11UseLocalhost is set to yes in the SSH daemon configuration.
     Ref: SRG-OS-000480-GPOS-00227
     """
-    assert sshd.get_config_section("x11uselocalhost") == "yes", (
-        "stigcompliance: X11UseLocalhost must be set to yes in sshd configuration"
-    )
+    assert (
+        sshd.get_config_section("x11uselocalhost") == "yes"
+    ), "stigcompliance: X11UseLocalhost must be set to yes in sshd configuration"
