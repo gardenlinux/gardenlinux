17c58d66397075c405f6b31f2b5e6df27b2dd26f
Vivus Ignis <ignis@posteo.de>
2026-04-27 13:16:06 +0200

    nx check

---
+++ tests/integration/security/compliance/test_disastig_00192.py
@@ -0,0 +1,22 @@
+"""
+Ref: SRG-OS-000433-GPOS-00192
+
+Verify the operating system implements non-executable data to protect its
+memory from unauthorized code execution.
+"""
+
+
+def test_nx_bit_hardware_support():
+    """Verify hardware supports Non-Executable (NX) memory."""
+    with open("/proc/cpuinfo") as cpuinfo:
+        assert (
+            " nx " in cpuinfo.read()
+        ), "Finding: Hardware does not support or report the NX bit."
+
+
+def test_nx_not_disabled_at_boot():
+    """Verify the kernel has not been explicitly told to disable NX."""
+    with open("/proc/cmdline") as cmdline:
+        assert (
+            "nx=off" not in cmdline.read()
+        ), "Finding: The kernel boot parameter 'nx=off' is present, disabling memory protection."
