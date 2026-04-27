f71e9b0b4c6c7bd2dd099ba4938970169ac31a6c
Saubhik Datta <50126327+saubhikdattagithub@users.noreply.github.com>
2026-04-27 13:51:24 +0200

    style: apply black formatting to test_disastig_00154.py

--- tests/integration/security/compliance/test_disastig_00154.py
+++ tests/integration/security/compliance/test_disastig_00154.py
@@ -12,6 +12,6 @@ def test_selinux_is_active_lsm(lsm):
     This test verifies that SELinux is active as a Linux Security Module.
     Ref: SRG-OS-000312-GPOS-00154
     """
-    assert "selinux" in lsm, (
-        "stigcompliance: SELinux is not listed as an active Linux Security Module"
-    )
+    assert (
+        "selinux" in lsm
+    ), "stigcompliance: SELinux is not listed as an active Linux Security Module"
