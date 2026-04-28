a7168e473397c20ef9bdb0e47ce311f52808c1d5
Vivus Ignis <ignis@posteo.de>
2026-04-28 10:28:51 +0200

    restrict tests to x86

--- tests/integration/security/compliance/test_disastig_00192.py
+++ tests/integration/security/compliance/test_disastig_00192.py
@@ -1,3 +1,5 @@
+import pytest
+
 """
 Ref: SRG-OS-000433-GPOS-00192
 
@@ -5,7 +7,10 @@ Verify the operating system implements non-executable data to protect its
 memory from unauthorized code execution.
 """
 
+# Note: for arm arch XN (execute never) feature is always present
 
+
+@pytest.mark.arch("amd64")
 def test_nx_bit_hardware_support():
     """Verify hardware supports Non-Executable (NX) memory."""
     with open("/proc/cpuinfo") as cpuinfo:
@@ -14,6 +19,7 @@ def test_nx_bit_hardware_support():
         ), "Finding: Hardware does not support or report the NX bit."
 
 
+@pytest.mark.arch("amd64")
 def test_nx_not_disabled_at_boot():
     """Verify the kernel has not been explicitly told to disable NX."""
     with open("/proc/cmdline") as cmdline:
