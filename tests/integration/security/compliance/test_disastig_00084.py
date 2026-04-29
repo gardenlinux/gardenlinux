ff1a7a5d059e6900943b0d299f7e67d73947bff3
Vivus Ignis <ignis@posteo.de>
2026-04-29 09:53:04 +0200

    fixed disa stig feature name

--- tests/integration/security/compliance/test_disastig_00084.py
+++ tests/integration/security/compliance/test_disastig_00084.py
@@ -7,7 +7,7 @@ Verify the operating system reveals error messages only to authorized users.
 """
 
 
-@pytest.mark.feature("stigMedium")
+@pytest.mark.feature("disaSTIGmedium")
 def test_systemd_journal_is_not_world_readable(file):
     assert file.is_owned_by_user("/var/log/journal", "root")
     assert file.has_permissions("/var/log/journal", "rwxr-s---")
