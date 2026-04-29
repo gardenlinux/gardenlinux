84e9f664ead7c0bcf51b1edd50cd6645aa936fdd
Vivus Ignis <ignis@posteo.de>
2026-04-29 09:48:54 +0200

    feature filter

--- tests/integration/security/compliance/test_disastig_00063.py
+++ tests/integration/security/compliance/test_disastig_00063.py
@@ -10,23 +10,21 @@ supports on-demand reporting requirements.
 """
 
 
+@pytest.mark.feature("log")
 def test_audit_reporting_tools_installed(dpkg):
-    if dpkg.package_is_installed("auditd"):
-        tools = ["aureport", "ausearch"]
-        for tool in tools:
-            path = shutil.which(tool)
-            assert (
-                path is not None
-            ), f"'{tool}' audit reporting utility is not installed"
+    tools = ["aureport", "ausearch"]
+    for tool in tools:
+        path = shutil.which(tool)
+        assert path is not None, f"'{tool}' audit reporting utility is not installed"
 
 
+@pytest.mark.feature("log")
 @pytest.mark.root(
     "Audit reporting requires root privileges to open /var/log/audit/audit.log"
 )
 def test_audit_reporting_execution(shell, dpkg):
-    if dpkg.package_is_installed("auditd"):
-        result = shell("aureport --summary", capture_output=True, ignore_exit_code=True)
+    result = shell("aureport --summary", capture_output=True, ignore_exit_code=True)
 
-        assert (
-            "Summary" in result.stdout
-        ), f"'aureport' executed but returned an unexpected result: {result.stdout}"
+    assert (
+        "Summary" in result.stdout
+    ), f"'aureport' executed but returned an unexpected result: {result.stdout}"
