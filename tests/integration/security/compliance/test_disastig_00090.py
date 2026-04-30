41760fbd4c9a9fa2cccb6791e35236f0b43f19ed
Vivus Ignis <ignis@posteo.de>
2026-04-30 11:03:24 +0200

    a way to mark disa stig rules covered by tests somewhere else

---
+++ tests/integration/security/compliance/test_disastig_00090.py
@@ -0,0 +1,10 @@
+import pytest
+
+"""
+Ref: SRG-OS-000240-GPOS-00090
+
+Verify the operating system automatically audits account disabling actions.
+"""
+
+def test_disastig_00090():
+    pytest.skip(reason="covered by test_disastig_00089.py")
---
+++ tests/integration/security/compliance/test_disastig_00091.py
@@ -0,0 +1,10 @@
+import pytest
+
+"""
+Ref: SRG-OS-000241-GPOS-00091
+Verify the operating system automatically audits account removal actions.
+"""
+
+
+def test_disastig_00091():
+    pytest.skip(reason="covered by test_disastig_00089.py")
