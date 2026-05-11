78d8d25f976d619e02779b14959cc21bca4ef79d
Vivus Ignis <ignis@posteo.de>
2026-05-11 12:13:47 +0200

    added missing feature annotation

--- tests/integration/security/compliance/test_disastig_00212.py
+++ tests/integration/security/compliance/test_disastig_00212.py
@@ -1,3 +1,5 @@
+import pytest
+
 """
 Ref: SRG-OS-000468-GPOS-00212
 
@@ -6,6 +8,7 @@ successful/unsuccessful attempts to delete security objects occur.
 """
 
 
+@pytest.mark.feature("log")
 def test_attempts_to_delete_are_audited(audit_rule):
     for syscall in ["unlink", "unlinkat", "rename", "renameat", "rmdir"]:
         assert audit_rule(syscall=syscall, rule_fields=["auid>=1000", "auid!=-1"])
