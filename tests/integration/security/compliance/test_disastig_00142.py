c406fc3b261d5f0c926cf809ebbd54a68e301ba8
Vivus Ignis <ignis@posteo.de>
2026-05-19 10:13:59 +0200

    checksum moved to file plugin

--- tests/integration/security/compliance/test_disastig_00142.py
+++ tests/integration/security/compliance/test_disastig_00142.py
@@ -11,11 +11,11 @@ audit records when it provides a report generation capability.
 """
 
 
-@pytest.mark.root(reason="Access to audit.log")
-def test_audit_report_does_not_alter_original_audit_logs(shell, file_checksum):
+@pytest.mark.root(reason="Needs to access root-owned audit.log")
+def test_audit_report_does_not_alter_original_audit_logs(shell, file):
     with TemporaryDirectory(prefix="pytest-disa-stig-") as tmpd:
         shutil.copy("/var/log/audit/audit.log", f"{tmpd}/audit.log")
-        before_report_checksum = file_checksum(f"{tmpd}/audit.log")
+        before_report_checksum = file.checksum(f"{tmpd}/audit.log")
         shell(f"aureport -if {tmpd}/audit.log")
-        after_report_checksum = file_checksum(f"{tmpd}/audit.log")
+        after_report_checksum = file.checksum(f"{tmpd}/audit.log")
     assert before_report_checksum == after_report_checksum
