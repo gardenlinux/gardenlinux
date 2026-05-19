abb45e2310163670aeef0f222832138185fdd2f2
Vivus Ignis <ignis@posteo.de>
2026-05-19 11:12:36 +0200

    disa stig 00061

---
+++ tests/integration/security/compliance/test_disastig_00061.py
@@ -0,0 +1,13 @@
+import pytest
+
+"""
+Ref: SRG-OS-000120-GPOS-00061
+
+Verify the operating system uses mechanisms meeting the requirements of
+applicable federal laws, Executive orders, directives, policies, regulations,
+standards, and guidance for authentication to a cryptographic module.
+"""
+
+
+def test_disastig_00061():
+    pytest.skip(reason="covered by test_fips.py")
