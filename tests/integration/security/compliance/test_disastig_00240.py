34d2134c79442eaeee955a8bafd77f51f608870b
Vivus Ignis <ignis@posteo.de>
2026-05-21 13:55:00 +0200

    disa stig 00240

---
+++ tests/integration/security/compliance/test_disastig_00240.py
@@ -0,0 +1,22 @@
+import pytest
+
+"""
+Ref: SRG-OS-000780-GPOS-00240
+
+Verify the operating system is configured to provide protected storage for
+cryptographic keys with organization-defined safeguards and/or hardware
+protected key store.
+"""
+
+
+@pytest.mark.feature("_tpm")
+def test_tpm2_service_enabled(systemd):
+    assert systemd.is_enabled("tpm2")
+    assert systemd.is_active("tpm2")
+
+
+@pytest.mark.feature("_tpm")
+def test__debug_cryptsetup(shell):
+    output = shell("cryptsetup list", capture_output=True)
+    print(f"{output=}")
+    assert False
