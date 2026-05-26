f48504c4452b93a6915b59e79ba970e9a0951f8a
Vivus Ignis <ignis@posteo.de>
2026-05-26 11:30:38 +0200

    disa stig 00014 (#4830)

---
+++ tests/integration/security/compliance/test_disastig_00014.py
@@ -0,0 +1,12 @@
+import pytest
+
+"""
+Ref: SRG-OS-000033-GPOS-00014
+
+Verify the operating system implements DoD-approved encryption to protect the
+confidentiality of remote access sessions.
+"""
+
+
+def test_disastig_00014():
+    pytest.skip(reason="covered by test_fips.py")
