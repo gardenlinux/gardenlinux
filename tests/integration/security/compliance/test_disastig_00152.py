84c5af21b6552d39d61b2e26d038a4d8d144cd3a
Saubhik Datta <50126327+saubhikdattagithub@users.noreply.github.com>
2026-06-11 14:56:49 +0200

    V-id + diki format

--- tests/integration/security/compliance/test_disastig_00152.py
+++ tests/integration/security/compliance/test_disastig_00152.py
@@ -1,12 +1,8 @@
-import pytest
-
 """
 Ref: SRG-OS-000365-GPOS-00152
-
-Verify the operating system audits the enforcement actions used to
-restrict access associated with changes to the system.
 """
 
+import pytest
 
 @pytest.mark.security_id(203719)
 @pytest.mark.testcov(["GL-TESTCOV-disaSTIGmedium-config-audit-rules-d-disaSTIG-rules"])
@@ -16,6 +12,7 @@ restrict access associated with changes to the system.
 @pytest.mark.booted(reason="requires audit subsystem running")
 @pytest.mark.root(reason="required to query audit logs")
 def test_enforcement_action_audited(audit_rule) -> None:
+    """Verify the operating system audits the enforcement actions used to restrict access associated with changes to the system"""
     assert audit_rule(
         fs_watch_path="/etc/shadow", access_types="wa"
     ), "stigcompliance: no audit rule found watching /etc/shadow for write/append access"
