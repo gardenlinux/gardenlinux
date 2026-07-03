fc6c2c571793d703f9e581c56a0c4755c74395ea
Georgi Chulkov <georgi.chulkov@sap.com>
2026-07-03 16:50:18 +0300

    Fix broken sudo test

--- features/server/test/test_reset_env_vars.py
+++ features/server/test/test_reset_env_vars.py
@@ -4,9 +4,9 @@ import pytest
 @pytest.mark.parametrize(
     "file,args,sudo",
     [
-        ("/etc/sudoers", {"Defaults": "env_reset"}, True)
+        ("/etc/sudoers", {"Defaults": "secure_path"}, True)
     ]
 )
 
-def test_reset_env_vars(client, file, args, sudo):
+def test_secure_path_is_set(client, file, args, sudo):
     file_content(client, file, args, invert=False, ignore_missing=False, sudo=sudo)
