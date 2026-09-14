5282d604bb7088fbe445823cde185e8955f66735
Saubhik Datta <50126327+saubhikdattagithub@users.noreply.github.com>
2026-09-14 13:13:29 +0200

    fix: update PYTHON_REPO_NAME to python-build-standalone

--- tests/util/python.env.sh
+++ tests/util/python.env.sh
@@ -5,7 +5,7 @@
 
 
 export PYTHON_REPO_OWNER="gardenlinux"
-export PYTHON_REPO_NAME="package-python-standalone"
+export PYTHON_REPO_NAME="python-build-standalone"
 export PYTHON_SOURCE="https://github.com/${PYTHON_REPO_OWNER}/${PYTHON_REPO_NAME}/releases/download"
 export PYTHON_VERSION_SHORT="3.14"
 export PYTHON_VERSION="3.14.7"
